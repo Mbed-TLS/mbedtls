1f761153408c1be3773009c64b41f34d85ea199b
Paul Bakker <p.j.bakker@polarssl.org>
2010-02-18 18:16:31 +0000

    - Fixed bug resulting in failure to send the last certificate in the chain in ssl_write_certificate() and ssl_write_certificate_request()

--- library/debug.c
+++ library/debug.c
@@ -182,7 +182,7 @@ void debug_print_crt( ssl_context *ssl, int level,
     prefix[maxlen] = '\0';
     maxlen = sizeof( str ) - 1;
 
-    while( crt != NULL && crt->next != NULL )
+    while( crt != NULL && crt->version != 0 )
     {
         char buf[1024];
         x509parse_cert_info( buf, sizeof( buf ) - 1, prefix, crt );

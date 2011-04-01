3efa575ff2bdaa52d7caccf28e0c2a79c50a1b60
Paul Bakker <p.j.bakker@polarssl.org>
2011-04-01 12:23:26 +0000

    - Ready for release 0.99-pre4

--- doxygen/input/doc_mainpage.h
+++ doxygen/input/doc_mainpage.h
@@ -4,7 +4,7 @@ l/**
  */
 
 /**
- * @mainpage PolarSSL v0.99-pre3 source code documentation
+ * @mainpage PolarSSL v0.99-pre4 source code documentation
  * 
  * This documentation describes the internal structure of PolarSSL.  It was
  * automatically generated from specially formatted comment blocks in

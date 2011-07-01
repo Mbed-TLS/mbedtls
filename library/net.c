1fad5bfb197a44241b8b7879f8520a651586b7da
Paul Bakker <p.j.bakker@polarssl.org>
2011-07-01 09:07:24 +0000

    - Added define for OpenBSD (sys/endian.h)

--- library/net.c
+++ library/net.c
@@ -59,7 +59,7 @@ static int wsa_init_done = 0;
 #include <netdb.h>
 #include <errno.h>
 
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) || defined(__OpenBSD__)
 #include <sys/endian.h>
 #elif defined(__APPLE__)
 #include <machine/endian.h>

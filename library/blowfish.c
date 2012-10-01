d6f17b492f8373bc0c002da901a30918623ab2d4
Paul Bakker <p.j.bakker@polarssl.org>
2012-10-01 20:58:19 +0000

    - Moved definition to top to prevent MS VC compiler warning

--- library/blowfish.c
+++ library/blowfish.c
@@ -58,8 +58,15 @@
 }
 #endif
 
+static const uint32_t P[BLOWFISH_ROUNDS + 2] = {
+        0x243F6A88L, 0x85A308D3L, 0x13198A2EL, 0x03707344L,
+        0xA4093822L, 0x299F31D0L, 0x082EFA98L, 0xEC4E6C89L,
+        0x452821E6L, 0x38D01377L, 0xBE5466CFL, 0x34E90C6CL,
+        0xC0AC29B7L, 0xC97C50DDL, 0x3F84D5B5L, 0xB5470917L,
+        0x9216D5D9L, 0x8979FB1BL
+};
+
 /* declarations of data at the end of this file */
-static const uint32_t P[];
 static const uint32_t S[4][256];
 
 static uint32_t F(blowfish_context *ctx, uint32_t x) 
@@ -359,14 +366,6 @@ int blowfish_crypt_ctr( blowfish_context *ctx,
     return( 0 );
 }
 #endif /* POLARSSL_CIPHER_MODE_CTR */
-
-static const uint32_t P[BLOWFISH_ROUNDS + 2] = {
-        0x243F6A88L, 0x85A308D3L, 0x13198A2EL, 0x03707344L,
-        0xA4093822L, 0x299F31D0L, 0x082EFA98L, 0xEC4E6C89L,
-        0x452821E6L, 0x38D01377L, 0xBE5466CFL, 0x34E90C6CL,
-        0xC0AC29B7L, 0xC97C50DDL, 0x3F84D5B5L, 0xB5470917L,
-        0x9216D5D9L, 0x8979FB1BL
-};
 
 static const uint32_t S[4][256] = {
     {   0xD1310BA6L, 0x98DFB5ACL, 0x2FFD72DBL, 0xD01ADFB7L,

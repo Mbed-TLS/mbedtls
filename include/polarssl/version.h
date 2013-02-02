c7a2da437e5174dd9487c442c6315114c2029169
Paul Bakker <p.j.bakker@polarssl.org>
2013-02-02 19:23:57 +0100

    Updated for PolarSSL 1.2.5

--- include/polarssl/version.h
+++ include/polarssl/version.h
@@ -39,16 +39,16 @@
  */
 #define POLARSSL_VERSION_MAJOR  1
 #define POLARSSL_VERSION_MINOR  2
-#define POLARSSL_VERSION_PATCH  4
+#define POLARSSL_VERSION_PATCH  5
 
 /**
  * The single version number has the following structure:
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define POLARSSL_VERSION_NUMBER         0x01020400
-#define POLARSSL_VERSION_STRING         "1.2.4"
-#define POLARSSL_VERSION_STRING_FULL    "PolarSSL 1.2.4"
+#define POLARSSL_VERSION_NUMBER         0x01020500
+#define POLARSSL_VERSION_STRING         "1.2.5"
+#define POLARSSL_VERSION_STRING_FULL    "PolarSSL 1.2.5"
 
 #if defined(POLARSSL_VERSION_C)
 

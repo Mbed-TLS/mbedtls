926c8e49fe1c4afba0370a1ec6d0ac481707b51f
Paul Bakker <p.j.bakker@polarssl.org>
2013-03-06 10:23:34 +0100

    Fixed possible NULL pointer exception in ssl_get_ciphersuite()

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -3482,6 +3482,9 @@ int ssl_get_ciphersuite_id( const char *ciphersuite_name )
 
 const char *ssl_get_ciphersuite( const ssl_context *ssl )
 {
+    if( ssl == NULL || ssl->session == NULL )
+        return NULL;
+
     return ssl_get_ciphersuite_name( ssl->session->ciphersuite );
 }
 

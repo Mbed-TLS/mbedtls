ac0fba53898c9113644665cda0ea16ae9f26d1ea
Paul Bakker <p.j.bakker@polarssl.org>
2013-03-13 10:28:40 +0100

    Added missing header for MD2 and made code compile with missing header files

--- library/x509write.c
+++ library/x509write.c
@@ -30,11 +30,24 @@
 #include "polarssl/asn1write.h"
 #include "polarssl/x509write.h"
 #include "polarssl/x509.h"
-#include "polarssl/sha1.h"
-#include "polarssl/sha2.h"
-#include "polarssl/sha4.h"
+#if defined(POLARSSL_MD2_C)
+#include "polarssl/md2.h"
+#endif
+#if defined(POLARSSL_MD4_C)
 #include "polarssl/md4.h"
+#endif
+#if defined(POLARSSL_MD5_C)
 #include "polarssl/md5.h"
+#endif
+#if defined(POLARSSL_SHA1_C)
+#include "polarssl/sha1.h"
+#endif
+#if defined(POLARSSL_SHA2_C)
+#include "polarssl/sha2.h"
+#endif
+#if defined(POLARSSL_SHA4_C)
+#include "polarssl/sha4.h"
+#endif
 
 int x509_write_pubkey_der( unsigned char *buf, size_t size, rsa_context *rsa )
 {

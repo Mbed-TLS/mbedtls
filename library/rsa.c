735b8fcb0bba4b02c4c5d106b8ab416fa810d05f
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2013-09-13 12:57:23 +0200

    Fix blunder in 8a109f1

--- library/rsa.c
+++ library/rsa.c
@@ -269,9 +269,9 @@ static int rsa_prepare_blinding( rsa_context *ctx,
     {
         /* We already have blinding values, just update them by squaring */
         MPI_CHK( mpi_mul_mpi( &ctx->Vi, &ctx->Vi, &ctx->Vi ) );
-        MPI_CHK( mpi_mod_mpi( &ctx->Vi, &ctx->Vi, &ctx->P ) );
+        MPI_CHK( mpi_mod_mpi( &ctx->Vi, &ctx->Vi, &ctx->N ) );
         MPI_CHK( mpi_mul_mpi( &ctx->Vf, &ctx->Vf, &ctx->Vf ) );
-        MPI_CHK( mpi_mod_mpi( &ctx->Vf, &ctx->Vf, &ctx->P ) );
+        MPI_CHK( mpi_mod_mpi( &ctx->Vf, &ctx->Vf, &ctx->N ) );
 
         return( 0 );
     }

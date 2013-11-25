b9f6d507dd7fafa940b5afbc6f1b85701eda8ba3
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2013-11-25 17:55:17 +0100

    crypt_and_hash: check MAC earlier

--- programs/aes/crypt_and_hash.c
+++ programs/aes/crypt_and_hash.c
@@ -454,17 +454,6 @@ int main( int argc, char *argv[] )
         }
 
         /*
-         * Write the final block of data
-         */
-        cipher_finish( &cipher_ctx, output, &olen );
-
-        if( fwrite( output, 1, olen, fout ) != olen )
-        {
-            fprintf( stderr, "fwrite(%ld bytes) failed\n", (long) olen );
-            goto exit;
-        }
-
-        /*
          * Verify the message authentication code.
          */
         md_hmac_finish( &md_ctx, digest );
@@ -484,6 +473,17 @@ int main( int argc, char *argv[] )
         {
             fprintf( stderr, "HMAC check failed: wrong key, "
                              "or file corrupted.\n" );
+            goto exit;
+        }
+
+        /*
+         * Write the final block of data
+         */
+        cipher_finish( &cipher_ctx, output, &olen );
+
+        if( fwrite( output, 1, olen, fout ) != olen )
+        {
+            fprintf( stderr, "fwrite(%ld bytes) failed\n", (long) olen );
             goto exit;
         }
     }

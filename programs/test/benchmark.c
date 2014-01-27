a43df0fffc1da6c83f3823a1066ee75eaa721d69
Gergely Budai <gbudai76@gmail.com>
2014-01-27 23:27:06 +0100

    Increase title size (fits to increased curve names). Give verbose errors on failures.

--- programs/test/benchmark.c
+++ programs/test/benchmark.c
@@ -49,14 +49,15 @@
 #include "polarssl/dhm.h"
 #include "polarssl/ecdsa.h"
 #include "polarssl/ecdh.h"
+#include "polarssl/error.h"
 
 #if defined _MSC_VER && !defined snprintf
 #define snprintf _snprintf
 #endif
 
 #define BUFSIZE         1024
-#define HEADER_FORMAT   "  %-18s :  "
-#define TITLE_LEN       19
+#define HEADER_FORMAT   "  %-21s :  "
+#define TITLE_LEN       22
 
 #if !defined(POLARSSL_TIMING_C)
 int main( int argc, char *argv[] )
@@ -131,7 +132,10 @@ do {                                                                    \
     }                                                                   \
                                                                         \
     if( ret != 0 )                                                      \
-        printf( "FAILED\n" );                                           \
+    {                                                                   \
+        polarssl_strerror( ret, ( char * )tmp, sizeof( tmp ) );         \
+        printf( "FAILED: %s\n", tmp );                                  \
+    }                                                                   \
     else                                                                \
         printf( "%9lu " TYPE "/s\n", i / 3 );                           \
 } while( 0 )

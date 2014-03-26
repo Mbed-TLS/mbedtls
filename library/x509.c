5fff23b92abbf303c02f9e35f38a552372b2750a
Paul Bakker <p.j.bakker@polarssl.org>
2014-03-26 15:34:54 +0100

    x509_get_current_time() uses localtime_r() to prevent thread issues

--- library/x509.c
+++ library/x509.c
@@ -636,18 +636,18 @@ static void x509_get_current_time( x509_time *now )
     now->min = st.wMinute;
     now->sec = st.wSecond;
 #else
-    struct tm *lt;
+    struct tm lt;
     time_t tt;
 
     tt = time( NULL );
-    lt = localtime( &tt );
+    localtime_r( &tt, &lt );
 
-    now->year = lt->tm_year + 1900;
-    now->mon = lt->tm_mon + 1;
-    now->day = lt->tm_mday;
-    now->hour = lt->tm_hour;
-    now->min = lt->tm_min;
-    now->sec = lt->tm_sec;
+    now->year = lt.tm_year + 1900;
+    now->mon = lt.tm_mon + 1;
+    now->day = lt.tm_mday;
+    now->hour = lt.tm_hour;
+    now->min = lt.tm_min;
+    now->sec = lt.tm_sec;
 #endif
 }
 

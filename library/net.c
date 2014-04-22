9292355974baae40efb7c2592afde09b312944ef
Markus Pfeiffer <markus.pfeiffer@morphism.de>
2014-04-22 20:16:15 +0000

    Make compilation on DragonFly work

--- library/net.c
+++ library/net.c
@@ -74,7 +74,7 @@ static int wsa_init_done = 0;
 #include <errno.h>
 
 #if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__) ||  \
-    defined(__DragonflyBSD__)
+    defined(__DragonFly__)
 #include <sys/endian.h>
 #elif defined(__APPLE__) || defined(HAVE_MACHINE_ENDIAN_H) ||   \
       defined(EFIX64) || defined(EFI32)

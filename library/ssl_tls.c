3a306b90673ccd5f057827ae76113eaf290e1e8b
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2014-04-29 15:11:17 +0200

    Fix misplaced #endif in ssl_tls.c

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -4753,8 +4753,6 @@ md_type_t ssl_md_alg_from_hash( unsigned char hash )
     }
 }
 
-#endif
-
 #if defined(POLARSSL_SSL_SET_CURVES)
 /*
  * Check is a curve proposed by the peer is in our list.
@@ -4852,3 +4850,5 @@ int ssl_check_cert_usage( const x509_crt *cert,
     return( 0 );
 }
 #endif /* POLARSSL_X509_CRT_PARSE_C */
+
+#endif /* POLARSSL_SSL_TLS_C */

Mbed-TLS/mbedtls

commit 02d800c15160b1e8dc26e405e41c1ae91d38dcd2

Fix BN ASM for PowerPC

include/polarssl/bn_mul.h
@@ -278,63 +278,84 @@
 
 #if defined(__MACH__) && defined(__APPLE__)
 
-#define MULADDC_INIT                            \
-    asm( "ld     r3, %0         " :: "m" (s));  \
-    asm( "ld     r4, %0         " :: "m" (d));  \
-    asm( "ld     r5, %0         " :: "m" (c));  \
-    asm( "ld     r6, %0         " :: "m" (b));  \
-    asm( "addi   r3, r3, -8     " );            \
-    asm( "addi   r4, r4, -8     " );            \
-    asm( "addic  r5, r5,  0     " );
+#define MULADDC_INIT                \
+    asm(                            \
+        "                           \
+        ld     r3, %3;              \
+        ld     r4, %4;              \
+        ld     r5, %5;              \
+        ld     r6, %6;              \
+        addi   r3, r3, -8;          \
+        addi   r4, r4, -8;          \
+        addic  r5, r5,  0;          \
+        "
 
-#define MULADDC_CORE                            \
-    asm( "ldu    r7, 8(r3)      " );            \
-    asm( "mulld  r8, r7, r6     " );            \
-    asm( "mulhdu r9, r7, r6     " );            \
-    asm( "adde   r8, r8, r5     " );            \
-    asm( "ld     r7, 8(r4)      " );            \
-    asm( "addze  r5, r9         " );            \
-    asm( "addc   r8, r8, r7     " );            \
-    asm( "stdu   r8, 8(r4)      " );
+#define MULADDC_CORE                \
+        "                           \
+        ldu    r7, 8(r3);           \
+        mulld  r8, r7, r6;          \
+        mulhdu r9, r7, r6;          \
+        adde   r8, r8, r5;          \
+        ld     r7, 8(r4);           \
+        addze  r5, r9;              \
+        addc   r8, r8, r7;          \
+        stdu   r8, 8(r4);           \
+        "
 
-#define MULADDC_STOP                            \
-    asm( "addze  r5, r5         " );            \
-    asm( "addi   r4, r4, 8      " );            \
-    asm( "addi   r3, r3, 8      " );            \
-    asm( "std    r5, %0         " : "=m" (c));  \
-    asm( "std    r4, %0         " : "=m" (d));  \
-    asm( "std    r3, %0         " : "=m" (s) :: \
-    "r3", "r4", "r5", "r6", "r7", "r8", "r9" );
+#define MULADDC_STOP                \
+        "                           \
+        addze  r5, r5;              \
+        addi   r4, r4, 8;           \
+        addi   r3, r3, 8;           \
+        std    r5, %0;              \
+        std    r4, %1;              \
+        std    r3, %2;              \
+        "                           \
+        : "=m" (c), "=m" (d), "=m" (s)              \
+        : "m" (s), "m" (d), "m" (c), "m" (b)        \
+        : "r3", "r4", "r5", "r6", "r7", "r8", "r9"  \
+    );
+
 
 #else
 
-#define MULADDC_INIT                            \
-    asm( "ld     %%r3, %0       " :: "m" (s));  \
-    asm( "ld     %%r4, %0       " :: "m" (d));  \
-    asm( "ld     %%r5, %0       " :: "m" (c));  \
-    asm( "ld     %%r6, %0       " :: "m" (b));  \
-    asm( "addi   %r3, %r3, -8   " );            \
-    asm( "addi   %r4, %r4, -8   " );            \
-    asm( "addic  %r5, %r5,  0   " );
+#define MULADDC_INIT                \
+    asm(                            \
+        "                           \
+        ld     %%r3, %3;            \
+        ld     %%r4, %4;            \
+        ld     %%r5, %5;            \
+        ld     %%r6, %6;            \
+        addi   %%r3, %%r3, -8;      \
+        addi   %%r4, %%r4, -8;      \
+        addic  %%r5, %%r5,  0;      \
+        "
 
-#define MULADDC_CORE                            \
-    asm( "ldu    %r7, 8(%r3)    " );            \
-    asm( "mulld  %r8, %r7, %r6  " );            \
-    asm( "mulhdu %r9, %r7, %r6  " );            \
-    asm( "adde   %r8, %r8, %r5  " );            \
-    asm( "ld     %r7, 8(%r4)    " );            \
-    asm( "addze  %r5, %r9       " );            \
-    asm( "addc   %r8, %r8, %r7  " );            \
-    asm( "stdu   %r8, 8(%r4)    " );
+#define MULADDC_CORE                \
+        "                           \
+        ldu    %%r7, 8(%%r3);       \
+        mulld  %%r8, %%r7, %%r6;    \
+        mulhdu %%r9, %%r7, %%r6;    \
+        adde   %%r8, %%r8, %%r5;    \
+        ld     %%r7, 8(%%r4);       \
+        addze  %%r5, %%r9;          \
+        addc   %%r8, %%r8, %%r7;    \
+        stdu   %%r8, 8(%%r4);       \
+        "
 
-#define MULADDC_STOP                            \
-    asm( "addze  %r5, %r5       " );            \
-    asm( "addi   %r4, %r4, 8    " );            \
-    asm( "addi   %r3, %r3, 8    " );            \
-    asm( "std    %%r5, %0       " : "=m" (c));  \
-    asm( "std    %%r4, %0       " : "=m" (d));  \
-    asm( "std    %%r3, %0       " : "=m" (s) :: \
-    "r3", "r4", "r5", "r6", "r7", "r8", "r9" );
+#define MULADDC_STOP                \
+        "                           \
+        addze  %%r5, %%r5;          \
+        addi   %%r4, %%r4, 8;       \
+        addi   %%r3, %%r3, 8;       \
+        std    %%r5, %0;            \
+        std    %%r4, %1;            \
+        std    %%r3, %2;            \
+        "                           \
+        : "=m" (c), "=m" (d), "=m" (s)              \
+        : "m" (s), "m" (d), "m" (c), "m" (b)        \
+        : "r3", "r4", "r5", "r6", "r7", "r8", "r9"  \
+    );
 
 #endif
 
@@ -342,63 +363,83 @@
 
 #if defined(__MACH__) && defined(__APPLE__)
 
-#define MULADDC_INIT                            \
-    asm( "lwz    r3, %0         " :: "m" (s));  \
-    asm( "lwz    r4, %0         " :: "m" (d));  \
-    asm( "lwz    r5, %0         " :: "m" (c));  \
-    asm( "lwz    r6, %0         " :: "m" (b));  \
-    asm( "addi   r3, r3, -4     " );            \
-    asm( "addi   r4, r4, -4     " );            \
-    asm( "addic  r5, r5,  0     " );
+#define MULADDC_INIT            \
+    asm(                        \
+        "                       \
+        lwz    r3, %3;          \
+        lwz    r4, %4;          \
+        lwz    r5, %5;          \
+        lwz    r6, %6;          \
+        addi   r3, r3, -4;      \
+        addi   r4, r4, -4;      \
+        addic  r5, r5,  0;      \
+        "
 
-#define MULADDC_CORE                            \
-    asm( "lwzu   r7, 4(r3)      " );            \
-    asm( "mullw  r8, r7, r6     " );            \
-    asm( "mulhwu r9, r7, r6     " );            \
-    asm( "adde   r8, r8, r5     " );            \
-    asm( "lwz    r7, 4(r4)      " );            \
-    asm( "addze  r5, r9         " );            \
-    asm( "addc   r8, r8, r7     " );            \
-    asm( "stwu   r8, 4(r4)      " );
+#define MULADDC_CORE            \
+        "                       \
+        lwzu   r7, 4(r3);       \
+        mullw  r8, r7, r6;      \
+        mulhwu r9, r7, r6;      \
+        adde   r8, r8, r5;      \
+        lwz    r7, 4(r4);       \
+        addze  r5, r9;          \
+        addc   r8, r8, r7;      \
+        stwu   r8, 4(r4);       \
+        "
 
-#define MULADDC_STOP                            \
-    asm( "addze  r5, r5         " );            \
-    asm( "addi   r4, r4, 4      " );            \
-    asm( "addi   r3, r3, 4      " );            \
-    asm( "stw    r5, %0         " : "=m" (c));  \
-    asm( "stw    r4, %0         " : "=m" (d));  \
-    asm( "stw    r3, %0         " : "=m" (s) :: \
-    "r3", "r4", "r5", "r6", "r7", "r8", "r9" );
+#define MULADDC_STOP            \
+        "                       \
+        addze  r5, r5;          \
+        addi   r4, r4, 4;       \
+        addi   r3, r3, 4;       \
+        stw    r5, %0;          \
+        stw    r4, %1;          \
+        stw    r3, %2;          \
+        "                       \
+        : "=m" (c), "=m" (d), "=m" (s)              \
+        : "m" (s), "m" (d), "m" (c), "m" (b)        \
+        : "r3", "r4", "r5", "r6", "r7", "r8", "r9"  \
+    );
 
 #else
 
-#define MULADDC_INIT                            \
-    asm( "lwz    %%r3, %0       " :: "m" (s));  \
-    asm( "lwz    %%r4, %0       " :: "m" (d));  \
-    asm( "lwz    %%r5, %0       " :: "m" (c));  \
-    asm( "lwz    %%r6, %0       " :: "m" (b));  \
-    asm( "addi   %r3, %r3, -4   " );            \
-    asm( "addi   %r4, %r4, -4   " );            \
-    asm( "addic  %r5, %r5,  0   " );
+#define MULADDC_INIT                \
+    asm(                            \
+        "                           \
+        lwz    %%r3, %3;            \
+        lwz    %%r4, %4;            \
+        lwz    %%r5, %5;            \
+        lwz    %%r6, %6;            \
+        addi   %%r3, %%r3, -4;      \
+        addi   %%r4, %%r4, -4;      \
+        addic  %%r5, %%r5,  0;      \
+        "
 
-#define MULADDC_CORE                            \
-    asm( "lwzu   %r7, 4(%r3)    " );            \
-    asm( "mullw  %r8, %r7, %r6  " );            \
-    asm( "mulhwu %r9, %r7, %r6  " );            \
-    asm( "adde   %r8, %r8, %r5  " );            \
-    asm( "lwz    %r7, 4(%r4)    " );            \
-    asm( "addze  %r5, %r9       " );            \
-    asm( "addc   %r8, %r8, %r7  " );            \
-    asm( "stwu   %r8, 4(%r4)    " );
+#define MULADDC_CORE                \
+        "                           \
+        lwzu   %%r7, 4(%%r3);       \
+        mullw  %%r8, %%r7, %%r6;    \
+        mulhwu %%r9, %%r7, %%r6;    \
+        adde   %%r8, %%r8, %%r5;    \
+        lwz    %%r7, 4(%%r4);       \
+        addze  %%r5, %%r9;          \
+        addc   %%r8, %%r8, %%r7;    \
+        stwu   %%r8, 4(%%r4);       \
+        "
 
-#define MULADDC_STOP                            \
-    asm( "addze  %r5, %r5       " );            \
-    asm( "addi   %r4, %r4, 4    " );            \
-    asm( "addi   %r3, %r3, 4    " );            \
-    asm( "stw    %%r5, %0       " : "=m" (c));  \
-    asm( "stw    %%r4, %0       " : "=m" (d));  \
-    asm( "stw    %%r3, %0       " : "=m" (s) :: \
-    "r3", "r4", "r5", "r6", "r7", "r8", "r9" );
+#define MULADDC_STOP                \
+        "                           \
+        addze  %%r5, %%r5;          \
+        addi   %%r4, %%r4, 4;       \
+        addi   %%r3, %%r3, 4;       \
+        stw    %%r5, %0;            \
+        stw    %%r4, %1;            \
+        stw    %%r3, %2;            \
+        "                           \
+        : "=m" (c), "=m" (d), "=m" (s)              \
+        : "m" (s), "m" (d), "m" (c), "m" (b)        \
+        : "r3", "r4", "r5", "r6", "r7", "r8", "r9"  \
+    );
 
 #endif
 

commit 71fd5c28ec396fa70c83543f9ef291839ed1eb9a

Fix bignum asm for OS X PPC
A typical PPC-supporting Xcode toolchain (Apple gcc + Apple cctools
assembler), when targeting PPC, treats semicolons as comment
delimiters, even for inline assembly. As a result, commit
02d800c15160b1e8dc26e405e41c1ae91d38dcd2 broke the bignum asm code
for OS X on both PPC32 and PPC64, by effectively commenting out
nearly all of the asm code. Replacing the semicolons with \n makes
this code work again.

include/polarssl/bn_mul.h
@@ -289,39 +289,43 @@
 #if defined(__powerpc64__) || defined(__ppc64__)
 
 #if defined(__MACH__) && defined(__APPLE__)
+/* Apple gcc + Apple cctools assembler (i.e. a typical Xcode toolchain with
+ * PPC support) treats semicolons as comment delimiters when targeting PPC,
+ * even for inline assembly. Thus, newlines (\n) are required.
+ */
 
 #define MULADDC_INIT                \
     asm(                            \
         "                           \
-        ld     r3, %3;              \
-        ld     r4, %4;              \
-        ld     r5, %5;              \
-        ld     r6, %6;              \
-        addi   r3, r3, -8;          \
-        addi   r4, r4, -8;          \
-        addic  r5, r5,  0;          \
+        ld     r3, %3\n             \
+        ld     r4, %4\n             \
+        ld     r5, %5\n             \
+        ld     r6, %6\n             \
+        addi   r3, r3, -8\n         \
+        addi   r4, r4, -8\n         \
+        addic  r5, r5,  0\n         \
         "
 
 #define MULADDC_CORE                \
         "                           \
-        ldu    r7, 8(r3);           \
-        mulld  r8, r7, r6;          \
-        mulhdu r9, r7, r6;          \
-        adde   r8, r8, r5;          \
-        ld     r7, 8(r4);           \
-        addze  r5, r9;              \
-        addc   r8, r8, r7;          \
-        stdu   r8, 8(r4);           \
+        ldu    r7, 8(r3)\n          \
+        mulld  r8, r7, r6\n         \
+        mulhdu r9, r7, r6\n         \
+        adde   r8, r8, r5\n         \
+        ld     r7, 8(r4)\n          \
+        addze  r5, r9\n             \
+        addc   r8, r8, r7\n         \
+        stdu   r8, 8(r4)\n          \
         "
 
 #define MULADDC_STOP                \
         "                           \
-        addze  r5, r5;              \
-        addi   r4, r4, 8;           \
-        addi   r3, r3, 8;           \
-        std    r5, %0;              \
-        std    r4, %1;              \
-        std    r3, %2;              \
+        addze  r5, r5\n             \
+        addi   r4, r4, 8\n          \
+        addi   r3, r3, 8\n          \
+        std    r5, %0\n             \
+        std    r4, %1\n             \
+        std    r3, %2\n             \
         "                           \
         : "=m" (c), "=m" (d), "=m" (s)              \
         : "m" (s), "m" (d), "m" (c), "m" (b)        \
@@ -374,39 +378,43 @@
 #else /* PPC32 */
 
 #if defined(__MACH__) && defined(__APPLE__)
+/* Apple gcc + Apple cctools assembler (i.e. a typical Xcode toolchain with
+ * PPC support) treats semicolons as comment delimiters when targeting PPC,
+ * even for inline assembly. Thus, newlines (\n) are required.
+ */
 
 #define MULADDC_INIT            \
     asm(                        \
         "                       \
-        lwz    r3, %3;          \
-        lwz    r4, %4;          \
-        lwz    r5, %5;          \
-        lwz    r6, %6;          \
-        addi   r3, r3, -4;      \
-        addi   r4, r4, -4;      \
-        addic  r5, r5,  0;      \
+        lwz    r3, %3\n         \
+        lwz    r4, %4\n         \
+        lwz    r5, %5\n         \
+        lwz    r6, %6\n         \
+        addi   r3, r3, -4\n     \
+        addi   r4, r4, -4\n     \
+        addic  r5, r5,  0\n     \
         "
 
 #define MULADDC_CORE            \
         "                       \
-        lwzu   r7, 4(r3);       \
-        mullw  r8, r7, r6;      \
-        mulhwu r9, r7, r6;      \
-        adde   r8, r8, r5;      \
-        lwz    r7, 4(r4);       \
-        addze  r5, r9;          \
-        addc   r8, r8, r7;      \
-        stwu   r8, 4(r4);       \
+        lwzu   r7, 4(r3)\n      \
+        mullw  r8, r7, r6\n     \
+        mulhwu r9, r7, r6\n     \
+        adde   r8, r8, r5\n     \
+        lwz    r7, 4(r4)\n      \
+        addze  r5, r9\n         \
+        addc   r8, r8, r7\n     \
+        stwu   r8, 4(r4)\n      \
         "
 
 #define MULADDC_STOP            \
         "                       \
-        addze  r5, r5;          \
-        addi   r4, r4, 4;       \
-        addi   r3, r3, 4;       \
-        stw    r5, %0;          \
-        stw    r4, %1;          \
-        stw    r3, %2;          \
+        addze  r5, r5\n         \
+        addi   r4, r4, 4\n      \
+        addi   r3, r3, 4\n      \
+        stw    r5, %0\n         \
+        stw    r4, %1\n         \
+        stw    r3, %2\n         \
         "                       \
         : "=m" (c), "=m" (d), "=m" (s)              \
         : "m" (s), "m" (d), "m" (c), "m" (b)        \
9eac4f7c4e847f31d67a2e60c5a07ced8b5f784c
Paul Bakker <p.j.bakker@polarssl.org>
2014-10-20 13:56:15 +0200

    Prepare for release 1.3.9

--- include/polarssl/version.h
+++ include/polarssl/version.h
@@ -43,16 +43,16 @@
  */
 #define POLARSSL_VERSION_MAJOR  1
 #define POLARSSL_VERSION_MINOR  3
-#define POLARSSL_VERSION_PATCH  8
+#define POLARSSL_VERSION_PATCH  9
 
 /**
  * The single version number has the following structure:
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define POLARSSL_VERSION_NUMBER         0x01030800
-#define POLARSSL_VERSION_STRING         "1.3.8"
-#define POLARSSL_VERSION_STRING_FULL    "PolarSSL 1.3.8"
+#define POLARSSL_VERSION_NUMBER         0x01030900
+#define POLARSSL_VERSION_STRING         "1.3.9"
+#define POLARSSL_VERSION_STRING_FULL    "PolarSSL 1.3.9"
 
 #if defined(POLARSSL_VERSION_C)
 

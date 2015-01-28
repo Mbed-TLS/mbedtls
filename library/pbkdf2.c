860b51642d94b41097e245ea0ed5c119a35ed194
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2015-01-28 17:12:07 +0000

    Fix url again

--- library/pbkdf2.c
+++ library/pbkdf2.c
@@ -8,7 +8,7 @@
  *
  *  Copyright (C) 2006-2014, ARM Limited, All Rights Reserved
  *
- *  This file is part of mbed TLS (https://www.polarssl.org)
+ *  This file is part of mbed TLS (https://polarssl.org)
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

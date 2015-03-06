e4d4890350f9ce4dd6eecf860dcbb45598334bf3
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2015-03-06 13:40:52 +0000

    Finish renaming website

--- programs/ssl/dtls_client.c
+++ programs/ssl/dtls_client.c
@@ -3,7 +3,7 @@
  *
  *  Copyright (C) 2014, Brainspark B.V.
  *
- *  This file is part of mbed TLS (https://polarssl.org)
+ *  This file is part of mbed TLS (https://tls.mbed.org)
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by

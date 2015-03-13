61fe8b02909f638a77918af090315e995bec7d10
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2015-03-13 14:33:16 +0000

    Add more -O level variety in all.sh

--- tests/scripts/all.sh
+++ tests/scripts/all.sh
@@ -129,9 +129,9 @@ cleanup
 cmake -D CMAKE_BUILD_TYPE:String=Debug .
 tests/scripts/curves.pl
 
-msg "build: Unix make, -O2 (gcc)" # ~ 30s
+msg "build: Unix make, -Os (gcc)" # ~ 30s
 cleanup
-CC=gcc CFLAGS=-Werror make
+CC=gcc CFLAGS='-Werror -Os' make
 
 # this is meant to cath missing #define polarssl_printf etc
 # disable fsio to catch some more missing #include <stdio.h>
@@ -144,7 +144,7 @@ scripts/config.pl unset POLARSSL_PLATFORM_MEMORY
 scripts/config.pl unset POLARSSL_MEMORY_C
 scripts/config.pl unset POLARSSL_MEMORY_BUFFER_ALLOC_C
 scripts/config.pl unset POLARSSL_FS_IO
-CC=gcc CFLAGS=-Werror make
+CC=gcc CFLAGS='-Werror -O0' make
 
 if uname -a | grep -F x86_64 >/dev/null; then
 msg "build: i386, make, gcc" # ~ 30s

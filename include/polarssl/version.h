3edec6c4edf398315b6b59e9ce26e12774095775
Paul Bakker <paul@brainspark.nl>
2015-08-11 13:22:10 +0100

    Prepare for 1.3.12 release

--- include/polarssl/version.h
+++ include/polarssl/version.h
@@ -40,16 +40,16 @@
  */
 #define POLARSSL_VERSION_MAJOR  1
 #define POLARSSL_VERSION_MINOR  3
-#define POLARSSL_VERSION_PATCH  11
+#define POLARSSL_VERSION_PATCH  12
 
 /**
  * The single version number has the following structure:
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define POLARSSL_VERSION_NUMBER         0x01030B00
-#define POLARSSL_VERSION_STRING         "1.3.11"
-#define POLARSSL_VERSION_STRING_FULL    "mbed TLS 1.3.11"
+#define POLARSSL_VERSION_NUMBER         0x01030C00
+#define POLARSSL_VERSION_STRING         "1.3.12"
+#define POLARSSL_VERSION_STRING_FULL    "mbed TLS 1.3.12"
 
 #if defined(POLARSSL_VERSION_C)
 

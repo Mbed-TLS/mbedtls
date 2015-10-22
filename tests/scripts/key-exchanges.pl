5df9216c9e4120d1bd1270b1ff710b22c81c05a7
Manuel Pégourié-Gonnard <mpg2@elzevir.fr>
2015-10-22 16:11:39 +0200

    Add script to test configs with single key exchanges

--- tests/scripts/key-exchanges.pl
+++ tests/scripts/key-exchanges.pl
@@ -1,19 +1,17 @@
 #!/usr/bin/perl
 
-# test dependencies on individual curves in tests
-# - build
-# - run test suite
+# test that all configs with only a single key exchange enabled build
 #
-# Usage: tests/scripts/curves.pl
+# Usage: tests/scripts/key-exchanges.pl
 
 use warnings;
 use strict;
 
 -d 'library' && -d 'include' && -d 'tests' or die "Must be run from root\n";
 
-my $sed_cmd = 's/^#define \(MBEDTLS_ECP_DP.*_ENABLED\)/\1/p';
+my $sed_cmd = 's/^#define \(MBEDTLS_KEY_EXCHANGE_.*_ENABLED\)/\1/p';
 my $config_h = 'include/mbedtls/config.h';
-my @curves = split( /\s+/, `sed -n -e '$sed_cmd' $config_h` );
+my @kexes = split( /\s+/, `sed -n -e '$sed_cmd' $config_h` );
 
 system( "cp $config_h $config_h.bak" ) and die;
 sub abort {
@@ -21,21 +19,23 @@ sub abort {
     die $_[0];
 }
 
-for my $curve (@curves) {
+for my $kex (@kexes) {
     system( "cp $config_h.bak $config_h" ) and die "$config_h not restored\n";
     system( "make clean" ) and die;
 
     print "\n******************************************\n";
-    print "* Testing without curve: $curve\n";
+    print "* Testing with key exchange: $kex\n";
     print "******************************************\n";
 
-    system( "scripts/config.pl unset $curve" )
-        and abort "Failed to disable $curve\n";
+    # full config with all key exchanges disabled except one
+    system( "scripts/config.pl full" ) and abort "Failed config full\n";
+    for my $k (@kexes) {
+        next if $k eq $kex;
+        system( "scripts/config.pl unset $k" )
+            and abort "Failed to disable $k\n";
+    }
 
-    system( "make lib" ) and abort "Failed to build lib: $curve\n";
-    system( "cd tests && make" ) and abort "Failed to build tests: $curve\n";
-    system( "make test" ) and abort "Failed test suite: $curve\n";
-
+    system( "make lib" ) and abort "Failed to build lib: $kex\n";
 }
 
 system( "mv $config_h.bak $config_h" ) and die "$config_h not restored\n";

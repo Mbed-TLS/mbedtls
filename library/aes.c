642ee198b6337a9a0f3b2b9e948188828490082f
Jussi Kivilinna <jussi.kivilinna@haltian.com>
2015-11-12 14:12:59 +0200

    Fix unused variable in AES selftest when CBC and CFB disabled
    This commit fixes following warning:
    
    > CC:  aes.c
    > aes.c: In function 'mbedtls_aes_self_test':
    > aes.c:1225:19: error: unused variable 'iv' [-Werror=unused-variable]
    >      unsigned char iv[16];
    >                    ^
    > cc1: all warnings being treated as errors

--- library/aes.c
+++ library/aes.c
@@ -1222,7 +1222,9 @@ int mbedtls_aes_self_test( int verbose )
     int ret = 0, i, j, u, v;
     unsigned char key[32];
     unsigned char buf[64];
+#if defined(MBEDTLS_CIPHER_MODE_CBC) || defined(MBEDTLS_CIPHER_MODE_CFB)
     unsigned char iv[16];
+#endif
 #if defined(MBEDTLS_CIPHER_MODE_CBC)
     unsigned char prv[16];
 #endif

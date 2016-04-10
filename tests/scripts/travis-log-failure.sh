6eaf3659eaa276828c125b33b212cdf5ddd6695a
Simon Butcher <simon.butcher@arm.com>
2016-04-10 15:11:27 +0100

    Fixes Travis post-mortem script dump following review
    Changes made:
     * Added copyright and project statement
     * Limited size of each file to dump to 1Mbyte
     * Changed name of script

--- tests/scripts/travis-log-failure.sh
+++ tests/scripts/travis-log-failure.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+# travis-log-failure.sh
+#
+# This file is part of mbed TLS (https://tls.mbed.org)
+#
+# Copyright (c) 2016, ARM Limited, All Rights Reserved
+#
+# Purpose
+#
 # List the server and client logs on failed ssl-opt.sh and compat.sh tests.
 # This script is used to make the logs show up in the Travis test results.
-
+#
 # Some of the logs can be very long: this means usually a couple of megabytes
-# but it can bee much more. For example, the client log of test 273 in ssl-opt.sh
+# but it can be much more. For example, the client log of test 273 in ssl-opt.sh
 # is more than 630 Megabytes long.
 
 if [ -d include/mbedtls ]; then :; else
@@ -13,13 +21,14 @@ if [ -d include/mbedtls ]; then :; else
 fi
 
 FILES="o-srv-*.log o-cli-*.log c-srv-*.log c-cli-*.log o-pxy-*.log"
+MAX_LOG_SIZE=1048576
 
 for PATTERN in $FILES; do
     for LOG in $( ls tests/$PATTERN 2>/dev/null ); do
         echo
         echo "****** BEGIN file: $LOG ******"
         echo
-        cat $LOG
+        tail -c $MAX_LOG_SIZE $LOG
         echo "****** END file: $LOG ******"
         echo
         rm $LOG

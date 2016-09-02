879e62697e2da35b1517b733cf9b2e8318b8d98e
Andres AG <andres.amayagarcia@arm.com>
2016-09-02 14:06:04 +0100

    Allow the entry_name size to be set in config.h
    Allow the size of the entry_name character array in x509_crt.c to be
    configurable through a macro in config.h. entry_name holds a
    path/filename string. The macro introduced in
    MBEDTLS_X509_MAX_FILE_PATH_LEN.

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -2492,6 +2492,7 @@
 
 /* X509 options */
 //#define MBEDTLS_X509_MAX_INTERMEDIATE_CA   8   /**< Maximum number of intermediate CAs in a verification chain. */
+//#define MBEDTLS_X509_MAX_FILE_PATH_LEN     512 /**< Maximum length of a path/filename string in bytes including the null terminator character ('\0'). */
 
 /**
  * Allow SHA-1 in the default TLS configuration for certificate signing.

67ae0b983992c8d3e6f348670736e3a292984257
Andres AG <andres.amayagarcia@arm.com>
2016-09-19 16:58:45 +0100

    Fix sig->tag update in mbedtls_x509_get_sig()

--- library/x509.c
+++ library/x509.c
@@ -559,16 +559,18 @@ int x509_get_sig( unsigned char **p, const unsigned char *end, x509_buf *sig )
 {
     int ret;
     size_t len;
+    int tag_type;
 
     if( ( end - *p ) < 1 )
         return( POLARSSL_ERR_X509_INVALID_SIGNATURE +
                 POLARSSL_ERR_ASN1_OUT_OF_DATA );
 
-    sig->tag = **p;
+    tag_type = **p;
 
     if( ( ret = asn1_get_bitstring_null( p, end, &len ) ) != 0 )
         return( POLARSSL_ERR_X509_INVALID_SIGNATURE + ret );
 
+    sig->tag = tag_type;
     sig->len = len;
     sig->p = *p;
 

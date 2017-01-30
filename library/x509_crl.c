8ad5acd6da039ba4eb2f43da8cf1b07e6971135f
Andres AG <andres.amayagarcia@arm.com>
2017-01-30 14:34:25 +0000

    Fix corner case uses of memory_buffer_alloc.c
    The corner cases fixed include:
        * Allocating a buffer of size 0. With this change, the allocator now
          returns a NULL pointer in this case. Note that changes in pem.c and
          x509_crl.c were required to fix tests that did not work under this
          assumption.
        * Initialising the allocator with less memory than required for headers.
        * Fix header chain checks for uninitialised allocator.

--- library/x509_crl.c
+++ library/x509_crl.c
@@ -257,7 +257,7 @@ int mbedtls_x509_crl_parse_der( mbedtls_x509_crl *chain,
 {
     int ret;
     size_t len;
-    unsigned char *p, *end;
+    unsigned char *p = NULL, *end;
     mbedtls_x509_buf sig_params1, sig_params2, sig_oid2;
     mbedtls_x509_crl *crl = chain;
 
@@ -294,7 +294,7 @@ int mbedtls_x509_crl_parse_der( mbedtls_x509_crl *chain,
     /*
      * Copy raw DER-encoded CRL
      */
-    if( ( p = mbedtls_calloc( 1, buflen ) ) == NULL )
+    if( buflen != 0 && ( p = mbedtls_calloc( 1, buflen ) ) == NULL )
         return( MBEDTLS_ERR_X509_ALLOC_FAILED );
 
     memcpy( p, buf, buflen );

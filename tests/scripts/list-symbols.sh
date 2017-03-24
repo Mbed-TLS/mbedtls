fca45ba00de209b26eea4c8a19ab17c97df9d3b7
Pawel Winogrodzki <pawelwi@microsoft.com>
2017-03-24 12:59:15 -0700

    Temporary build check.

--- tests/scripts/list-symbols.sh
+++ tests/scripts/list-symbols.sh
@@ -14,8 +14,8 @@ fi
 
 cp include/mbedtls/config.h include/mbedtls/config.h.bak
 scripts/config.pl full
-CFLAGS=-fno-asynchronous-unwind-tables make clean lib >/dev/null 2>&1
 mv include/mbedtls/config.h.bak include/mbedtls/config.h
+CFLAGS=-fno-asynchronous-unwind-tables make clean lib
 if uname | grep -F Darwin >/dev/null; then
     nm -gUj library/libmbed*.a 2>/dev/null | sed -n -e 's/^_//p'
 elif uname | grep -F Linux >/dev/null; then

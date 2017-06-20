f2a597fa3dd1c7b15e0fee62f6932b253295803d
Simon Butcher <simon.butcher@arm.com>
2017-06-20 23:08:10 +0100

    Update the version number to 2.5.1

--- include/mbedtls/version.h
+++ include/mbedtls/version.h
@@ -39,16 +39,16 @@
  */
 #define MBEDTLS_VERSION_MAJOR  2
 #define MBEDTLS_VERSION_MINOR  5
-#define MBEDTLS_VERSION_PATCH  0
+#define MBEDTLS_VERSION_PATCH  1
 
 /**
  * The single version number has the following structure:
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define MBEDTLS_VERSION_NUMBER         0x02050000
-#define MBEDTLS_VERSION_STRING         "2.5.0"
-#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.5.0"
+#define MBEDTLS_VERSION_NUMBER         0x02050100
+#define MBEDTLS_VERSION_STRING         "2.5.1"
+#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.5.1"
 
 #if defined(MBEDTLS_VERSION_C)
 

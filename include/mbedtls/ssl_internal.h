f2e16336d56697db29eb7c7dda338f73629977b6
Hanno Becker <hanno.becker@arm.com>
2017-06-26 13:26:58 +0100

    Prevent clever optimization to prematurely quit loop in safe memcmp
    The previous version of `mbedtls_ssl_safer_memcmp` did not qualify the
    pointers to the arrays to be compared as volatile, theoretically
    opening the possibility for the compiler to notice that the loop
    operation `diff |= A[i] ^ B[i]` is pointless if `diff = -1`. This
    commit changes this. It also declares the stack variable `diff` as
    volatile, to force read and write in every loop; omitting that, the
    compiler would still be allowed to get away with reading `A[i]` and
    `B[i]` but not doing the XOR and not updating `diff`.

--- include/mbedtls/ssl_internal.h
+++ include/mbedtls/ssl_internal.h
@@ -584,9 +584,9 @@ void mbedtls_ssl_dtls_replay_update( mbedtls_ssl_context *ssl );
 static inline int mbedtls_ssl_safer_memcmp( const void *a, const void *b, size_t n )
 {
     size_t i;
-    const unsigned char *A = (const unsigned char *) a;
-    const unsigned char *B = (const unsigned char *) b;
-    unsigned char diff = 0;
+    volatile const unsigned char *A = (volatile const unsigned char *) a;
+    volatile const unsigned char *B = (volatile const unsigned char *) b;
+    volatile unsigned char diff = 0;
 
     for( i = 0; i < n; i++ )
         diff |= A[i] ^ B[i];

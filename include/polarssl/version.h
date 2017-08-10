03bd0229ab672234507d97b7b0cf4b164cf445b3
Simon Butcher <simon.butcher@arm.com>
2017-08-10 11:52:14 +0100

    Update version to 1.3.21

--- include/polarssl/version.h
+++ include/polarssl/version.h
@@ -40,16 +40,16 @@
  */
 #define POLARSSL_VERSION_MAJOR  1
 #define POLARSSL_VERSION_MINOR  3
-#define POLARSSL_VERSION_PATCH  20
+#define POLARSSL_VERSION_PATCH  21
 
 /**
  * The single version number has the following structure:
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define POLARSSL_VERSION_NUMBER         0x01031400
-#define POLARSSL_VERSION_STRING         "1.3.20"
-#define POLARSSL_VERSION_STRING_FULL    "mbed TLS 1.3.20"
+#define POLARSSL_VERSION_NUMBER         0x01031500
+#define POLARSSL_VERSION_STRING         "1.3.21"
+#define POLARSSL_VERSION_STRING_FULL    "mbed TLS 1.3.21"
 
 #if defined(POLARSSL_VERSION_C)
 

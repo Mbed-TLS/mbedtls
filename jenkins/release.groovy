734a99f6c8665ebaf71eec86dec2d514d625c486
Mohammad Azim Khan <Azim.Khan@ARM.com>
2017-08-17 16:50:35 +0100

    Code coverage statistics job

--- jenkins/release.groovy
+++ jenkins/release.groovy
@@ -1,6 +1,20 @@
 
-def dispatch_job() {
-    echo "This is the release job!"
+/* Create code coverage job */
+def get_code_coverage_job(){
+    return {
+        node('linux') {
+            checkout scm
+            sh "./tests/scripts/basic-build-test.sh"
+        }
+    }
 }
 
+/* Jenkinsfile interface to this script. */
+def dispatch_job() {
+    parallel_jobs = []
+    parallel_jobs['code_coverage'] = get_code_coverage_job();
+    parallel parallel_jobs
+}
+
+/* Required for load statement in Jenkinsfile */
 return this

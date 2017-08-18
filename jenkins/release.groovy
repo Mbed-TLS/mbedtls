a22a62ee35ecc7a604a3e4f340076a1ea4b271c2
Mohammad Azim Khan <Azim.Khan@ARM.com>
2017-08-18 10:36:15 +0100

    stash source code for all sub jobs

--- jenkins/release.groovy
+++ jenkins/release.groovy
@@ -3,7 +3,7 @@
 def get_code_coverage_job(){
     return {
         node('linux') {
-            checkout scm
+            unstash 'src'
             sh "./tests/scripts/basic-build-test.sh"
         }
     }
@@ -12,6 +12,12 @@ def get_code_coverage_job(){
 /* Jenkinsfile interface to this script. */
 def dispatch_job() {
     parallel_jobs = [:]
+    parallel_jobs['scm'] = {
+        node{
+            checkout scm
+            stash includes: '*', name: 'src'
+        }
+    }
     parallel_jobs['code_coverage'] = get_code_coverage_job();
     parallel parallel_jobs
 }

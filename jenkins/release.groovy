df092f63a13d5346904772a7c525d1879ff29a4a
Mohammad Azim Khan <Azim.Khan@ARM.com>
2017-08-22 11:32:31 +0100

    Add mbed-os examples testing

--- jenkins/release.groovy
+++ jenkins/release.groovy
@@ -9,10 +9,97 @@ def get_code_coverage_job(){
     }
 }
 
+def get_all_sh_job(){
+    return {
+        node('linux') {
+            unstash 'src'
+            sh "./tests/scripts/all.sh"
+        }
+    }
+}
+
+def find_examples (){
+    examples = []
+    File[] files = new File(pwd()).listFiles();
+    for (File file: files){
+        if (file.isDirectory()) {
+            File[] subfiles = file.listFiles();
+            for (File subfile: subfiles){
+                if (subfile.getName().equals("mbed-os.lib")) {
+                    examples << file.getName()
+                }
+            }
+        }
+    }
+    return examples;
+}
+
+
+def checkout_mbed_os_examples(){
+    def examples = find_examples()
+    dir('examples'){
+        git 'git@github.com:ARMmbed/mbed-os-example-tls.git'
+        stash 'examples_src'
+        /* checkout mbed-os */
+        def oneexample = examples[0]
+        if ( oneexample != null ) {
+            dir( oneexample ){
+                git 'git@github.com:ARMmbed/mbed-os.git'
+                sh '''
+sha=`cut -d "#" -f 2 ../mbed-os.lib`
+git reset --hard $sha
+'''
+                dir('mbed-os'){
+                    /* Deploy mbedtls src into mbed-os */
+                    dir('features/mbedtls/importer') {
+                        dir('TARGET_IGNORE/mbedtls'){
+                            unstash('src')
+                        }
+                        sh 'make all'
+                    }
+                    stash 'mbed-os_src'
+                }
+            }
+        }
+    }
+    return examples
+}
+
+def gen_mbed_os_example_job( example, compiler, platform ){
+    return {
+       node( compiler ){
+            timestamps {
+                deleteDir()
+                unstash "examples_src"
+                dir( example ){
+                    unstash "mbed-os_src"
+                    sh """
+mbed config root .
+mbed compile -m ${platform} -t ${toolchain}
+"""
+                }
+            }
+       }
+   }
+}
+
+compilers = ['ARM', 'GCC_ARM', 'IAR']
+platforms = ['K64F']
+
 /* Jenkinsfile interface to this script. */
 def dispatch_job() {
+    /* Checkout mbed-os-example-tls */
     parallel_jobs = [:]
     parallel_jobs['code_coverage'] = get_code_coverage_job();
+    example = checkout_mbed_os_examples()
+    for( example in examples ) {
+        for( compiler in compilers ) {
+            for( platform in platforms ) {
+                parallel_jobs["${example}-${platform}-${compiler}"] =
+                    gen_mbed_os_example_job(example, compiler, platform)
+            }
+        }
+    }
     parallel parallel_jobs
 }
 

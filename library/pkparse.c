7b2e85dd7ccb4c253df76ac0517841874bf72e17
Sanne Wouda <sanne.wouda@arm.com>
2017-08-30 21:10:42 +0100

    Use both applicable error codes and a proper coding style

--- library/pkparse.c
+++ library/pkparse.c
@@ -182,7 +182,8 @@ static int pk_get_ecparams( unsigned char **p, const unsigned char *end,
     int ret;
 
     if ( end - *p < 1 )
-        return MBEDTLS_ERR_ASN1_OUT_OF_DATA;
+        return( MBEDTLS_ERR_PK_KEY_INVALID_FORMAT +
+                MBEDTLS_ERR_ASN1_OUT_OF_DATA );
 
     /* Tag may be either OID or SEQUENCE */
     params->tag = **p;

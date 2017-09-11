a4d0ca79fe4253e7171ea79c591eb1a5686ec782
Hannes Tschofenig <Hannes.Tschofenig@arm.com>
2017-09-11 10:40:46 +0200

    Enhancing the output of the ssl_client2 file so that the test programs recognize a successful protocol run.

--- programs/ssl/ssl_client2.c
+++ programs/ssl/ssl_client2.c
@@ -445,7 +445,7 @@ static int ssl_sig_hashes_for_test[] = {
 
 int main( int argc, char *argv[] )
 {
-    int ret = 0, len, tail_len, i, written, frags, retry_left;
+    int ret = 0, len, tail_len, i, written, read, frags, retry_left;
     mbedtls_net_context server_fd;
     unsigned char buf[MBEDTLS_SSL_MAX_CONTENT_LEN + 1];
 #if defined(MBEDTLS_KEY_EXCHANGE__SOME__PSK_ENABLED)
@@ -1585,9 +1585,8 @@ send_request:
     /*
      * 7. Read the HTTP response
      */
-    mbedtls_printf( "  < Read from server:" );
-    fflush( stdout );
 
+	read = 0; 
     do
     {
         len = sizeof( buf ) - 1;
@@ -1625,9 +1624,10 @@ send_request:
         }
 
         len = ret;
+		read += len; 
         buf[len] = '\0';
 		mbedtls_printf("%s", (char *)buf);
-
+		
         /* End of message should be detected according to the syntax of the
          * application protocol (eg HTTP), just use a dummy test here. */
         if( ret > 0 && buf[len-1] == '\n' )
@@ -1638,6 +1638,8 @@ send_request:
     }
     while( 1 );
 
+	mbedtls_printf("  < Read from server:  %d bytes read",read);
+	fflush(stdout);
     /*
      * 7b. Simulate hard reset and reconnect from same port?
      */

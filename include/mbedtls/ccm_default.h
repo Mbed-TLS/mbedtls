80646e51932de4d6bd0e47bd072fa56b1764299f
Ron Eldor <Ron.Eldor@arm.com>
2017-09-25 13:53:24 +0300

    Update copyright year
    Update the copyright year of the new file

--- include/mbedtls/ccm_default.h
+++ include/mbedtls/ccm_default.h
@@ -8,7 +8,7 @@
  *       is required. Application developers should call the
  *       base function instead (without the _default suffix).
  *
- *  Copyright (C) 2006-2015, ARM Limited, All Rights Reserved
+ *  Copyright (C) 2017, ARM Limited, All Rights Reserved
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may

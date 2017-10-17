580869dae8d27198e5c37f7484a73f3f0cdd091b
Hanno Becker <hanno.becker@arm.com>
2017-10-17 10:29:18 +0100

    Handle RSA_EXPORT_UNSUPPORTED error code in strerror

--- library/error.c
+++ library/error.c
@@ -331,6 +331,8 @@ void mbedtls_strerror( int ret, char *buf, size_t buflen )
             mbedtls_snprintf( buf, buflen, "RSA - The output buffer for decryption is not large enough" );
         if( use_ret == -(MBEDTLS_ERR_RSA_RNG_FAILED) )
             mbedtls_snprintf( buf, buflen, "RSA - The random generator failed to generate non-zeros" );
+        if( use_ret == -(MBEDTLS_ERR_RSA_EXPORT_UNSUPPORTED) )
+            mbedtls_snprintf( buf, buflen, "RSA - The requested parameter export is not possible/allowed" );
 #endif /* MBEDTLS_RSA_C */
 
 #if defined(MBEDTLS_SSL_TLS_C)

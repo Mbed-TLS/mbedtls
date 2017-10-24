614d9c06677dfac460e60208c8e7fefcbacd9505
Andres Amaya Garcia <andres.amayagarcia@arm.com>
2017-10-24 21:27:43 +0100

    Add a utils.h file that contains common functions
    The new header contains common information across various mbed TLS
    modules and avoids code duplication. To start, utils.h currently only
    contains the mbedtls_zeroize() function.

---
+++ include/mbedtls/utils.h
@@ -0,0 +1,39 @@
+/**
+ * \file utils.h
+ *
+ * \brief mbed TLS utility functions
+ *
+ *  Copyright (C) 2017, ARM Limited, All Rights Reserved
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *  not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  This file is part of mbed TLS (https://tls.mbed.org)
+ */
+#ifndef MBEDTLS_UTILS_H
+#define MBEDTLS_UTILS_H
+
+#include <stddef.h>
+
+/**
+ * \brief       Securely zeroize a buffer
+ *
+ * \param buf   Buffer to be zeroized
+ * \param len   Length of the buffer in bytes
+ *
+ * \note        This implementation should never be optimized out by the
+ *              compiler
+ */
+void mbedtls_zeroize( void *buf, size_t len );
+
+#endif /* MBEDTLS_UTILS_H */

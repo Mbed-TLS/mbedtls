2412061a5a55410e8fffc583b3ce3a2f0dfc067d
Hanno Becker <hanno.becker@arm.com>
2017-10-26 11:53:35 +0100

    Correct typo and improve documentation

--- include/mbedtls/rsa.h
+++ include/mbedtls/rsa.h
@@ -230,11 +230,11 @@ int mbedtls_rsa_public( mbedtls_rsa_context *ctx,
  * \note           The input and output buffers must be large
  *                 enough (eg. 128 bytes if RSA-1024 is used).
  *
- * \note           Blinding is used if and onlf if a PRNG is provided.
+ * \note           Blinding is used if and only if a PRNG is provided.
  *
  * \note           If blinding is used, both the base of exponentation
- *                 and the exponent are blinded, preventing both statistical
- *                 timing and power analysis attacks.
+ *                 and the exponent are blinded, providing protection
+ *                 against some side-channel attacks.
  *
  * \warning        It is deprecated and a security risk to not provide
  *                 a PRNG here and thereby prevent the use of blinding.

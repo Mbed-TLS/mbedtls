52de8e01a0c9700c39893c470fdd6b98da69f740
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2017-10-26 09:47:36 +0200

    Document choice of script exit code

--- tests/scripts/test-ref-configs.pl
+++ tests/scripts/test-ref-configs.pl
@@ -43,6 +43,7 @@ my $config_h = 'include/mbedtls/config.h';
 system( "cp $config_h $config_h.bak" ) and die;
 sub abort {
     system( "mv $config_h.bak $config_h" ) and warn "$config_h not restored\n";
+    # use an exit code between 1 and 124 for git bisect (die returns 255)
     warn $_[0];
     exit 1;
 }

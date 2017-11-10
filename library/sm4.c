dd829baed5dd6228415bcf665972880f3cd4d3d8
Markku-Juhani O. Saarinen <mjos@mjos.fi>
2017-11-10 09:16:56 +0000

    Updated copyright headers

--- library/sm4.c
+++ library/sm4.c
@@ -1,7 +1,7 @@
 /*
  *  SM4 Implementation
  *
- *  Copyright (C) 2017, ARM Limited, All Rights Reserved
+ *  Copyright (C) 2006-2017, ARM Limited, All Rights Reserved
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may

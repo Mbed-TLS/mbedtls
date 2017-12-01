6fca15171f3a8f5fb35fe7498e6c75f3b47cc8c6
Markku-Juhani O. Saarinen <mjos@mjos.fi>
2017-12-01 15:41:38 +0000

    MBEDTLS_ARIA_ALT added as a feature

--- library/version_features.c
+++ library/version_features.c
@@ -123,6 +123,9 @@ static const char *features[] = {
 #if defined(MBEDTLS_SHA512_ALT)
     "MBEDTLS_SHA512_ALT",
 #endif /* MBEDTLS_SHA512_ALT */
+#if defined(MBEDTLS_ARIA_ALT)
+    "MBEDTLS_ARIA_ALT",
+#endif /* MBEDTLS_ARIA_ALT */
 #if defined(MBEDTLS_ECP_ALT)
     "MBEDTLS_ECP_ALT",
 #endif /* MBEDTLS_ECP_ALT */

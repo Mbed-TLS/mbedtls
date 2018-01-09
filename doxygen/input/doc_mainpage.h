e737fb1abeb97d497b0ad9b3dbe71e1d100ef263
Jaeden Amero <jaeden.amero@arm.com>
2018-01-09 16:24:57 +0000

    Update version to 2.1.10

--- doxygen/input/doc_mainpage.h
+++ doxygen/input/doc_mainpage.h
@@ -21,7 +21,7 @@
  */
 
 /**
- * @mainpage mbed TLS v2.1.9 source code documentation
+ * @mainpage mbed TLS v2.1.10 source code documentation
  *
  * This documentation describes the internal structure of mbed TLS.  It was
  * automatically generated from specially formatted comment blocks in

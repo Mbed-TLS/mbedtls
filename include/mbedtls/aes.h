1b708e5f406508382afb2e789e75f42aa83d3ca3
Rose Zadik <rose.zadik@arm.com>
2018-01-29 11:32:38 +0000

    Update aes.h
    Removed tabs

--- include/mbedtls/aes.h
+++ include/mbedtls/aes.h
@@ -74,13 +74,13 @@ typedef struct
     int nr;                     /*!< The number of rounds. */
     uint32_t *rk;               /*!< AES round keys. */
     uint32_t buf[68];           /*!< Unaligned data buffer. This buffer can
-	                                 hold 32 extra Bytes, which can be used for
-									 one of the following purposes:
+                                     hold 32 extra Bytes, which can be used for
+                                     one of the following purposes:
                                      <ul><li>Alignment if VIA padlock is 
-									         used.</li>
-									 <li>Simplifying key expansion in the 256-bit
-									     case by generating an extra round key.
-										 </li></ul> */
+                                             used.</li>
+                                     <li>Simplifying key expansion in the 256-bit
+                                         case by generating an extra round key.
+                                         </li></ul> */
 }
 mbedtls_aes_context;
 

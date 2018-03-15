0211c32c9af554a816401efa14064f058bd5aeb1
Andrzej Kurek <andrzej.kurek@arm.com>
2018-03-15 05:16:24 -0400

    Change the cpp test to be optional
    Remove unnecessary defines from the test.
    Test by defining TEST_CPP using makefiles or cmake.

--- programs/test/header_test.cpp
+++ programs/test/header_test.cpp
@@ -102,14 +102,6 @@
 
 #if defined(MBEDTLS_PLATFORM_C)
 #include "mbedtls/platform.h"
-#else
-#include <stdio.h>
-#include <stdlib.h>
-#define mbedtls_printf     printf
-#define mbedtls_snprintf   snprintf
-#define mbedtls_exit       exit
-#define MBEDTLS_EXIT_SUCCESS EXIT_SUCCESS
-#define MBEDTLS_EXIT_FAILURE EXIT_FAILURE
 #endif
 
 #if defined(MBEDTLS_MEMORY_BUFFER_ALLOC_C)

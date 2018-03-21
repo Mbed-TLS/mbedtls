19ceb7104ca4e23f3f6f2ed60a118d6fc3113090
Gilles Peskine <Gilles.Peskine@arm.com>
2018-03-21 08:40:26 +0100

    all.sh: add opposites to all boolean options
    All options can now be overridden by a subsequent option, e.g.
    "all.sh --foo --no-foo" is equivalent to "all.sh --no-foo". This
    allows making wrapper scripts with default options and occasionally
    overriding those options when running the wrapper script.

--- tests/scripts/all.sh
+++ tests/scripts/all.sh
@@ -94,7 +94,6 @@ CONFIG_BAK="$CONFIG_H.bak"
 MEMORY=0
 FORCE=0
 KEEP_GOING=0
-RELEASE=0
 RUN_ARMCC=1
 YOTTA=1
 
@@ -126,8 +125,12 @@ General options:
   -m|--memory           Additional optional memory tests.
      --armcc            Run ARM Compiler builds (on by default).
      --no-armcc         Skip ARM Compiler builds.
+     --no-force         Refuse to overwrite modified files (default).
+     --no-keep-going    Stop at the first error (default).
+     --no-memory        No additional memory tests (default).
      --no-yotta         Skip yotta module build.
      --out-of-source-dir=<path>  Directory used for CMake out-of-source build tests.
+     --random-seed      Use a random seed value for randomized tests (default).
   -r|--release-test     Run this script in release mode. This fixes the seed value to 1.
   -s|--seed             Integer seed value to use for this test run.
      --yotta            Build yotta module (on by default).
@@ -226,11 +229,15 @@ while [ $# -gt 0 ]; do
         --keep-going|-k) KEEP_GOING=1;;
         --memory|-m) MEMORY=1;;
         --no-armcc) RUN_ARMCC=0;;
+        --no-force) FORCE=0;;
+        --no-keep-going) KEEP_GOING=0;;
+        --no-memory) MEMORY=0;;
         --no-yotta) YOTTA=0;;
         --openssl) shift; OPENSSL="$1";;
         --openssl-legacy) shift; OPENSSL_LEGACY="$1";;
         --out-of-source-dir) shift; OUT_OF_SOURCE_DIR="$1";;
-        --release-test|-r) RELEASE=1;;
+        --random-seed) unset SEED;;
+        --release-test|-r) SEED=1;;
         --seed|-s) shift; SEED="$1";;
         --yotta) YOTTA=1;;
         *)
@@ -337,11 +344,6 @@ if_build_succeeded () {
     fi
 }
 
-if [ $RELEASE -eq 1 ]; then
-    # Fix the seed value to 1 to ensure that the tests are deterministic.
-    SEED=1
-fi
-
 msg "info: $0 configuration"
 echo "MEMORY: $MEMORY"
 echo "FORCE: $FORCE"
@@ -367,7 +369,9 @@ export GNUTLS_CLI="$GNUTLS_CLI"
 export GNUTLS_SERV="$GNUTLS_SERV"
 
 # Avoid passing --seed flag in every call to ssl-opt.sh
-[ ! -z ${SEED+set} ] && export SEED
+if [ -n "${SEED-}" ]; then
+  export SEED
+fi
 
 # Make sure the tools we need are available.
 check_tools "$OPENSSL" "$OPENSSL_LEGACY" "$GNUTLS_CLI" "$GNUTLS_SERV" \

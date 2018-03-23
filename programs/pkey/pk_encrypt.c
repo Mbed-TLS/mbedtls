0dc3b9c1b58225e941e3458c2221ef1f408b4709
Azim Khan <Azim.Khan@arm.com>
2018-03-23 18:27:08 +0000

    Use Windows version of snprintf on Windows

--- programs/pkey/pk_encrypt.c
+++ programs/pkey/pk_encrypt.c
@@ -144,8 +144,8 @@ int main( int argc, char *argv[] )
     {
         int len = 0;
         char write_buf[5];
-        len = mbedtls_snprintf( write_buf, sizeof( write_buf ), "%02X%s", buf[i],
-                 ( i + 1 ) % 16 == 0 ? "\r\n" : " " );
+        len = mbedtls_snprintf( write_buf, sizeof( write_buf ),
+                "%02X%s", buf[i], ( i + 1 ) % 16 == 0 ? "\r\n" : " " );
         mbedtls_fwrite(write_buf, len, f);
     }
 

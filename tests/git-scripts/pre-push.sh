051cfd1f92eee9aca9acee985d0f010d56cf1391
Ron Eldor <Ron.Eldor@arm.com>
2018-04-01 17:43:26 +0300

    Add doxygen.sh script to git hooks
    Add the doxygen.sh script to the pre-push git script

--- tests/git-scripts/pre-push.sh
+++ tests/git-scripts/pre-push.sh
@@ -46,3 +46,4 @@ run_test ./tests/scripts/check-doxy-blocks.pl
 run_test ./tests/scripts/check-names.sh
 run_test ./tests/scripts/check-generated-files.sh
 run_test ./tests/scripts/check-files.py
+run_test ./tests/scripts/doxygen.sh

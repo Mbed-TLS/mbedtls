df041403902b1fa6158a13cd3bb4af4cb13015c6
Andrzej Kurek <andrzej.kurek@arm.com>
2018-04-25 10:58:19 +0100

    Adjust the rest of programs return codes on platform initialization failure

--- programs/x509/cert_req.c
+++ programs/x509/cert_req.c
@@ -175,7 +175,7 @@ int main( int argc, char *argv[] )
 #if defined(MBEDTLS_PLATFORM_C)
     if( ( ret = mbedtls_platform_setup( &platform_ctx ) ) != 0 )
     {
-        mbedtls_printf( " failed\n  !  mbedtls_platform_setup returned %d", ret );
+        mbedtls_printf( " failed\n  !  mbedtls_platform_setup returned %d\n\n", -ret );
         return( 1 );
     }
 #endif

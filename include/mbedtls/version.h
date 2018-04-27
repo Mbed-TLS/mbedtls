6c0fba43505ed9cfdeab777d34d2d9ec53560e47
Jaeden Amero <jaeden.amero@arm.com>
2018-04-27 13:13:54 +0100

    Update version to 2.1.12

--- include/mbedtls/version.h
+++ include/mbedtls/version.h
@@ -39,16 +39,16 @@
  */
 #define MBEDTLS_VERSION_MAJOR  2
 #define MBEDTLS_VERSION_MINOR  1
-#define MBEDTLS_VERSION_PATCH  11
+#define MBEDTLS_VERSION_PATCH  12
 
 /**
  * The single version number has the following structure:
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define MBEDTLS_VERSION_NUMBER         0x02010B00
-#define MBEDTLS_VERSION_STRING         "2.1.11"
-#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.1.11"
+#define MBEDTLS_VERSION_NUMBER         0x02010C00
+#define MBEDTLS_VERSION_STRING         "2.1.12"
+#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.1.12"
 
 #if defined(MBEDTLS_VERSION_C)
 

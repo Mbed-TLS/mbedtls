f39d8349a0476a1e11c5bba26b1e668b50ac1017
Hanno Becker <hanno.becker@arm.com>
2018-05-16 15:05:01 +0100

    Add tracing support
    This commit adds a small piece of tracing code which allows to print
    debugging messages from different sources with different colors and
    different nesting according to the depth of function calls.
    
    This significantly simplifies the debugging of MPS as it allows
    to visually set apart the messages from different Layers.
    
    The file `trace.h` contains the following macros:
    (1) TRACE_INIT( fmt, ... )
        This macro should be called at the beginning of a
        function which uses the tracing capability.
        It prints the specified message (which uses the same
        formatting as for `printf`) and increases the indentation
        of all subsequent TRACE calls within this function.
    
    (2) RETURN( retval )
        Return from a function that uses tracing, and
        decrease the indentation level.
    
    (3) TRACE( type, fmt, ... )
        Print message of a given type. The supported types
        are currently `trace_comment` and `trace_error`.

---
+++ include/mbedtls/mps/trace.h
@@ -0,0 +1,113 @@
+
+#ifndef MBEDTLS_MPS_TRACE_H
+#define MBEDTLS_MPS_TRACE_H
+
+/*
+ * Adapt this to enable/disable tracing output
+ * from the various layers of the MPS.
+ */
+
+#define TRACE_ENABLE_LAYER_1
+#define TRACE_ENABLE_LAYER_2
+#define TRACE_ENABLE_LAYER_3
+#define TRACE_ENABLE_READER
+#define TRACE_ENABLE_WRITER
+
+/*
+ * This shouldn't be touched.
+ */
+
+__attribute__((unused)) static int trace_id;
+
+typedef enum
+{
+    trace_comment=0,
+    trace_call,
+    trace_error
+} trace_type;
+
+#define TRACE_ID_LAYER_1 1
+#define TRACE_ID_LAYER_2 2
+#define TRACE_ID_LAYER_3 3
+#define TRACE_ID_WRITER  4
+#define TRACE_ID_READER  5
+
+#if defined(TRACE_ENABLE_LAYER_1)
+#define TRACE_MASK_LAYER_1 (1u << TRACE_ID_LAYER_1 )
+#else
+#define TRACE_MASK_LAYER_1 0
+#endif
+
+#if defined(TRACE_ENABLE_LAYER_2)
+#define TRACE_MASK_LAYER_2 (1u << TRACE_ID_LAYER_2 )
+#else
+#define TRACE_MASK_LAYER_2 0
+#endif
+
+#if defined(TRACE_ENABLE_LAYER_3)
+#define TRACE_MASK_LAYER_3 (1u << TRACE_ID_LAYER_3 )
+#else
+#define TRACE_MASK_LAYER_3 0
+#endif
+
+#if defined(TRACE_ENABLE_READER)
+#define TRACE_MASK_READER (1u << TRACE_ID_READER )
+#else
+#define TRACE_MASK_READER 0
+#endif
+
+#if defined(TRACE_ENABLE_WRITER)
+#define TRACE_MASK_WRITER (1u << TRACE_ID_WRITER )
+#else
+#define TRACE_MASK_WRITER 0
+#endif
+
+#define TRACE_MASK ( TRACE_MASK_LAYER_1 |           \
+                     TRACE_MASK_LAYER_2 |           \
+                     TRACE_MASK_LAYER_3 |           \
+                     TRACE_MASK_READER  |           \
+                     TRACE_MASK_WRITER )
+
+/* We have to avoid globals because E-ACSL chokes on them...
+ * Wrap everything in stub functions. */
+int get_trace_depth( void );
+void inc_trace_depth( void );
+void dec_trace_depth( void );
+
+void trace_color( int id );
+void trace_indent( int level, trace_type ty );
+
+#define TRACE( type, fmt, ... )                                         \
+    do {                                                                \
+        if( ! ( TRACE_MASK & ( 1u << trace_id ) ) )                     \
+            break;                                                      \
+        trace_indent( get_trace_depth(), type );                        \
+        trace_color( trace_id );                                        \
+        printf( "[%d|L%u]: " fmt "\n", trace_id, __LINE__, ##__VA_ARGS__); \
+        trace_color( 0 );                                               \
+    } while( 0 )
+
+#define TRACE_INIT( fmt, ... )                                          \
+    do {                                                                \
+        if( ! ( TRACE_MASK & ( 1u << trace_id ) ) )                     \
+            break;                                                      \
+        TRACE( trace_call, fmt, ##__VA_ARGS__ );                        \
+        inc_trace_depth();                                              \
+    } while( 0 )
+
+#define TRACE_END()                                                     \
+    do {                                                                \
+        if( ! ( TRACE_MASK & ( 1u << trace_id ) ) )                     \
+            break;                                                      \
+        dec_trace_depth();                                              \
+    } while( 0 )
+
+#define RETURN( val )                           \
+    do {                                        \
+        /* Breaks tail recursion. */            \
+        int ret__ = val;                        \
+        TRACE_END();                            \
+        return( ret__ );                        \
+    } while( 0 )
+
+#endif /* MBEDTLS_MPS_TRACE_H */

e55a556f4eb9c5c8f0f767450acca4045491469d
Hanno Becker <hanno.becker@arm.com>
2018-05-22 11:30:11 +0100

    Restructure outgoing CrtReq: Writing 2/2, comment old code

--- library/ssl_srv.c
+++ library/ssl_srv.c
@@ -3200,113 +3200,98 @@ static int ssl_write_certificate_request( mbedtls_ssl_context *ssl )
      *    n+4 .. ...  Distinguished Name #1
      *    ... .. ...  length of DN 2, etc.
      */
-    buf = ssl->out_msg;
-    p = buf + 4;
+/*     buf = ssl->out_msg; */
+/*     p = buf + 4; */
+/*     /\* */
+/*      * Supported certificate types */
+/*      * */
+/*      *     ClientCertificateType certificate_types<1..2^8-1>; */
+/*      *     enum { (255) } ClientCertificateType; */
+/*      *\/ */
+/*     ct_len = 0; */
+/* #if defined(MBEDTLS_RSA_C) */
+/*     p[1 + ct_len++] = MBEDTLS_SSL_CERT_TYPE_RSA_SIGN; */
+/* #endif */
+/* #if defined(MBEDTLS_ECDSA_C) */
+/*     p[1 + ct_len++] = MBEDTLS_SSL_CERT_TYPE_ECDSA_SIGN; */
+/* #endif */
+/*     p[0] = (unsigned char) ct_len++; */
+/*     p += ct_len; */
+/*     sa_len = 0; */
+/* #if defined(MBEDTLS_SSL_PROTO_TLS1_2) */
+/*     /\* */
+/*      * Add signature_algorithms for verify (TLS 1.2) */
+/*      * */
+/*      *     SignatureAndHashAlgorithm supported_signature_algorithms<2..2^16-2>; */
+/*      * */
+/*      *     struct { */
+/*      *           HashAlgorithm hash; */
+/*      *           SignatureAlgorithm signature; */
+/*      *     } SignatureAndHashAlgorithm; */
+/*      * */
+/*      *     enum { (255) } HashAlgorithm; */
+/*      *     enum { (255) } SignatureAlgorithm; */
+/*      *\/ */
+/*     if( ssl->minor_ver == MBEDTLS_SSL_MINOR_VERSION_3 ) */
+/*     { */
+/*         const int *cur; */
+/*         /\* */
+/*          * Supported signature algorithms */
+/*          *\/ */
+/*         for( cur = ssl->conf->sig_hashes; *cur != MBEDTLS_MD_NONE; cur++ ) */
+/*         { */
+/*             unsigned char hash = mbedtls_ssl_hash_from_md_alg( *cur ); */
+/*             if( MBEDTLS_SSL_HASH_NONE == hash || mbedtls_ssl_set_calc_verify_md( ssl, hash ) ) */
+/*                 continue; */
+/* #if defined(MBEDTLS_RSA_C) */
+/*             p[2 + sa_len++] = hash; */
+/*             p[2 + sa_len++] = MBEDTLS_SSL_SIG_RSA; */
+/* #endif */
+/* #if defined(MBEDTLS_ECDSA_C) */
+/*             p[2 + sa_len++] = hash; */
+/*             p[2 + sa_len++] = MBEDTLS_SSL_SIG_ECDSA; */
+/* #endif */
+/*         } */
+/*         p[0] = (unsigned char)( sa_len >> 8 ); */
+/*         p[1] = (unsigned char)( sa_len      ); */
+/*         sa_len += 2; */
+/*         p += sa_len; */
+/*     } */
+/* #endif /\* MBEDTLS_SSL_PROTO_TLS1_2 *\/ */
+/*     /\* */
+/*      * DistinguishedName certificate_authorities<0..2^16-1>; */
+/*      * opaque DistinguishedName<1..2^16-1>; */
+/*      *\/ */
+/*     p += 2; */
+/*     total_dn_size = 0; */
+/*     if( ssl->conf->cert_req_ca_list ==  MBEDTLS_SSL_CERT_REQ_CA_LIST_ENABLED ) */
+/*     { */
+/* #if defined(MBEDTLS_SSL_SERVER_NAME_INDICATION) */
+/*         if( ssl->handshake->sni_ca_chain != NULL ) */
+/*             crt = ssl->handshake->sni_ca_chain; */
+/*         else */
+/* #endif */
+/*             crt = ssl->conf->ca_chain; */
+/*         while( crt != NULL && crt->version != 0 ) */
+/*         { */
+/*             dn_size = crt->subject_raw.len; */
 
-    /*
-     * Supported certificate types
-     *
-     *     ClientCertificateType certificate_types<1..2^8-1>;
-     *     enum { (255) } ClientCertificateType;
-     */
-    ct_len = 0;
-
-#if defined(MBEDTLS_RSA_C)
-    p[1 + ct_len++] = MBEDTLS_SSL_CERT_TYPE_RSA_SIGN;
-#endif
-#if defined(MBEDTLS_ECDSA_C)
-    p[1 + ct_len++] = MBEDTLS_SSL_CERT_TYPE_ECDSA_SIGN;
-#endif
-
-    p[0] = (unsigned char) ct_len++;
-    p += ct_len;
-
-    sa_len = 0;
-#if defined(MBEDTLS_SSL_PROTO_TLS1_2)
-    /*
-     * Add signature_algorithms for verify (TLS 1.2)
-     *
-     *     SignatureAndHashAlgorithm supported_signature_algorithms<2..2^16-2>;
-     *
-     *     struct {
-     *           HashAlgorithm hash;
-     *           SignatureAlgorithm signature;
-     *     } SignatureAndHashAlgorithm;
-     *
-     *     enum { (255) } HashAlgorithm;
-     *     enum { (255) } SignatureAlgorithm;
-     */
-    if( ssl->minor_ver == MBEDTLS_SSL_MINOR_VERSION_3 )
-    {
-        const int *cur;
-
-        /*
-         * Supported signature algorithms
-         */
-        for( cur = ssl->conf->sig_hashes; *cur != MBEDTLS_MD_NONE; cur++ )
-        {
-            unsigned char hash = mbedtls_ssl_hash_from_md_alg( *cur );
-
-            if( MBEDTLS_SSL_HASH_NONE == hash || mbedtls_ssl_set_calc_verify_md( ssl, hash ) )
-                continue;
-
-#if defined(MBEDTLS_RSA_C)
-            p[2 + sa_len++] = hash;
-            p[2 + sa_len++] = MBEDTLS_SSL_SIG_RSA;
-#endif
-#if defined(MBEDTLS_ECDSA_C)
-            p[2 + sa_len++] = hash;
-            p[2 + sa_len++] = MBEDTLS_SSL_SIG_ECDSA;
-#endif
-        }
-
-        p[0] = (unsigned char)( sa_len >> 8 );
-        p[1] = (unsigned char)( sa_len      );
-        sa_len += 2;
-        p += sa_len;
-    }
-#endif /* MBEDTLS_SSL_PROTO_TLS1_2 */
-
-    /*
-     * DistinguishedName certificate_authorities<0..2^16-1>;
-     * opaque DistinguishedName<1..2^16-1>;
-     */
-    p += 2;
-
-    total_dn_size = 0;
-
-    if( ssl->conf->cert_req_ca_list ==  MBEDTLS_SSL_CERT_REQ_CA_LIST_ENABLED )
-    {
-#if defined(MBEDTLS_SSL_SERVER_NAME_INDICATION)
-        if( ssl->handshake->sni_ca_chain != NULL )
-            crt = ssl->handshake->sni_ca_chain;
-        else
-#endif
-            crt = ssl->conf->ca_chain;
-
-        while( crt != NULL && crt->version != 0 )
-        {
-            dn_size = crt->subject_raw.len;
-
-            if( end < p ||
-                (size_t)( end - p ) < dn_size ||
-                (size_t)( end - p ) < 2 + dn_size )
-            {
-                MBEDTLS_SSL_DEBUG_MSG( 1, ( "skipping CAs: buffer too short" ) );
-                break;
-            }
-
-            *p++ = (unsigned char)( dn_size >> 8 );
-            *p++ = (unsigned char)( dn_size      );
-            memcpy( p, crt->subject_raw.p, dn_size );
-            p += dn_size;
-
-            MBEDTLS_SSL_DEBUG_BUF( 3, "requested DN", p - dn_size, dn_size );
-
-            total_dn_size += 2 + dn_size;
-            crt = crt->next;
-        }
-    }
+/*             if( end < p || */
+/*                 (size_t)( end - p ) < dn_size || */
+/*                 (size_t)( end - p ) < 2 + dn_size ) */
+/*             { */
+/*                 MBEDTLS_SSL_DEBUG_MSG( 1, ( "skipping CAs: buffer too short" ) ); */
+/*                 break; */
+/*             } */
+/*             *p++ = (unsigned char)( dn_size >> 8 ); */
+/*             *p++ = (unsigned char)( dn_size      ); */
+/*             memcpy( p, crt->subject_raw.p, dn_size ); */
+/*             p += dn_size; */
+/*             MBEDTLS_SSL_DEBUG_BUF( 3, "requested DN", p - dn_size, dn_size ); */
+/*             total_dn_size += 2 + dn_size; */
+/*             crt = crt->next; */
+/*         } */
+/*     } */
 
     ssl->out_msglen  = p - buf;
     ssl->out_msgtype = MBEDTLS_SSL_MSG_HANDSHAKE;

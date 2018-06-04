3094ab1cd98cb7d0ecaad0fc08e05e3d821e3de0
Hanno Becker <hanno.becker@arm.com>
2018-06-04 17:45:14 +0100

    Update copyright statement in header for reader

--- include/mbedtls/mps/reader.h
+++ include/mbedtls/mps/reader.h
@@ -91,7 +91,7 @@
  *     previous consumer request
  *
  *
- *  Copyright (C) 2006-2015, ARM Limited, All Rights Reserved
+ *  Copyright (C) 2006-2018, ARM Limited, All Rights Reserved
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may

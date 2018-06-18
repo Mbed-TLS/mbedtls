2d9d6db60f5fd0a4993d90e47f39462647624ad6
Gilles Peskine <Gilles.Peskine@arm.com>
2018-06-18 23:25:28 +0200

    check-names: also check PSA files
    Allow both mbedtls and psa identifiers in either set of files for now.

--- tests/scripts/list-macros.sh
+++ tests/scripts/list-macros.sh
@@ -8,6 +8,7 @@ if [ -d include/mbedtls ]; then :; else
 fi
 
 HEADERS=$( ls include/mbedtls/*.h | egrep -v 'compat-1\.3\.h' )
+HEADERS="$HEADERS configs/config-default.h"
 
 sed -n -e 's/.*#define \([a-zA-Z0-9_]*\).*/\1/p' $HEADERS \
     | egrep -v '^(asm|inline|EMIT|_CRT_SECURE_NO_DEPRECATE)$|^MULADDC_' \

259a56b1c1bd7e9b89add77b532d85a71c1ed158
Mohammad Azim Khan <Azim.Khan@arm.com>
2018-06-19 16:16:49 +0100

    Script for mbed-os SSL Client sample

---
+++ tests/scripts/build-mbed-os-sslclient.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+
+# Validate that script is invoked from correct path
+if [ ! -x tests/scripts/$(basename "$0") ]; then
+    echo "This script should be run from mbedtls root!"
+    exit 1
+fi
+
+# Setup paths
+MBEDTLS_ROOT=$(pwd)
+MBED_OS_SSLCLIENT=$MBEDTLS_ROOT/programs/mbed-os-sslclient
+MBEDTLS_IMPORTER_DIR=$MBED_OS_SSLCLIENT/mbed-os/features/mbedtls/importer/
+MBED_OS_SSLCLIENT_CONFIG=$MBED_OS_SSLCLIENT/mbed-os/features/mbedtls/inc/mbedtls/config.h
+
+# Fetch mbed-os
+cd $MBED_OS_SSLCLIENT
+mbed config root .
+mbed deploy
+
+# Import mbedtls into mbed-os
+ln -s $MBEDTLS_ROOT $MBEDTLS_IMPORTER_DIR/TARGET_IGNORE/mbedtls
+cd $MBEDTLS_IMPORTER_DIR
+make deploy
+
+# Enable serialized network and file system
+$MBEDTLS_ROOT/scripts/config.pl -f $MBED_OS_SSLCLIENT_CONFIG set MBEDTLS_SERIALIZE_C
+$MBEDTLS_ROOT/scripts/config.pl -f $MBED_OS_SSLCLIENT_CONFIG set MBEDTLS_FS_IO
+
+# Build ssl client
+cd $MBED_OS_SSLCLIENT
+mbed-cli config root .
+mbed-cli compile -m K64F -t GCC_ARM

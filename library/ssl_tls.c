fd1c5e84531a1cd705b8d72bd39e1dd7558c7786
Angus Gratton <angus@espressif.com>
2018-06-20 15:43:50 +1000

    Check for invalid short Alert messages
    (Short Change Cipher Spec & Handshake messages are already checked for.)

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -4071,6 +4071,16 @@ read_record_header:
 
     if( ssl->in_msgtype == MBEDTLS_SSL_MSG_ALERT )
     {
+        if( ssl->in_msglen != 2 )
+        {
+            /* Note: Standard allows for more than one 2 byte alert
+               to be packed in a single message, but Mbed TLS doesn't
+               currently support this. */
+            MBEDTLS_SSL_DEBUG_MSG( 1, ( "invalid alert message, len: %d",
+                           ssl->in_msglen ) );
+            return( MBEDTLS_ERR_SSL_INVALID_RECORD );
+        }
+
         MBEDTLS_SSL_DEBUG_MSG( 2, ( "got an alert message, type: [%d:%d]",
                        ssl->in_msg[0], ssl->in_msg[1] ) );
 

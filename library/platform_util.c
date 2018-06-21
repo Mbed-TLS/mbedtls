b328044163c192d0bad542883b032d1e5a947b4f
irwir <irwir@users.noreply.github.com>
2018-06-21 13:51:46 +0300

    Tabs removed

--- library/platform_util.c
+++ library/platform_util.c
@@ -35,34 +35,34 @@
 void mbedtls_platform_zeroize(void *buf, size_t len)
 {
 #ifndef MBEDTLS_PLATFORM_ZEROIZE_ALT
-	/*
-	* This implementation should never be optimized out by the compiler
-	*
-	* This implementation for mbedtls_platform_zeroize() was inspired from Colin
-	* Percival's blog article at:
-	*
-	* http://www.daemonology.net/blog/2014-09-04-how-to-zero-a-buffer.html
-	*
-	* It uses a volatile function pointer to the standard memset(). Because the
-	* pointer is volatile the compiler expects it to change at
-	* any time and will not optimize out the call that could potentially perform
-	* other operations on the input buffer instead of just setting it to 0.
-	* Nevertheless, as pointed out by davidtgoldblatt on Hacker News
-	* (refer to http://www.daemonology.net/blog/2014-09-05-erratum.html for
-	* details), optimizations of the following form are still possible:
-	*
-	* if( memset_func != memset )
-	*     memset_func( buf, 0, len );
-	*
-	* Note that it is extremely difficult to guarantee that
-	* mbedtls_platform_zeroize() will not be optimized out by aggressive compilers
-	* in a portable way. For this reason, Mbed TLS also provides the configuration
-	* option MBEDTLS_PLATFORM_ZEROIZE_ALT, which allows users to configure
-	* mbedtls_platform_zeroize() to use a suitable implementation for their
-	* platform and needs.
-	*/
-	static const void * (*const volatile memset_func)(void *, int, size_t) = memset;
-	memset_func(buf, 0, len);
+/*
+ * This implementation should never be optimized out by the compiler
+ *
+ * This implementation for mbedtls_platform_zeroize() was inspired from Colin
+ * Percival's blog article at:
+ *
+ * http://www.daemonology.net/blog/2014-09-04-how-to-zero-a-buffer.html
+ *
+ * It uses a volatile function pointer to the standard memset(). Because the
+ * pointer is volatile the compiler expects it to change at
+ * any time and will not optimize out the call that could potentially perform
+ * other operations on the input buffer instead of just setting it to 0.
+ * Nevertheless, as pointed out by davidtgoldblatt on Hacker News
+ * (refer to http://www.daemonology.net/blog/2014-09-05-erratum.html for
+ * details), optimizations of the following form are still possible:
+ *
+ * if( memset_func != memset )
+ *     memset_func( buf, 0, len );
+ *
+ * Note that it is extremely difficult to guarantee that
+ * mbedtls_platform_zeroize() will not be optimized out by aggressive compilers
+ * in a portable way. For this reason, Mbed TLS also provides the configuration
+ * option MBEDTLS_PLATFORM_ZEROIZE_ALT, which allows users to configure
+ * mbedtls_platform_zeroize() to use a suitable implementation for their
+ * platform and needs.
+*/
+    static const void * (*const volatile memset_func)(void *, int, size_t) = memset;
+    memset_func(buf, 0, len);
 #else
 #error "MBEDTLS_PLATFORM_ZEROIZE_ALT was defined, but neither function nor macro were provided for 'mbedtls_platform_zeroize'."
 #endif /* MBEDTLS_PLATFORM_ZEROIZE_ALT */

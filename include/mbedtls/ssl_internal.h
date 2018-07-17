b628a80b7695fabfe07078ac5826e233f87191ca
Hanno Becker <hanno.becker@arm.com>
2018-07-17 10:19:47 +0100

    Fix definition of SSL_SOME_MODES_USE_MAC
    The previous definition was lacking the case of the ARIA and DES ciphers.

--- include/mbedtls/ssl_internal.h
+++ include/mbedtls/ssl_internal.h
@@ -141,7 +141,8 @@
     ( defined(MBEDTLS_CIPHER_MODE_CBC) &&                               \
       ( defined(MBEDTLS_AES_C)      ||                                  \
         defined(MBEDTLS_CAMELLIA_C) ||                                  \
-        defined(MBEDTLS_ARIA_C) ) )
+        defined(MBEDTLS_ARIA_C)     ||                                  \
+        defined(MBEDTLS_DES_C) ) )
 #define MBEDTLS_SSL_SOME_MODES_USE_MAC
 #endif
 

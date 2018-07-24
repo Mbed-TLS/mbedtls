d21e958c3e494837b9897696de748a10e3ee48f7
k-stachowiak <krzysiek.stachowiak@gmail.com>
2018-07-24 12:53:54 +0200

    Fix code formatting

--- library/x509_crt.c
+++ library/x509_crt.c
@@ -559,7 +559,7 @@ static int x509_get_crt_ext( unsigned char **p,
 
         /* Get extension ID */
         if( ( ret = mbedtls_asn1_get_tag( p, end_ext_data, &extn_oid.len,
-				  MBEDTLS_ASN1_OID ) ) != 0 )
+                                          MBEDTLS_ASN1_OID ) ) != 0 )
             return( MBEDTLS_ERR_X509_INVALID_EXTENSIONS + ret );
 
         extn_oid.tag = MBEDTLS_ASN1_OID;

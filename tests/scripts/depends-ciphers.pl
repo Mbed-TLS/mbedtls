a60182e7032db220a7da2e54c9b846586a9331d4
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2018-08-04 13:24:11 +0200

    Add new test script depends-ciphers.pl
    There will be three steps of tests:
    1. build the library
    2. build the test suites
    3. run the test suites
    
    Currently all of them are disabled as none passes. They will be enabled
    in order in the following commits after the issues they uncover are resolved.
    Co-authored-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
    Signed-off-by: Andrzej Kurek <andrzej.kurek@arm.com>

---
+++ tests/scripts/depends-ciphers.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/env perl
+
+# depends-ciphers.pl
+#
+# Copyright (c) 2018, ARM Limited, All Rights Reserved
+#
+# Purpose
+#
+# To test the code dependencies on individual ciphers in each test suite. This
+# is a verification step to ensure we don't ship test suites that do not work
+# for some build options.
+#
+# The process is:
+#       for each possible cipher
+#           build the library and test suites with the hash disabled
+#           execute the test suites
+#
+# And any test suite with the wrong dependencies will fail.
+#
+# Usage: tests/scripts/depends-ciphers.pl
+#
+# This script should be executed from the root of the project directory.
+#
+# For best effect, run either with cmake disabled, or cmake enabled in a mode
+# that includes -Werror.
+
+use warnings;
+use strict;
+
+-d 'library' && -d 'include' && -d 'tests' or die "Must be run from root\n";
+
+my $config_h = 'include/mbedtls/config.h';
+
+# get a list of ciphers from the cipher module
+my $cipher_h = 'include/mbedtls/cipher.h';
+my $sed_cmd = 's/^    MBEDTLS_CIPHER_ID_\([A-Z0-9_]*\),.*/\1/p';
+my @ciphers = split( /\s+/, `sed -n -e '$sed_cmd' $cipher_h` );
+@ciphers = map { "MBEDTLS_${_}_C" } grep { ! m/NULL|3DES/ } @ciphers;
+
+# Some algorithms can't be disabled on their own as others depend on them, so
+# we list those reverse-dependencies here to keep check_config.h happy.
+my %revdeps = (
+    'MBEDTLS_AES_C'         => ['MBEDTLS_CTR_DRBG_C'],
+);
+
+system( "cp $config_h $config_h.bak" ) and die;
+sub abort {
+    system( "mv $config_h.bak $config_h" ) and warn "$config_h not restored\n";
+    # use an exit code between 1 and 124 for git bisect (die returns 255)
+    warn $_[0];
+    exit 1;
+}
+
+for my $cipher (@ciphers) {
+    system( "cp $config_h.bak $config_h" ) and die "$config_h not restored\n";
+    system( "make clean" ) and die;
+
+    print "\n******************************************\n";
+    print "* Testing without cipher: $cipher\n";
+    print "******************************************\n";
+
+    system( "scripts/config.pl unset $cipher" )
+        and abort "Failed to disable $cipher\n";
+    if( exists $revdeps{$cipher} ) {
+        for my $opt (@{ $revdeps{$cipher} }) {
+            system( "scripts/config.pl unset $opt" )
+                and abort "Failed to disable $opt\n";
+        }
+    }
+
+    #system( "CFLAGS='-Werror -Wall -Wextra' make lib" )
+    #    and abort "Failed to build lib: $cipher\n";
+    #system( "cd tests && make" ) and abort "Failed to build tests: $cipher\n";
+    #system( "make test" ) and abort "Failed test suite: $cipher\n";
+}
+
+system( "mv $config_h.bak $config_h" ) and die "$config_h not restored\n";
+system( "make clean" ) and die;
+exit 0;

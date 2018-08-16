b34149c00ff3b629a531feb5f57ea817c10f5c97
Hanno Becker <hanno.becker@arm.com>
2018-08-16 15:29:06 +0100

    Add test for buffering of record from next epoch

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -5751,6 +5751,14 @@ run_test    "DTLS reordering: Buffer out-of-order handshake message" \
             -c "Buffering HS message" \
             -c "Next handshake message has been buffered - load"
 
+run_test    "DTLS reordering: Buffer record from future epoch" \
+            -p "$P_PXY drop=3 seed=2" \
+            "$P_SRV cookies=0 dtls=1 debug_level=2" \
+            "$P_CLI dtls=1 debug_level=2" \
+            0 \
+            -s "Buffer record from epoch 1" \
+            -s "Found buffered record from current epoch - load"
+
 run_test    "DTLS reordering: Buffer out-of-order CCS message"\
             -p "$P_PXY delay=3 seed=1" \
             "$P_SRV cookies=0 dtls=1 debug_level=2" \

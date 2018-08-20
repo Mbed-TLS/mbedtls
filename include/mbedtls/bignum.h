05cff953c99d12cf6d67c86a84a1b94367763fbb
Ko- <k.stoffelen@cs.ru.nl>
2018-08-20 12:59:57 +0100

    Make GNUC-compatible compilers use the right mbedtls_t_udbl again on Aarch64 builds.

--- include/mbedtls/bignum.h
+++ include/mbedtls/bignum.h
@@ -129,7 +129,8 @@
         defined(__ppc64__) || defined(__powerpc64__)  || \
         defined(__ia64__)  || defined(__alpha__)      || \
         ( defined(__sparc__) && defined(__arch64__) ) || \
-        defined(__s390x__) || defined(__mips64) )
+        defined(__s390x__) || defined(__mips64)       || \
+        defined(__aarch64__) )
         #if !defined(MBEDTLS_HAVE_INT64)
             #define MBEDTLS_HAVE_INT64
         #endif /* MBEDTLS_HAVE_INT64 */
@@ -140,8 +141,9 @@
             typedef unsigned int mbedtls_t_udbl __attribute__((mode(TI)));
             #define MBEDTLS_HAVE_UDBL
         #endif /* !MBEDTLS_NO_UDBL_DIVISION */
-    #elif defined(__aarch64__)
+    #elif defined(__ARMCC_VERSION) && defined(__aarch64__)
         /*
+         * __ARMCC_VERSION is defined for both armcc and armclang and
          * __aarch64__ is only defined by armclang when compiling 64-bit code
          */
         #if !defined(MBEDTLS_HAVE_INT64)

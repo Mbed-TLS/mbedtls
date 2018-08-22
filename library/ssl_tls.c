081bd81865881b82fc5d04847189b01fe4df8c1e
Hanno Becker <hanno.becker@arm.com>
2018-08-22 16:07:59 +0100

    ssl_write_handshake_msg(): Always append CCS messages to flights
    The previous code appended messages to flights only if their handshake type,
    as derived from the first byte in the message, was different from
    MBEDTLS_SSL_HS_HELLO_REQUEST. This check should only be performed
    for handshake records, while CCS records should immediately be appended.

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -3142,7 +3142,8 @@ int mbedtls_ssl_write_handshake_msg( mbedtls_ssl_context *ssl )
     /* Either send now, or just save to be sent (and resent) later */
 #if defined(MBEDTLS_SSL_PROTO_DTLS)
     if( ssl->conf->transport == MBEDTLS_SSL_TRANSPORT_DATAGRAM &&
-        hs_type != MBEDTLS_SSL_HS_HELLO_REQUEST )
+        ( ssl->out_msgtype != MBEDTLS_SSL_MSG_HANDSHAKE ||
+          hs_type != MBEDTLS_SSL_HS_HELLO_REQUEST ) )
     {
         if( ( ret = ssl_flight_append( ssl ) ) != 0 )
         {

5f95c798a321ca7cb8a754100bdae9ccd397685c
Hanno Becker <hanno.becker@arm.com>
2018-09-05 14:36:36 +0100

    Remove another mentioning of IAR from config.h

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -149,7 +149,7 @@
  * when called behaves similarly to the gmtime() function from the C standard,
  * but is thread safe. Mbed TLS will try to identify the underlying platform
  * and configure an appropriate underlying implementation (e.g. gmtime_r() for
- * POSIX and gmtime_s() for Windows and IAR). If this is not possible, then
+ * POSIX and gmtime_s() for Windows). If this is not possible, then
  * gmtime() will be used. Refer to the documentation for
  * mbedtls_platform_gmtime() for more information.
  *

2431d3ea8c6a7e77fa6e6df658b1c895de583981
Krzysztof Stachowiak <krzysztof.stachowiak@arm.com>
2018-09-12 17:43:21 +0200

    Remove teardown calls duplicated during a rebase

--- programs/pkey/dh_genprime.c
+++ programs/pkey/dh_genprime.c
@@ -222,9 +222,6 @@ exit:
     fflush( stdout ); getchar();
 #endif
 
-#if defined(MBEDTLS_PLATFORM_C)
-    mbedtls_platform_teardown( &platform_ctx );
-#endif
     return( exit_code );
 }
 #endif /* MBEDTLS_BIGNUM_C && MBEDTLS_ENTROPY_C && MBEDTLS_FS_IO &&

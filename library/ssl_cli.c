1c1c20ed4d8357c73d53443277a901013da7d745
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2018-09-12 10:34:43 +0200

    Fix some whitespace issues

--- library/ssl_cli.c
+++ library/ssl_cli.c
@@ -2077,8 +2077,8 @@ static int ssl_parse_server_ecdh_params( mbedtls_ssl_context *ssl,
     {
         MBEDTLS_SSL_DEBUG_RET( 1, ( "mbedtls_ecdh_read_params" ), ret );
 #if defined(MBEDTLS_SSL__ECP_RESTARTABLE)
-            if( ret == MBEDTLS_ERR_ECP_IN_PROGRESS )
-                ret = MBEDTLS_ERR_SSL_CRYPTO_IN_PROGRESS;
+        if( ret == MBEDTLS_ERR_ECP_IN_PROGRESS )
+            ret = MBEDTLS_ERR_SSL_CRYPTO_IN_PROGRESS;
 #endif
         return( ret );
     }

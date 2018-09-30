6e3606e4f6b00a14e728fc5f866ba48fd1ecf5a0
Simon Butcher <simon.butcher@arm.com>
2018-09-30 21:53:16 +0100

    Fix run-test-suites.pl to screen for files
    Changes run-test-suites.pl to filter out directories, and select only files
    as on OSX, test coverage tests create .dSYM directories which were being
    accidentally selected to execute.

--- tests/scripts/run-test-suites.pl
+++ tests/scripts/run-test-suites.pl
@@ -37,7 +37,7 @@ if ( defined($switch) && ( $switch eq "-v" || $switch eq "--verbose" ) ) {
 # and profiling information, etc. We can't just grep {! /\./} because
 # some of our test cases' base names contain a dot.
 my @suites = grep { -x $_ || /\.exe$/ } glob 'test_suite_*';
-@suites = grep { !/\.c$/ && !/\.data$/ } @suites;
+@suites = grep { !/\.c$/ && !/\.data$/ && -f } @suites;
 die "$0: no test suite found\n" unless @suites;
 
 # in case test suites are linked dynamically

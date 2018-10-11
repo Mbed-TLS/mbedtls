8381fcb3f9fcab6206556d1410b06246f3de8117
Jaeden Amero <jaeden.amero@arm.com>
2018-10-11 12:06:15 +0100

    aes: xts: Update inaccurate comments
    It is inaccurate to call a data unit a "sector". A disk sector is a
    common use case for the data unit, but there exist other types of data
    units that are not sectors.

--- library/aes.c
+++ library/aes.c
@@ -1142,7 +1142,7 @@ int mbedtls_aes_crypt_xts( mbedtls_aes_xts_context *ctx,
     unsigned char prev_tweak[16];
     unsigned char tmp[16];
 
-    /* Sectors must be at least 16 bytes. */
+    /* Data units must be at least 16 bytes long. */
     if( length < 16 )
         return MBEDTLS_ERR_AES_INVALID_INPUT_LENGTH;
 

ee334a3a5cfbc78abbb74d81aa74c5e8e9e6597e
Hanno Becker <hanno.becker@arm.com>
2018-10-24 12:33:07 +0100

    Remove Doxygen tags from documentation of private structure

--- library/x509_create.c
+++ library/x509_create.c
@@ -38,12 +38,12 @@
 typedef struct {
    const char *name; /* String representation of AttributeType, e.g.
                       * "CN" or "emailAddress". */
-   size_t name_len;  /* Length of \c name, without trailing \c 0 byte. */
+   size_t name_len;  /* Length of 'name', without trailing 0 byte. */
    const char *oid;  /* String representation of OID of AttributeType,
                       * as per RFC 5280, Appendix A.1. */
    int default_tag;  /* The default character encoding used for the
                       * given attribute type, e.g.
-                      * #MBEDTLS_ASN1_UTF8_STRING for UTF-8. */
+                      * MBEDTLS_ASN1_UTF8_STRING for UTF-8. */
 } x509_attr_descriptor_t;
 
 #define ADD_STRLEN( s )     s, sizeof( s ) - 1

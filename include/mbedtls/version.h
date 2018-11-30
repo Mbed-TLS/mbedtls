5dfca1e1bdf5bc8f0e9e9c3035a3d0a6f0a10b75
Jaeden Amero <jaeden.amero@arm.com>
2018-11-30 16:00:50 +0000

    Update library version to 2.15.1

--- include/mbedtls/version.h
+++ include/mbedtls/version.h
@@ -40,16 +40,16 @@
  */
 #define MBEDTLS_VERSION_MAJOR  2
 #define MBEDTLS_VERSION_MINOR  15
-#define MBEDTLS_VERSION_PATCH  0
+#define MBEDTLS_VERSION_PATCH  1
 
 /**
  * The single version number has the following structure:
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define MBEDTLS_VERSION_NUMBER         0x020F0000
-#define MBEDTLS_VERSION_STRING         "2.15.0"
-#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.15.0"
+#define MBEDTLS_VERSION_NUMBER         0x020F0100
+#define MBEDTLS_VERSION_STRING         "2.15.1"
+#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.15.1"
 
 #if defined(MBEDTLS_VERSION_C)
 

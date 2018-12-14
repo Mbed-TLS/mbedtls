2a97fbd2b554982ef5fb5b5044a4c262283512c9
Christoph M. Wintersteiger <cwinter@microsoft.com>
2018-12-14 13:34:06 +0000

    ECDH: Exclude FStar and Hacl* from exported symbol checks

--- tests/scripts/list-symbols.sh
+++ tests/scripts/list-symbols.sh
@@ -17,9 +17,9 @@ scripts/config.pl full
 CFLAGS=-fno-asynchronous-unwind-tables make clean lib >/dev/null 2>&1
 mv include/mbedtls/config.h.bak include/mbedtls/config.h
 if uname | grep -F Darwin >/dev/null; then
-    nm -gUj library/libmbed*.a 2>/dev/null | sed -n -e 's/^_//p'
+    nm -gUj library/libmbed*.a 2>/dev/null | sed -n -e 's/^_//p' | grep -v -e ^FStar -e ^Hacl
 elif uname | grep -F Linux >/dev/null; then
-    nm -og library/libmbed*.a | grep -v '^[^ ]*: *U \|^$\|^[^ ]*:$' | sed 's/^[^ ]* . //'
+    nm -og library/libmbed*.a | grep -v '^[^ ]*: *U \|^$\|^[^ ]*:$' | sed 's/^[^ ]* . //' | grep -v -e ^FStar -e ^Hacl
 fi | sort > exported-symbols
 make clean
 

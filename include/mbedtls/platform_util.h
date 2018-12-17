6d0816a8ae3973df2ff2d181034e2f4ef077666c
Hanno Becker <hanno.becker@arm.com>
2018-12-17 11:30:27 +0000

    Introduce macros to deprecate constants in the API
    This commit introduces macros
    
    * MBEDTLS_DEPRECATED_STRING_CONSTANT
    * MBEDTLS_DEPRECATED_NUMERIC_CONSTANT
    
    to platform_util.h which can be used to deprecate public macro constants.
    
    Their definition is essentially taken from dhm.h where the
    MBEDTLS_DEPRECATED_STRING_CONSTANT was used to deprecate
    insecure hardcoded DHM primes.

--- include/mbedtls/platform_util.h
+++ include/mbedtls/platform_util.h
@@ -41,6 +41,27 @@
 extern "C" {
 #endif
 
+/* Internal helper macros for deprecating API constants. */
+#if !defined(MBEDTLS_DEPRECATED_REMOVED)
+#if defined(MBEDTLS_DEPRECATED_WARNING)
+/* Deliberately don't (yet) define MBEDTLS_DEPRECATED here to avoid
+ * conflict with other headers which define and use it, too.
+ * We might want to move all these definitions here at some point
+ * for uniformity. */
+__attribute__((deprecated))
+typedef char const * mbedtls_deprecated_string_constant_t;
+#define MBEDTLS_DEPRECATED_STRING_CONSTANT( VAL )       \
+    ( (mbedtls_deprecated_string_constant_t) ( VAL ) )
+ __attribute__((deprecated))
+typedef int mbedtls_deprecated_numeric_constant_t;
+#define MBEDTLS_DEPRECATED_NUMERIC_CONSTANT( VAL )       \
+    ( (mbedtls_deprecated_numeric_constant_t) ( VAL ) )
+#else /* MBEDTLS_DEPRECATED_WARNING */
+#define MBEDTLS_DEPRECATED_STRING_CONSTANT( VAL ) VAL
+#define MBEDTLS_DEPRECATED_NUMERIC_CONSTANT( VAL ) VAL
+#endif /* MBEDTLS_DEPRECATED_WARNING */
+#endif /* MBEDTLS_DEPRECATED_REMOVED */
+
 /**
  * \brief       Securely zeroize a buffer
  *

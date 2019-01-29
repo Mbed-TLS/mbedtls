2305440563f52ed9cbc582fd98cbb0458edc1c90
Hanno Becker <hanno.becker@arm.com>
2019-01-29 15:47:12 +0000

    Threading: Fix style and rename is_mutex_valid() to mutex_is_valid()

--- library/threading.c
+++ library/threading.c
@@ -36,17 +36,18 @@ static void threading_mutex_init_pthread( mbedtls_threading_mutex_t *mutex )
     if( mutex == NULL )
         return;
 
-    mutex->is_valid = (pthread_mutex_init( &mutex->mutex, NULL ) == 0) ? 1 : 0;
+    mutex->is_valid =
+        ( pthread_mutex_init( &mutex->mutex, NULL ) == 0 ) ? 1 : 0;
 }
 
-static bool is_mutex_valid(mbedtls_threading_mutex_t *mutex)
+static int mutex_is_valid( mbedtls_threading_mutex_t *mutex )
 {
-    return (mutex->is_valid == 1);
+    return( mutex->is_valid == 1 );
 }
 
 static void threading_mutex_free_pthread( mbedtls_threading_mutex_t *mutex )
 {
-    if( mutex == NULL || !is_mutex_valid(mutex) )
+    if( mutex == NULL || !mutex_is_valid( mutex ) )
         return;
 
     (void) pthread_mutex_destroy( &mutex->mutex );
@@ -55,7 +56,7 @@ static void threading_mutex_free_pthread( mbedtls_threading_mutex_t *mutex )
 
 static int threading_mutex_lock_pthread( mbedtls_threading_mutex_t *mutex )
 {
-    if( mutex == NULL || !is_mutex_valid(mutex) )
+    if( mutex == NULL || !mutex_is_valid( mutex ) )
         return( MBEDTLS_ERR_THREADING_BAD_INPUT_DATA );
 
     if( pthread_mutex_lock( &mutex->mutex ) != 0 )
@@ -66,7 +67,7 @@ static int threading_mutex_lock_pthread( mbedtls_threading_mutex_t *mutex )
 
 static int threading_mutex_unlock_pthread( mbedtls_threading_mutex_t *mutex )
 {
-    if( mutex == NULL || !is_mutex_valid(mutex) )
+    if( mutex == NULL || !mutex_is_valid( mutex ) )
         return( MBEDTLS_ERR_THREADING_BAD_INPUT_DATA );
 
     if( pthread_mutex_unlock( &mutex->mutex ) != 0 )

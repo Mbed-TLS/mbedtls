84879e32efe3fd32c122ffc3d8da91a8d0a19bf2
Hanno Becker <hanno.becker@arm.com>
2019-01-31 07:44:03 +0000

    Add compile-time guards around helper routine

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -5435,6 +5435,7 @@ write_msg:
     return( ret );
 }
 
+#if defined(MBEDTLS_SSL_RENEGOTIATION) && defined(MBEDTLS_SSL_CLI_C)
 static int ssl_check_peer_crt_unchanged( mbedtls_ssl_context *ssl,
                                          unsigned char *crt_buf,
                                          size_t crt_buf_len )
@@ -5449,6 +5450,7 @@ static int ssl_check_peer_crt_unchanged( mbedtls_ssl_context *ssl,
 
     return( memcmp( peer_crt->raw.p, crt_buf, crt_buf_len) );
 }
+#endif /* MBEDTLS_SSL_RENEGOTIATION && MBEDTLS_SSL_CLI_C */
 
 /*
  * Once the certificate message is read, parse it into a cert chain and

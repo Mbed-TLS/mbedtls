ab43997f44b03c2299e3c56f168a0b74c60a6d9f
Jaeden Amero <jaeden.amero@arm.com>
2019-02-15 14:12:05 +0000

    psa: Disallow use of invalid cipher contexts
    Ensure that when doing cipher operations out of order,
    PSA_ERROR_BAD_STATE is returned as documented in crypto.h and the PSA
    Crypto specification.

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -3073,6 +3073,12 @@ psa_status_t psa_cipher_update( psa_cipher_operation_t *operation,
     psa_status_t status;
     int ret;
     size_t expected_output_size;
+
+    if( operation->alg == 0 )
+    {
+        return( PSA_ERROR_BAD_STATE );
+    }
+
     if( ! PSA_ALG_IS_STREAM_CIPHER( operation->alg ) )
     {
         /* Take the unprocessed partial block left over from previous

0a54c265ec9d2837ad9007b90832d23de21c3b2d
Christoph M. Wintersteiger <cwinter@microsoft.com>
2019-02-15 12:34:40 +0000

    ECDSA: Refactor return value checks for mbedtls_ecdsa_can_do

--- programs/test/benchmark.c
+++ programs/test/benchmark.c
@@ -855,7 +855,7 @@ int main( int argc, char *argv[] )
              curve_info->grp_id != MBEDTLS_ECP_DP_NONE;
              curve_info++ )
         {
-            if( mbedtls_ecdsa_can_do( curve_info->grp_id ) == 0 )
+            if( ! mbedtls_ecdsa_can_do( curve_info->grp_id ) )
                 continue;
 
             mbedtls_ecdsa_init( &ecdsa );
@@ -877,7 +877,7 @@ int main( int argc, char *argv[] )
              curve_info->grp_id != MBEDTLS_ECP_DP_NONE;
              curve_info++ )
         {
-            if( mbedtls_ecdsa_can_do( curve_info->grp_id ) == 0 )
+            if( ! mbedtls_ecdsa_can_do( curve_info->grp_id ) )
                 continue;
 
             mbedtls_ecdsa_init( &ecdsa );

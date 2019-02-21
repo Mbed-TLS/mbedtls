ded167e18cf7f82fecb5f0d08a356e241056db5d
Hanno Becker <hanno.becker@arm.com>
2019-02-21 14:34:46 +0000

    Add raw buffer holding SubjectAlternativeName ext to CRT structure
    This is analogous to a previous commit for the `ExtendedKeyUsage`
    extension: We aim at not using dynamically allocated linked lists
    to represent the components of the `SubjectAlternativeName` extension,
    but to traverse the raw ASN.1 data when needed.
    
    This commit adds a field to `mbedtls_x509_crt` containing the raw
    ASN.1 buffer bounds of the `SubjectAlternativeNames` extension.

--- include/mbedtls/x509_crt.h
+++ include/mbedtls/x509_crt.h
@@ -80,6 +80,7 @@ typedef struct mbedtls_x509_crt
     mbedtls_x509_buf subject_id;        /**< Optional X.509 v2/v3 subject unique identifier. */
     mbedtls_x509_buf v3_ext;            /**< Optional X.509 v3 extensions.  */
     mbedtls_x509_sequence subject_alt_names;    /**< Optional list of Subject Alternative Names (Only dNSName supported). */
+    mbedtls_x509_buf_raw subject_alt_raw; /**< Raw data for SubjectAlternativeNames extension. */
 
     int ext_types;              /**< Bit string containing detected and parsed extensions */
     int ca_istrue;              /**< Optional Basic Constraint extension value: 1 if this certificate belongs to a CA, 0 otherwise. */

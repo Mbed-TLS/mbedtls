2ea119af471265c184a287e5f5924c5f5bbab3c4
Hanno Becker <hanno.becker@arm.com>
2019-02-21 17:13:46 +0000

    Remove `sig_oid` parameter from mbedtls_x509_sig_alg_gets()
    The function `mbedtls_x509_sig_alg_gets()` previously needed the
    raw ASN.1 OID string even though it is implicit in the PK and MD
    parameters.
    
    This commit modifies `mbedtls_x509_sig_alg_gets()` to infer the OID
    and remove it from the parameters.
    
    This will be needed for the new X.509 CRT structure which will
    likely not store the signature OID.
    
    Care has to be taken to handle the case of RSASSA-PSS correctly,
    where the hash algorithm in the OID list is set to MBEDTLS_MD_NONE
    because it's only determined by the algorithm parameters.

--- include/mbedtls/x509.h
+++ include/mbedtls/x509.h
@@ -329,7 +329,7 @@ int mbedtls_x509_get_serial( unsigned char **p, const unsigned char *end,
                      mbedtls_x509_buf *serial );
 int mbedtls_x509_get_ext( unsigned char **p, const unsigned char *end,
                   mbedtls_x509_buf *ext, int tag );
-int mbedtls_x509_sig_alg_gets( char *buf, size_t size, const mbedtls_x509_buf *sig_oid,
+int mbedtls_x509_sig_alg_gets( char *buf, size_t size,
                        mbedtls_pk_type_t pk_alg, mbedtls_md_type_t md_alg,
                        const void *sig_opts );
 int mbedtls_x509_key_size_helper( char *buf, size_t buf_size, const char *name );

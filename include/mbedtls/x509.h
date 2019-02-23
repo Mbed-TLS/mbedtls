b9a40f330cdb153c846a20a42c90f7117385944b
Hanno Becker <hanno.becker@arm.com>
2019-02-23 12:12:46 +0000

    Add version of MBEDTLS_X509_SAFE_SNPRINTF supporting cleanup section
    This will be needed in debugging functions that maintain heap
    allocated state, and which hence needs to be freed in case an
    `snprintf()` call fails.

--- include/mbedtls/x509.h
+++ include/mbedtls/x509.h
@@ -376,6 +376,18 @@ int mbedtls_x509_write_sig( unsigned char **p, unsigned char *start,
         p += (size_t) ret;                                  \
     } while( 0 )
 
+#define MBEDTLS_X509_SAFE_SNPRINTF_WITH_CLEANUP             \
+    do {                                                    \
+        if( ret < 0 || (size_t) ret >= n )                  \
+        {                                                   \
+            ret = MBEDTLS_ERR_X509_BUFFER_TOO_SMALL;        \
+            goto cleanup;                                   \
+        }                                                   \
+                                                            \
+        n -= (size_t) ret;                                  \
+        p += (size_t) ret;                                  \
+    } while( 0 )
+
 #ifdef __cplusplus
 }
 #endif

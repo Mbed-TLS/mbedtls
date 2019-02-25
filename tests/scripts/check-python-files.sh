e70c6dcee0189151cf4bd7969cf475206d0d7738
Gilles Peskine <Gilles.Peskine@arm.com>
2019-02-25 20:25:02 +0100

    Call pylint3, not pylint
    We use Python 3, so call Pylint for Python 3, not for Python 2.

--- tests/scripts/check-python-files.sh
+++ tests/scripts/check-python-files.sh
@@ -9,10 +9,10 @@
 # Run 'pylint' on Python files for programming errors and helps enforcing
 # PEP8 coding standards.
 
-if `hash pylint > /dev/null 2>&1`; then
-    pylint -j 2 tests/scripts/generate_test_code.py
-    pylint -j 2 tests/scripts/test_generate_test_code.py
-    pylint -j 2 tests/scripts/mbedtls_test.py
+if `hash pylint3 > /dev/null 2>&1`; then
+    pylint3 -j 2 tests/scripts/generate_test_code.py
+    pylint3 -j 2 tests/scripts/test_generate_test_code.py
+    pylint3 -j 2 tests/scripts/mbedtls_test.py
 else
-    echo "$0: WARNING: 'pylint' not found! Skipping checks on Python files."
+    echo "$0: WARNING: 'pylint3' not found! Skipping checks on Python files."
 fi

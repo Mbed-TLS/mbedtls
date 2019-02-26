bd5580abb1792883883895c617d6a935f924c4ed
Hanno Becker <hanno.becker@arm.com>
2019-02-26 12:36:01 +0000

    Add further debug statements on assertion failures

--- library/ssl_cli.c
+++ library/ssl_cli.c
@@ -2419,6 +2419,7 @@ static int ssl_get_ecdh_params_from_cert( mbedtls_ssl_context *ssl )
     if( ssl->session_negotiate->peer_cert == NULL )
     {
         /* Should never happen */
+        MBEDTLS_SSL_DEBUG_MSG( 1, ( "should never happen" ) );
         return( MBEDTLS_ERR_SSL_INTERNAL_ERROR );
     }
     peer_pk = &ssl->session_negotiate->peer_cert->pk;
@@ -2772,6 +2773,7 @@ start_processing:
         if( ssl->session_negotiate->peer_cert == NULL )
         {
             /* Should never happen */
+            MBEDTLS_SSL_DEBUG_MSG( 1, ( "should never happen" ) );
             return( MBEDTLS_ERR_SSL_INTERNAL_ERROR );
         }
         peer_pk = &ssl->session_negotiate->peer_cert->pk;

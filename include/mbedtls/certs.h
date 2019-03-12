a04cc48f9882057c7b778324c3c693fb6789415b
Hanno Becker <hanno.becker@arm.com>
2019-03-12 16:42:09 +0000

    Add declarations for DER encoded test CRTs to certs.h

--- include/mbedtls/certs.h
+++ include/mbedtls/certs.h
@@ -42,9 +42,13 @@ extern const char   mbedtls_test_cas_pem[];
 extern const size_t mbedtls_test_cas_pem_len;
 #endif
 
-/* List of all CA certificates, terminated by NULL */
+/* List of all PEM-encoded CA certificates, terminated by NULL */
 extern const char * mbedtls_test_cas[];
 extern const size_t mbedtls_test_cas_len[];
+
+/* List of all DER-encoded CA certificates, terminated by NULL */
+extern const char * mbedtls_test_cas_der[];
+extern const size_t mbedtls_test_cas_der_len[];
 
 /*
  * Convenience for users who just want a certificate:
@@ -80,6 +84,20 @@ extern const char   mbedtls_test_cli_crt_ec[];
 extern const size_t mbedtls_test_cli_crt_ec_len;
 extern const char   mbedtls_test_cli_key_ec[];
 extern const size_t mbedtls_test_cli_key_ec_len;
+extern const char   mbedtls_test_ca_crt_ec_der[];
+extern const size_t mbedtls_test_ca_crt_ec_der_len;
+extern const char   mbedtls_test_ca_key_ec_der[];
+extern const size_t mbedtls_test_ca_key_ec_der_len;
+extern const char   mbedtls_test_ca_pwd_ec_der[];
+extern const size_t mbedtls_test_ca_pwd_ec_der_len;
+extern const char   mbedtls_test_srv_crt_ec_der[];
+extern const size_t mbedtls_test_srv_crt_ec_der_len;
+extern const char   mbedtls_test_srv_key_ec_der[];
+extern const size_t mbedtls_test_srv_key_ec_der_len;
+extern const char   mbedtls_test_cli_crt_ec_der[];
+extern const size_t mbedtls_test_cli_crt_ec_der_len;
+extern const char   mbedtls_test_cli_key_ec_der[];
+extern const size_t mbedtls_test_cli_key_ec_der_len;
 #endif
 
 #if defined(MBEDTLS_RSA_C)
@@ -97,6 +115,20 @@ extern const char   mbedtls_test_cli_crt_rsa[];
 extern const size_t mbedtls_test_cli_crt_rsa_len;
 extern const char   mbedtls_test_cli_key_rsa[];
 extern const size_t mbedtls_test_cli_key_rsa_len;
+extern const char   mbedtls_test_ca_crt_rsa_der[];
+extern const size_t mbedtls_test_ca_crt_rsa_der_len;
+extern const char   mbedtls_test_ca_key_rsa_der[];
+extern const size_t mbedtls_test_ca_key_rsa_der_len;
+extern const char   mbedtls_test_ca_pwd_rsa_der[];
+extern const size_t mbedtls_test_ca_pwd_rsa_der_len;
+extern const char   mbedtls_test_srv_crt_rsa_der[];
+extern const size_t mbedtls_test_srv_crt_rsa_der_len;
+extern const char   mbedtls_test_srv_key_rsa_der[];
+extern const size_t mbedtls_test_srv_key_rsa_der_len;
+extern const char   mbedtls_test_cli_crt_rsa_der[];
+extern const size_t mbedtls_test_cli_crt_rsa_der_len;
+extern const char   mbedtls_test_cli_key_rsa_der[];
+extern const size_t mbedtls_test_cli_key_rsa_der_len;
 #endif
 
 #ifdef __cplusplus

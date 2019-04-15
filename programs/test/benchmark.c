a4fd9f20f3c8a1bebf14894b6402c115f20cb8f6
Christoph M. Wintersteiger <cwinter@microsoft.com>
2019-04-15 11:09:00 +0100

    Fix macros in benchmark.c
    #2124 may suffer from the same problem.

--- programs/test/benchmark.c
+++ programs/test/benchmark.c
@@ -31,11 +31,7 @@
 #include <stdlib.h>
 #define mbedtls_exit       exit
 #define mbedtls_printf     printf
-#define mbedtls_snprintf   snprintf
 #define mbedtls_free       free
-#define mbedtls_exit            exit
-#define MBEDTLS_EXIT_SUCCESS    EXIT_SUCCESS
-#define MBEDTLS_EXIT_FAILURE    EXIT_FAILURE
 #endif
 
 #if !defined(MBEDTLS_TIMING_C)

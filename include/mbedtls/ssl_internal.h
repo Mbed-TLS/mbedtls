fe6bb8ccc214993289999614acfebb0d2e6f65b2
Hanno Becker <hanno.becker@arm.com>
2019-05-20 14:49:02 +0100

    Unify documentation of internal SSL record structure
    - Don't use Doxygen style comments
    - Document CID and CID length fields.

--- include/mbedtls/ssl_internal.h
+++ include/mbedtls/ssl_internal.h
@@ -653,18 +653,18 @@ struct mbedtls_ssl_transform
 
 typedef struct
 {
-    uint8_t ctr[8];         /*!< Record sequence number        */
-    uint8_t type;           /*!< Record type                   */
-    uint8_t ver[2];         /*!< SSL/TLS version               */
+    uint8_t ctr[8];         /* Record sequence number        */
+    uint8_t type;           /* Record type                   */
+    uint8_t ver[2];         /* SSL/TLS version               */
 
-    unsigned char *buf;     /*!< Memory buffer enclosing the record content */
-    size_t buf_len;         /*!< Buffer length */
-    size_t data_offset;     /*!< Offset of record content */
-    size_t data_len;        /*!< Length of record content */
+    unsigned char *buf;     /* Memory buffer enclosing the record content */
+    size_t buf_len;         /* Buffer length */
+    size_t data_offset;     /* Offset of record content */
+    size_t data_len;        /* Length of record content */
 
 #if defined(MBEDTLS_SSL_CID)
-    uint8_t cid_len;
-    unsigned char cid[ MBEDTLS_SSL_CID_LEN_MAX ];
+    uint8_t cid_len;        /* Length of the CID (0 if not present) */
+    unsigned char cid[ MBEDTLS_SSL_CID_LEN_MAX ]; /* The CID        */
 #endif /* MBEDTLS_SSL_CID */
 
 } mbedtls_record;

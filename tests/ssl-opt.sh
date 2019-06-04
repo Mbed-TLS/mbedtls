cc281b8a549a7bb0e0c0c8b23efaaa7699974424
Jarno Lamsa <jarno.lamsa@arm.com>
2019-06-04 15:21:13 +0300

    ssl-opt.sh tests for serialization are currently using stub implementation

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -1115,7 +1115,7 @@ run_test    "Truncated HMAC, DTLS: client enabled, server enabled" \
 # Tests for Context serialization
 
 requires_config_enabled MBEDTLS_SSL_CONTEXT_SERIALIZATION
-run_test    "Context serialization, client serializes/deserializes" \
+run_test    "(STUB) Context serialization, client serializes/deserializes" \
             "$P_SRV serialize=0 exchanges=2" \
             "$P_CLI serialize=1 exchanges=2" \
             0 \
@@ -1123,7 +1123,7 @@ run_test    "Context serialization, client serializes/deserializes" \
             -S "Deserializing connection..."
 
 requires_config_enabled MBEDTLS_SSL_CONTEXT_SERIALIZATION
-run_test    "Context serialization, server serializes/deserializes" \
+run_test    "(STUB) Context serialization, server serializes/deserializes" \
             "$P_SRV serialize=1 exchanges=2" \
             "$P_CLI serialize=0 exchanges=2" \
             0 \
@@ -1131,7 +1131,7 @@ run_test    "Context serialization, server serializes/deserializes" \
             -s "Deserializing connection..."
 
 requires_config_enabled MBEDTLS_SSL_CONTEXT_SERIALIZATION
-run_test    "Context serialization, both serialize/deserialize" \
+run_test    "(STUB) Context serialization, both serialize/deserialize" \
             "$P_SRV serialize=1 exchanges=2" \
             "$P_CLI serialize=1 exchanges=2" \
             0 \

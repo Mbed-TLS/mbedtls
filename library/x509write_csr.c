dff85e7e8a94e5204e7eed2a44cb13ec9184f007
k-stachowiak <krzysiek.stachowiak@gmail.com>
2019-06-13 11:54:49 +0200

    Remove unnecessary empty line

--- library/x509write_csr.c
+++ library/x509write_csr.c
@@ -43,7 +43,6 @@
 #include "mbedtls/pem.h"
 #endif
 
-
 /* Implementation that should never be optimized out by the compiler */
 static void mbedtls_zeroize( void *v, size_t n ) {
     volatile unsigned char *p = v; while( n-- ) *p++ = 0;

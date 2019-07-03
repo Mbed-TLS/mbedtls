950de1e331be096241ffcba27582b65675addd92
Gilles Peskine <Gilles.Peskine@arm.com>
2019-07-03 20:43:32 +0200

    Test that a shared library build produces a dynamically linked executable

--- tests/scripts/all.sh
+++ tests/scripts/all.sh
@@ -835,12 +835,14 @@ component_test_platform_calloc_macro () {
 component_test_make_shared () {
     msg "build/test: make shared" # ~ 40s
     make SHARED=1 all check
+    ldd programs/util/strerror | grep libmbedcrypto
 }
 
 component_test_cmake_shared () {
     msg "build/test: cmake shared" # ~ 2min
     cmake -DUSE_SHARED_MBEDTLS_LIBRARY=On .
     make
+    ldd programs/util/strerror | grep libmbedcrypto
     make test
 }
 

d829d0fbd0f873aaa5cb2bde307f3b58d15bea10
Hanno Becker <hanno.becker@arm.com>
2019-07-25 10:28:57 +0100

    Remove return value from mbedtls_ssl_pend_fatal_alert()
    This function is often called when there's already an error code to handle,
    and one of the reasons to introduce the pending of alerts was to _not_ have
    another potential error code to take care of. Reflect this by making `void`
    the return type of `mbedtls_ssl_pend_fatal_alert()`.

--- include/mbedtls/ssl_internal.h
+++ include/mbedtls/ssl_internal.h
@@ -1739,12 +1739,11 @@ static inline unsigned int mbedtls_ssl_conf_get_ems_enforced(
  * This function must not be called multiple times without manually
  * inspecting and clearing ssl->pending_fatal_alert_msg in between.
  */
-MBEDTLS_ALWAYS_INLINE static inline int mbedtls_ssl_pend_fatal_alert(
+MBEDTLS_ALWAYS_INLINE static inline void mbedtls_ssl_pend_fatal_alert(
     mbedtls_ssl_context *ssl,
     unsigned char message )
 {
     ssl->pend_alert_msg = message;
-    return( 0 );
 }
 
 #endif /* ssl_internal.h */

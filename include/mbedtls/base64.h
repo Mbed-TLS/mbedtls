f613e6fbb883e9c5fb20919ab5de3e5919416904
aggresss <aggresss@163.com>
2019-08-04 14:07:46 +0800

    Add base64 urlsafe support

--- include/mbedtls/base64.h
+++ include/mbedtls/base64.h
@@ -81,6 +81,50 @@ int mbedtls_base64_encode( unsigned char *dst, size_t dlen, size_t *olen,
 int mbedtls_base64_decode( unsigned char *dst, size_t dlen, size_t *olen,
                    const unsigned char *src, size_t slen );
 
+/**
+ * \brief          Encode a buffer into base64_urlsafe format
+ *
+ * \param dst      destination buffer
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data to be encoded
+ *
+ * \return         0 if successful, or MBEDTLS_ERR_BASE64_BUFFER_TOO_SMALL.
+ *                 *olen is always updated to reflect the amount
+ *                 of data that has (or would have) been written.
+ *                 If that length cannot be represented, then no data is
+ *                 written to the buffer and *olen is set to the maximum
+ *                 length representable as a size_t.
+ *
+ * \note           Call this function with dlen = 0 to obtain the
+ *                 required buffer size in *olen
+ *                 Relative to base64, base64_urlsafe replace '+' to '-' and replace '/' to '_'
+ */
+int mbedtls_base64_urlsafe_encode( unsigned char *dst, size_t dlen, size_t *olen,
+                   const unsigned char *src, size_t slen );
+
+/**
+ * \brief          Decode a base64_urlsafe-formatted buffer
+ *
+ * \param dst      destination buffer (can be NULL for checking size)
+ * \param dlen     size of the destination buffer
+ * \param olen     number of bytes written
+ * \param src      source buffer
+ * \param slen     amount of data to be decoded
+ *
+ * \return         0 if successful, MBEDTLS_ERR_BASE64_BUFFER_TOO_SMALL, or
+ *                 MBEDTLS_ERR_BASE64_INVALID_CHARACTER if the input data is
+ *                 not correct. *olen is always updated to reflect the amount
+ *                 of data that has (or would have) been written.
+ *
+ * \note           Call this function with *dst = NULL or dlen = 0 to obtain
+ *                 the required buffer size in *olen
+ */
+int mbedtls_base64_urlsafe_decode( unsigned char *dst, size_t dlen, size_t *olen,
+                   const unsigned char *src, size_t slen );
+
+
 #if defined(MBEDTLS_SELF_TEST)
 /**
  * \brief          Checkup routine
@@ -88,6 +132,13 @@ int mbedtls_base64_decode( unsigned char *dst, size_t dlen, size_t *olen,
  * \return         0 if successful, or 1 if the test failed
  */
 int mbedtls_base64_self_test( int verbose );
+
+/**
+ * \brief          Checkup routine
+ *
+ * \return         0 if successful, or 1 if the test failed
+ */
+int mbedtls_base64_urlsafe_self_test( int verbose );
 
 #endif /* MBEDTLS_SELF_TEST */
 

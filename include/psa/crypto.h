15731c14221e58cbeb8c892ce048a0fe11e5699d
Adrian L. Shaw <adrianlshaw@gmail.com>
2019-08-06 16:21:00 +0100

    Added PSA_ERROR_STORAGE_FAILURE to psa_export_key
    It may be possible that an implementation does not
    fetch key material until a command like
    this is called and such an error may occur if an
    off-chip secure storage dependency may have been wiped.

--- include/psa/crypto.h
+++ include/psa/crypto.h
@@ -831,6 +831,7 @@ psa_status_t psa_copy_key(psa_key_handle_t source_handle,
  * \retval #PSA_ERROR_COMMUNICATION_FAILURE
  * \retval #PSA_ERROR_HARDWARE_FAILURE
  * \retval #PSA_ERROR_CORRUPTION_DETECTED
+ * \retval #PSA_ERROR_STORAGE_FAILURE
  * \retval #PSA_ERROR_BAD_STATE
  *         The library has not been previously initialized by psa_crypto_init().
  *         It is implementation-dependent whether a failure to initialize

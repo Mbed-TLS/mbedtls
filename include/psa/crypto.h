1f1e1a52537554e96d81306c1fec8a97ac02b2a3
Adrian L. Shaw <adrianlshaw@gmail.com>
2019-08-13 11:44:30 +0100

    Add storage failure to psa_mac_verify_finish

--- include/psa/crypto.h
+++ include/psa/crypto.h
@@ -2010,6 +2010,7 @@ psa_status_t psa_cipher_update(psa_cipher_operation_t *operation,
  * \retval #PSA_ERROR_COMMUNICATION_FAILURE
  * \retval #PSA_ERROR_HARDWARE_FAILURE
  * \retval #PSA_ERROR_CORRUPTION_DETECTED
+ * \retval #PSA_ERROR_STORAGE_FAILURE
  * \retval #PSA_ERROR_BAD_STATE
  *         The library has not been previously initialized by psa_crypto_init().
  *         It is implementation-dependent whether a failure to initialize

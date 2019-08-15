f483973c37fe0f1e70015351332b93d6dd8e7efd
Adrian L. Shaw <adrianlshaw@gmail.com>
2019-08-15 13:30:57 +0100

    Add PSA_ERROR_STORAGE_FAILURE to psa_cipher_generate_iv

--- include/psa/crypto.h
+++ include/psa/crypto.h
@@ -2766,6 +2766,7 @@ psa_status_t psa_aead_verify(psa_aead_operation_t *operation,
  * \retval #PSA_ERROR_COMMUNICATION_FAILURE
  * \retval #PSA_ERROR_HARDWARE_FAILURE
  * \retval #PSA_ERROR_CORRUPTION_DETECTED
+ * \retval #PSA_ERROR_STORAGE_FAILURE
  * \retval #PSA_ERROR_BAD_STATE
  *         The library has not been previously initialized by psa_crypto_init().
  *         It is implementation-dependent whether a failure to initialize

fcb8711f6f23edca8995e2fdfa655e4d8cd79c4c
Jaeden Amero <jaeden.amero@arm.com>
2019-09-06 13:27:00 +0100

    Bump version to Mbed TLS 2.16.3

--- doxygen/input/doc_mainpage.h
+++ doxygen/input/doc_mainpage.h
@@ -24,7 +24,7 @@
  */
 
 /**
- * @mainpage mbed TLS v2.16.2 source code documentation
+ * @mainpage mbed TLS v2.16.3 source code documentation
  *
  * This documentation describes the internal structure of mbed TLS.  It was
  * automatically generated from specially formatted comment blocks in

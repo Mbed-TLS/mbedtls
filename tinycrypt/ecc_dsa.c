cffedb548f58ea2252a58d53a2b85aa15c7d9390
Simon Butcher <simon.butcher@arm.com>
2019-09-09 16:28:54 +0100

    Add SPDX lines to each imported TinyCrypt file
    Out of the 6 tinycrypt files included in Mbed TLS, this commit adds SPDX lines
    to each for the BSD 3 Clause licence.

--- tinycrypt/ecc_dsa.c
+++ tinycrypt/ecc_dsa.c
@@ -3,6 +3,8 @@
 /* Copyright (c) 2014, Kenneth MacKay
  * All rights reserved.
  *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *  * Redistributions of source code must retain the above copyright notice,

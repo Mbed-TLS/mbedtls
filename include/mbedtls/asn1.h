63e38fe9147070d99e2d3c841dddf2274daa85c2
Hanno Becker <hanno.becker@arm.com>
2019-09-11 14:16:40 +0100

    ASN.1: Add helper macro to compare ASN.1 buffer to OID string

--- include/mbedtls/asn1.h
+++ include/mbedtls/asn1.h
@@ -132,6 +132,10 @@
         ( ( MBEDTLS_OID_SIZE(oid_str) != (oid_buf)->len ) ||                \
           memcmp( (oid_str), (oid_buf)->p, (oid_buf)->len) != 0 )
 
+#define MBEDTLS_OID_CMP_RAW(oid_str, oid_buf, oid_buf_len)              \
+        ( ( MBEDTLS_OID_SIZE(oid_str) != (oid_buf_len) ) ||             \
+          memcmp( (oid_str), (oid_buf), (oid_buf_len) ) != 0 )
+
 #ifdef __cplusplus
 extern "C" {
 #endif

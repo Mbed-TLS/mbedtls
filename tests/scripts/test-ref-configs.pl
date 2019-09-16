a708dae94beadeb93d770439b18be367f53fe5d7
Gilles Peskine <Gilles.Peskine@arm.com>
2019-09-16 15:06:03 +0200

    Add comment to help syntax highlighting in editors

--- tests/scripts/test-ref-configs.pl
+++ tests/scripts/test-ref-configs.pl
@@ -18,7 +18,7 @@ use strict;
 
 my %configs = (
     'config-mini-tls1_1.h' => {
-        'compat' => '-m tls1_1 -f \'^DES-CBC3-SHA$\|^TLS-RSA-WITH-3DES-EDE-CBC-SHA$\'',
+        'compat' => '-m tls1_1 -f \'^DES-CBC3-SHA$\|^TLS-RSA-WITH-3DES-EDE-CBC-SHA$\'', #'
     },
     'config-suite-b.h' => {
         'compat' => "-m tls1_2 -f 'ECDHE-ECDSA.*AES.*GCM' -p mbedTLS",

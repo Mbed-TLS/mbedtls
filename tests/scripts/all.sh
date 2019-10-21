bfeed663d2e19704596744af8e2cb3378231a06d
Gilles Peskine <Gilles.Peskine@arm.com>
2019-10-21 19:06:33 +0200

    Asan make builds: avoid sanitizer recovery
    Some sanitizers default to displaying an error message and recovering.
    This could result in a test being recorded as passing despite a
    complaint from the sanitizer. Turn off sanitizer recovery to avoid
    this risk.

--- tests/scripts/all.sh
+++ tests/scripts/all.sh
@@ -129,7 +129,7 @@ pre_initialize_variables () {
     export CTEST_OUTPUT_ON_FAILURE=1
 
     # CFLAGS and LDFLAGS for Asan builds that don't use CMake
-    ASAN_CFLAGS='-Werror -Wall -Wextra -fsanitize=address,undefined'
+    ASAN_CFLAGS='-Werror -Wall -Wextra -fsanitize=address,undefined -fno-sanitize-recover=all'
 
     # Gather the list of available components. These are the functions
     # defined in this script whose name starts with "component_".

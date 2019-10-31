1a9bd94549bfea5824b42c4470eb04f9ae7a4a24
Gilles Peskine <Gilles.Peskine@arm.com>
2019-10-31 16:11:34 +0100

    Disable MBEDTLS_MEMORY_BUFFER_ALLOC_C after config.pl full
    Enabling memory_buffer_alloc is slow and makes ASan ineffective. We
    have a patch pending to remove it from the full config. In the
    meantime, disable it explicitly.

--- tests/scripts/all.sh
+++ tests/scripts/all.sh
@@ -877,6 +877,7 @@ component_test_se_default () {
 component_test_se_full () {
     msg "build: full config + MBEDTLS_PSA_CRYPTO_SE_C"
     scripts/config.pl full
+    scripts/config.pl unset MBEDTLS_MEMORY_BUFFER_ALLOC_C
     scripts/config.pl set MBEDTLS_PSA_CRYPTO_SE_C
     make CC=gcc CFLAGS='-Werror -Wall -Wextra -O2 -fsanitize=address' LDFLAGS='-fsanitize=address'
 

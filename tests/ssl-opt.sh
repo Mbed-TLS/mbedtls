95a17fbe172654c4c54acb032852d3d5d432fab0
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2020-01-02 11:58:00 +0100

    Add test for record compression in ssl-opt.sh
    Deprecated but still needs to be tested.

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -905,6 +905,18 @@ run_test    "Default, DTLS" \
             -s "Protocol is DTLSv1.2" \
             -s "Ciphersuite is TLS-ECDHE-RSA-WITH-CHACHA20-POLY1305-SHA256"
 
+requires_config_enabled MBEDTLS_ZLIB_SUPPORT
+run_test    "Default (compression enabled)" \
+            "$P_SRV debug_level=3" \
+            "$P_CLI debug_level=3" \
+            0 \
+            -s "Allocating compression buffer" \
+            -c "Allocating compression buffer" \
+            -s "Record expansion is unknown (compression)" \
+            -c "Record expansion is unknown (compression)" \
+            -S "error" \
+            -C "error"
+
 # Test current time in ServerHello
 requires_config_enabled MBEDTLS_HAVE_TIME
 run_test    "ServerHello contains gmt_unix_time" \

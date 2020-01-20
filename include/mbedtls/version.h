ee88f8145d0c07bd2b2516e2f414bcd0572dd566
Janos Follath <janos.follath@arm.com>
2020-01-20 14:28:41 +0000

    Bump version to Mbed TLS 2.7.13

--- include/mbedtls/version.h
+++ include/mbedtls/version.h
@@ -40,16 +40,16 @@
  */
 #define MBEDTLS_VERSION_MAJOR  2
 #define MBEDTLS_VERSION_MINOR  7
-#define MBEDTLS_VERSION_PATCH  12
+#define MBEDTLS_VERSION_PATCH  13
 
 /**
  * The single version number has the following structure:
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define MBEDTLS_VERSION_NUMBER         0x02070C00
-#define MBEDTLS_VERSION_STRING         "2.7.12"
-#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.7.12"
+#define MBEDTLS_VERSION_NUMBER         0x02070D00
+#define MBEDTLS_VERSION_STRING         "2.7.13"
+#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.7.13"
 
 #if defined(MBEDTLS_VERSION_C)
 

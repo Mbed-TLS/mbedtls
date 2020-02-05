f1a3828ad8f03c8f6b8978bdfb6351bbf761453d
Hanno Becker <hanno.becker@arm.com>
2020-02-05 16:14:29 +0000

    Adapt preamble for newly created ssl_msg.c

--- library/ssl_msg.c
+++ library/ssl_msg.c
@@ -1,7 +1,8 @@
 /*
- *  SSLv3/TLSv1 shared functions
+ *  Generic SSL/TLS messaging layer functions
+ *  (record layer + retransmission state machine)
  *
- *  Copyright (C) 2006-2015, ARM Limited, All Rights Reserved
+ *  Copyright (C) 2006-2020, ARM Limited, All Rights Reserved
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may

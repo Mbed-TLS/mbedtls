1174db5bac1e4ca0298d6b835e3877490271f777
Gilles Peskine <Gilles.Peskine@arm.com>
2020-02-26 19:52:06 +0100

    Revert "ecp: Remove dependency on TLS and X.509"
    This reverts commit de0a41b716ae4d9e938236771d49a880480eb66e.

--- library/ecp.c
+++ library/ecp.c
@@ -374,7 +374,7 @@ int mbedtls_ecp_check_budget( const mbedtls_ecp_group *grp,
  * Curves are listed in order: largest curves first, and for a given size,
  * fastest curves first. This provides the default order for the SSL module.
  *
- * Reminder: update profiles in Mbed TLS's x509_crt.c when adding new curves!
+ * Reminder: update profiles in x509_crt.c when adding a new curves!
  */
 static const mbedtls_ecp_curve_info ecp_supported_curves[] =
 {

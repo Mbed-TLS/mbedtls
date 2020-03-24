399b82f98665427d9491195b4d53ce30c46adafb
Gilles Peskine <Gilles.Peskine@arm.com>
2020-03-24 18:36:56 +0100

    Pylint: minor code simplifications
    Simplify the code in minor ways. Each of this changes fixes a warning
    from Pylint 2.4 that doesn't appear with Pylint 1.7.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/scripts/generate_test_code.py
+++ tests/scripts/generate_test_code.py
@@ -402,8 +402,7 @@ def parse_dependencies(inp_str):
     :param inp_str: Input string with macros delimited by ':'.
     :return: list of dependencies
     """
-    dependencies = [dep for dep in map(validate_dependency,
-                                       inp_str.split(':'))]
+    dependencies = list(map(validate_dependency, inp_str.split(':')))
     return dependencies
 
 

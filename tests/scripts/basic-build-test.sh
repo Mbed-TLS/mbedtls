ed1f6735259eddbce4473e1ffdac2aa3909ab5c9
Gilles Peskine <Gilles.Peskine@arm.com>
2020-04-09 18:32:48 +0200

    If 'make lcov' failed, exit immediately
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/scripts/basic-build-test.sh
+++ tests/scripts/basic-build-test.sh
@@ -100,7 +100,15 @@ echo
 
 # Step 3 - Process the coverage report
 cd ..
-make lcov |tee tests/cov-$TEST_OUTPUT
+{
+    make lcov
+    echo SUCCESS
+} | tee tests/cov-$TEST_OUTPUT
+
+if [ "$(tail -n1 tests/cov-$TEST_OUTPUT)" != "SUCCESS" ]; then
+    echo >&2 "Fatal: 'make lcov' failed"
+    exit 2
+fi
 
 
 # Step 4 - Summarise the test report

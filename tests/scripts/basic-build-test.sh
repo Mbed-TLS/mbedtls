c877c24ed0514ad0e398d0fd6ca7db3593bef740
Gilles Peskine <Gilles.Peskine@arm.com>
2020-04-10 00:18:55 +0200

    Enable extra features that have tests
    The "full" configuration excludes some deprecated or experimental
    features. Enable the ones that have tests, don't have extra
    requirements and don't turn off some other feature.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/scripts/basic-build-test.sh
+++ tests/scripts/basic-build-test.sh
@@ -68,6 +68,10 @@ export LDFLAGS=' --coverage'
 make clean
 cp "$CONFIG_H" "$CONFIG_BAK"
 scripts/config.py full
+# Enable some deprecated or experimental features that are not in the
+# full config, but are compatible with it and have tests.
+scripts/config.py set MBEDTLS_SSL_PROTO_SSL3
+scripts/config.py set MBEDTLS_PSA_CRYPTO_SE_C
 make -j
 
 

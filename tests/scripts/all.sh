7c220d7d3783dc3d2792ca87382dd4c22afc20af
Gilles Peskine <Gilles.Peskine@arm.com>
2020-04-20 13:21:27 +0200

    Keep going after a shell "[" a.k.a. "test" fails
    This is necessary to actually keep going and finish the
    component-specific cleanup in component_test_cmake_out_of_source if
    ssl-opt.err is non-empty.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/scripts/all.sh
+++ tests/scripts/all.sh
@@ -522,6 +522,7 @@ pre_setup_keep_going () {
             *[!A-Za-z]"test"|*[!A-Za-z]"test"[!A-Za-z]*) true;;
             "tests/"*) true;;
             "grep "*|"! grep "*) true;;
+            "test "*|"[ "*) true;;
             *) false;;
         esac
     }

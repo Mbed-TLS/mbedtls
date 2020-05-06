97b9253e3553ebed7d7362efdd0cc227e5d65dd0
Gilles Peskine <Gilles.Peskine@arm.com>
2020-05-06 22:20:36 +0200

    New test driver implementing sign_hash with a transparent key
    Only supports deterministic ECDSA with SHA-256.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/psa_crypto_helpers.h
+++ tests/psa_crypto_helpers.h
@@ -74,6 +74,114 @@ static void test_helper_psa_done( int line, const char *file )
 
 
 
+#if defined(MBEDTLS_PSA_CRYPTO_DRIVERS)
+
+#include "mbedtls/md.h"
+#include "mbedtls/ecdsa.h"
+
+/* If non-null, on success, copy this to the output. */
+void *test_driver_forced_output = NULL;
+size_t test_driver_forced_output_length = 0;
+
+psa_status_t test_transparent_signature_sign_hash_status = PSA_ERROR_NOT_SUPPORTED;
+unsigned long test_transparent_signature_sign_hash_hit = 0;
+psa_status_t test_transparent_signature_sign_hash(
+    const psa_key_attributes_t *attributes,
+    const uint8_t *key, size_t key_length,
+    psa_algorithm_t alg,
+    const uint8_t *hash, size_t hash_length,
+    uint8_t *signature, size_t signature_size, size_t *signature_length )
+{
+    ++test_transparent_signature_sign_hash_hit;
+
+    if( test_transparent_signature_sign_hash_status != PSA_SUCCESS )
+        return( test_transparent_signature_sign_hash_status );
+
+    if( test_driver_forced_output != NULL )
+    {
+        if( test_driver_forced_output_length > signature_size )
+            return( PSA_ERROR_BUFFER_TOO_SMALL );
+        memcpy( signature, test_driver_forced_output,
+                test_driver_forced_output_length );
+        *signature_length = test_driver_forced_output_length;
+        return( PSA_SUCCESS );
+    }
+
+#if defined(MBEDTLS_ECDSA_C) && defined(MBEDTLS_ECDSA_DETERMINISTIC) && \
+    defined(MBEDTLS_SHA256_C)
+    if( alg != PSA_ALG_DETERMINISTIC_ECDSA( PSA_ALG_SHA_256 ) )
+        return( PSA_ERROR_GENERIC_ERROR );
+    if( ! PSA_KEY_TYPE_IS_ECC_KEY_PAIR( psa_get_key_type( attributes ) ) )
+        return( PSA_ERROR_INVALID_ARGUMENT );
+
+    /* Beyond this point, the driver is actually doing the work of
+     * calculating the signature. */
+
+    /* FIXME: the current API design forces us to cheat since we're
+     * getting a pointer to something unspecified. The current
+     * implementation happens to put a pointer to the mbedtls
+     * representation here due to the layout of the `data` union in
+     * `psa_key_slot_t`. */
+    mbedtls_ecp_keypair *ecp = (mbedtls_ecp_keypair*) key;
+    (void) key_length;
+
+    /* Code mostly copied from psa_ecdsa_sign() in psa_crypto.c. */
+    int ret;
+    mbedtls_mpi r, s;
+    size_t curve_bytes = PSA_BITS_TO_BYTES( ecp->grp.pbits );
+    mbedtls_md_type_t md_alg = MBEDTLS_MD_SHA256;
+    mbedtls_mpi_init( &r );
+    mbedtls_mpi_init( &s );
+    if( signature_size < 2 * curve_bytes )
+        return( PSA_ERROR_BUFFER_TOO_SMALL );
+    MBEDTLS_MPI_CHK( mbedtls_ecdsa_sign_det( &ecp->grp, &r, &s, &ecp->d,
+                                  hash, hash_length, md_alg ) );
+    MBEDTLS_MPI_CHK( mbedtls_mpi_write_binary( &r,
+                                               signature,
+                                               curve_bytes ) );
+    MBEDTLS_MPI_CHK( mbedtls_mpi_write_binary( &s,
+                                               signature + curve_bytes,
+                                               curve_bytes ) );
+cleanup:
+    mbedtls_mpi_free( &r );
+    mbedtls_mpi_free( &s );
+    if( ret == 0 )
+    {
+        *signature_length = 2 * curve_bytes;
+        return( PSA_SUCCESS );
+    }
+    else
+#endif /* defined(MBEDTLS_ECDSA_C) && defined(MBEDTLS_ECDSA_DETERMINISTIC) && \
+          defined(MBEDTLS_SHA256_C) */
+    {
+        /* Something bad happened. There's no easy way to translate the
+         * error code. Use a debugger if this comes up. */
+        return( PSA_ERROR_GENERIC_ERROR );
+    }
+}
+
+psa_status_t test_opaque_signature_sign_hash(
+    const psa_key_attributes_t *attributes,
+    const uint8_t *key, size_t key_length,
+    psa_algorithm_t alg,
+    const uint8_t *hash, size_t hash_length,
+    uint8_t *signature, size_t signature_size, size_t *signature_length )
+{
+    (void) attributes;
+    (void) key;
+    (void) key_length;
+    (void) alg;
+    (void) hash;
+    (void) hash_length;
+    (void) signature;
+    (void) signature_size;
+    (void) signature_length;
+    return( PSA_ERROR_NOT_SUPPORTED );
+}
+#endif /* MBEDTLS_PSA_CRYPTO_DRIVERS */
+
+
+
 #if defined(RECORD_PSA_STATUS_COVERAGE_LOG)
 #include <psa/crypto.h>
 

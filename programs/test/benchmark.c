e22da99224050305777add1c74b81092d09c6006
Zhai Zhaoxuan <kxuanobj@gmail.com>
2020-05-09 23:50:32 +0800

    Fix typo in program benchmark.
    Signed-off-by: Zhai Zhaoxuan <kxuanobj@gmail.com>

--- programs/test/benchmark.c
+++ programs/test/benchmark.c
@@ -101,7 +101,7 @@ int main( void )
 #define OPTIONS                                                         \
     "md4, md5, ripemd160, sha1, sha256, sha512,\n"                      \
     "arc4, des3, des, camellia, blowfish, chacha20,\n"                  \
-    "aes_cbc, aes_gcm, aes_ccm, aes_ctx, chachapoly,\n"                 \
+    "aes_cbc, aes_gcm, aes_ccm, aes_xts, chachapoly,\n"                 \
     "aes_cmac, des3_cmac, poly1305\n"                                   \
     "havege, ctr_drbg, hmac_drbg\n"                                     \
     "rsa, dhm, ecdsa, ecdh.\n"

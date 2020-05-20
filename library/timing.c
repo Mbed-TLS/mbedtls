479d8de31d54591d0157f2dd21323ba55b8c7ab2
Ørjan Malde <orjan.malde@foxi.me>
2020-05-20 09:32:39 +0000

    Add support for midipix, a POSIX layer for Microsoft Windows
    Signed-off-by: Ørjan Malde <orjan.malde@foxi.me>

--- library/timing.c
+++ library/timing.c
@@ -40,7 +40,7 @@
 
 #if !defined(unix) && !defined(__unix__) && !defined(__unix) && \
     !defined(__APPLE__) && !defined(_WIN32) && !defined(__QNXNTO__) && \
-    !defined(__HAIKU__)
+    !defined(__HAIKU__) && !defined(__midipix__)
 #error "This module only works on Unix and Windows, see MBEDTLS_TIMING_C in config.h"
 #endif
 

5a0dbb3fda88901d682ed48c31529a96a9e32961
Daniel Axtens <dja@axtens.net>
2020-05-28 11:43:41 +1000

    Do not include time.h without MBEDTLS_HAVE_TIME
    MBEDTLS_HAVE_TIME is documented as: "System has time.h and time()."
    
    If that is not defined, do not attempt to include time.h.
    
    A particular problem is platform-time.h, which should only be included if
    MBEDTLS_HAVE_TIME is defined, which makes everything messier. Maybe it
    should be refactored to have the check inside the header.
    
    Signed-off-by: Daniel Axtens <dja@axtens.net>

--- programs/fuzz/common.c
+++ programs/fuzz/common.c
@@ -4,15 +4,17 @@
 #include <stdlib.h>
 #include "mbedtls/ctr_drbg.h"
 
+#if defined(MBEDTLS_HAVE_TIME)
 mbedtls_time_t dummy_constant_time( mbedtls_time_t* time )
 {
     (void) time;
     return 0x5af2a056;
 }
+#endif
 
 void dummy_init()
 {
-#if defined(MBEDTLS_PLATFORM_TIME_ALT)
+#if (defined(MBEDTLS_HAVE_TIME) && defined(MBEDTLS_PLATFORM_TIME_ALT))
     mbedtls_platform_set_time( dummy_constant_time );
 #else
     fprintf(stderr, "Warning: fuzzing without constant time\n");

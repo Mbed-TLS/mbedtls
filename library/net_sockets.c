9911e4add4ae4a6a3b6a5590828d5addb3eb84ab
Flavio Ceolin <flavio.ceolin@intel.com>
2020-05-27 22:41:19 -0700

    Check if feature macro is defined before define it
    Zephyr's native posix port define _POSIX_C_SOURCE with a higher value
    during the build, so when mbedTLS defines it with a different value
    breaks the build.
    
    As Zephyr is already defining a higher value is guaranteed that mbedTLS
    required features will be available. So, just define it in case it was
    not defined before.
    
    [taken from Zephyr mbedtls module:
    https://github.com/zephyrproject-rtos/mbedtls/commit/76dcd6eecae4c1b556a52b1b9ef66e75fc538bc5]
    
    Signed-off-by: Flavio Ceolin <flavio.ceolin@intel.com>
    Signed-off-by: David Brown <david.brown@linaro.org>

--- library/net_sockets.c
+++ library/net_sockets.c
@@ -47,10 +47,14 @@
 /* Enable definition of getaddrinfo() even when compiling with -std=c99. Must
  * be set before config.h, which pulls in glibc's features.h indirectly.
  * Harmless on other platforms. */
+#ifndef _POSIX_C_SOURCE
 #define _POSIX_C_SOURCE 200112L
+#endif
 
 #if defined(__NetBSD__)
+#ifndef _XOPEN_SOURCE
 #define _XOPEN_SOURCE 600 /* sockaddr_storage */
+#endif
 #endif
 
 #if !defined(MBEDTLS_CONFIG_FILE)

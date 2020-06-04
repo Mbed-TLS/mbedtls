16bf0e234663936ff533292ef64fba7001f694dc
Hanno Becker <hanno.becker@arm.com>
2020-06-04 13:27:34 +0100

    Fix debug print of explicit IV
    The previous version attempted to write the explicit IV from
    the destination buffer before it has been written there.
    
    Signed-off-by: Hanno Becker <hanno.becker@arm.com>

--- library/ssl_msg.c
+++ library/ssl_msg.c
@@ -853,8 +853,8 @@ int mbedtls_ssl_encrypt_buf( mbedtls_ssl_context *ssl,
         MBEDTLS_SSL_DEBUG_BUF( 4, "IV used (internal)",
                                iv, transform->ivlen );
         MBEDTLS_SSL_DEBUG_BUF( 4, "IV used (transmitted)",
-                               data - dynamic_iv_len * dynamic_iv_is_explicit,
-                               dynamic_iv_len * dynamic_iv_is_explicit );
+                               dynamic_iv,
+                               dynamic_iv_is_explicit ? dynamic_iv_len : 0 );
         MBEDTLS_SSL_DEBUG_BUF( 4, "additional data used for AEAD",
                                add_data, add_data_len );
         MBEDTLS_SSL_DEBUG_MSG( 3, ( "before encrypt: msglen = %d, "

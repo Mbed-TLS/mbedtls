2e865e5e494a4b5fa4ee812f2e517eda6ce4e276
Brent Gardner <bgardner@squarelabs.net>
2020-07-08 10:44:16 -0600

    Add linker

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ echo "Performing build"
 mkdir -p build
 cd build
 cmake -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=1 \
+  -DCMAKE_LINKER=/opt/android-ndk-r20b/android-ndk-r20b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ld \
   -DCMAKE_C_COMPILER=/opt/android-ndk-r20b/android-ndk-r20b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang \
   -DCMAKE_CXX_COMPILER=/opt/android-ndk-r20b/android-ndk-r20b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang++ \
   ..

e3f99847da6e611f8e4e63c51b8ad20b8b020f07
Brent Gardner <bgardner@squarelabs.net>
2020-07-08 11:08:49 -0600

    fix abi?

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir -p build
 cd build
 cmake -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=1 \
   -DCMAKE_TOOLCHAIN_FILE=/opt/android-ndk-r20b/android-ndk-r20b/build/cmake/android.toolchain.cmake \
-  -DANDROID_ABI=24 \
+  -DANDROID_ABI=arm64-v8a \
   -DANDROID_NATIVE_API_LEVEL=25 \
   ..
 cd -

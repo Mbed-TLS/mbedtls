66f729091f9f9e6e1a8009e1d2a82834eb28e697
Brent Gardner <bgardner@squarelabs.net>
2020-07-08 12:40:57 -0600

    Try to parameterize, but fail

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ echo "Performing build"
 mkdir -p build
 cd build
 cmake -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=1 \
-  -DCMAKE_TOOLCHAIN_FILE=/opt/android-ndk-r20b/android-ndk-r20b/build/cmake/android.toolchain.cmake \
-  -DANDROID_ABI=arm64-v8a \
-  -DANDROID_NATIVE_API_LEVEL=25 \
+  -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake \
+  -DANDROID_ABI=${ANDROID_ABI} \
+  -DANDROID_NATIVE_API_LEVEL=${ANDROID_PLATFORM} \
   ..
 cd -
 cmake --build ./build -- -j`nproc`

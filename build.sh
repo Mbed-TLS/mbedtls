33910ad0371807010d204855c1145536220a014f
Brent Gardner <bgardner@squarelabs.net>
2020-07-08 10:10:12 -0600

    Fix build command

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ cmake -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=1 \
   -DCMAKE_C_COMPILER=/opt/android-ndk-r20b/android-ndk-r20b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang \
   -DCMAKE_CXX_COMPILER=/opt/android-ndk-r20b/android-ndk-r20b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang++ \
   -B build -S .
-cmake --build ./build -j`nproc`
+cmake --build ./build -- -j`nproc`

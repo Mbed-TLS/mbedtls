b7874c18c7052cd688d776a519d6813819ec1073
Brent Gardner <bgardner@squarelabs.net>
2020-07-08 10:38:33 -0600

    wtf yolo

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 echo "Performing build"
 mkdir -p build
+cd build
 cmake -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=1 \
   -DCMAKE_C_COMPILER=/opt/android-ndk-r20b/android-ndk-r20b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang \
   -DCMAKE_CXX_COMPILER=/opt/android-ndk-r20b/android-ndk-r20b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android24-clang++ \
-  -B ./build -S .
+  ..
+cd -
 cmake --build ./build -- -j`nproc`

a77067a2ddb5abffd88d45dbd4f6ec3c9dea8c78
Brent Gardner <bgardner@squarelabs.net>
2020-07-08 09:24:08 -0600

    Try to cross compile for android

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Renaming asset to a descriptive filename"
+BUILD_TIME=`date +%F_%T`
+FILENAME=/opt/vhserver-${DRONE_SOURCE_BRANCH}-${BUILD_TIME}-${DRONE_COMMIT_SHA:0:8}.apk
+cp ./app/build/outputs/apk/release/app-release.apk $FILENAME
+echo "New filename: " $FILENAME
+
+echo "Some useful env info"
+curl --version
+ls -l $FILENAME
+
+echo "Uploading asset to Nexus"
+curl -v --user "vhtest:$NEXUS_PWD" --upload-file $FILENAME https://nexus.mersive.xyz/repository/vhserver/${DRONE_SOURCE_BRANCH}/
---
+++ setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Some useful env info"
+printenv
+echo ${NEXUS_PWD} | sha256sum
+
+echo "Installing the necessary packages"
+apt-get -qq update && apt-get -qq install -y openjdk-8-jdk unzip wget curl git python python3
+mkdir -p ~/.ssh/
+mkdir -p /opt/
+
+echo "Downloading and installing the Android SDK"
+DL_TARGET_SDK=/opt/android-sdk.zip
+curl -sSf --user "vhtest:$NEXUS_PWD" -o $DL_TARGET_SDK https://nexus.mersive.xyz/repository/vhserver/sdk-tools-linux-4333796.zip
+mkdir -p $ANDROID_HOME
+unzip -q -d $ANDROID_HOME $DL_TARGET_SDK
+
+echo "Configuring the SDK"
+mkdir -p /root/.android
+touch /root/.android/repositories.cfg
+yes | sdkmanager --licenses > /dev/null
+sdkmanager "tools" "platform-tools" "build-tools;25.0.3" "platforms;android-25" "add-ons;addon-google_apis-google-24" > /dev/null
+sdkmanager "extras;android;m2repository" "extras;google;google_play_services" "patcher;v4" > /dev/null
+
+echo "Downloading and installing the Android NDK"
+DL_TARGET_NDK=/opt/android-ndk-r20b-linux-x86_64.zip
+wget -q -O $DL_TARGET_NDK https://dl.google.com/android/repository/android-ndk-r20b-linux-x86_64.zip
+unzip -q -d $ANDROID_NDK_HOME $DL_TARGET_NDK

fa452c4566745721add9e305ada68c6d9801e284
Hanno Becker <hanno.becker@arm.com>
2020-08-14 15:42:49 +0100

    Fix guard in ECJPAKE tests in ssl-opt.sh
    Three tests were guarded by `MBEDTLS_KEY_EXCHANGE_ECJPAKE`,
    not `MBEDTLS_KEY_EXCHANGE_ECJPAKE_ENABLED`, as it should be.
    
    Curiously, the guard still functioned as intended, perhaps
    because `MBEDTLS_KEY_EXCHANGE_ECJPAKE` is a prefix of
    `MBEDTLS_KEY_EXCHANGE_ECJPAKE_ENABLED`.
    
    Signed-off-by: Hanno Becker <hanno.becker@arm.com>

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -5740,7 +5740,7 @@ run_test    "PSK callback: wrong key" \
 
 # Tests for EC J-PAKE
 
-requires_config_enabled MBEDTLS_KEY_EXCHANGE_ECJPAKE
+requires_config_enabled MBEDTLS_KEY_EXCHANGE_ECJPAKE_ENABLED
 run_test    "ECJPAKE: client not configured" \
             "$P_SRV debug_level=3" \
             "$P_CLI debug_level=3" \
@@ -5754,7 +5754,7 @@ run_test    "ECJPAKE: client not configured" \
             -C "found ecjpake_kkpp extension" \
             -S "None of the common ciphersuites is usable"
 
-requires_config_enabled MBEDTLS_KEY_EXCHANGE_ECJPAKE
+requires_config_enabled MBEDTLS_KEY_EXCHANGE_ECJPAKE_ENABLED
 run_test    "ECJPAKE: server not configured" \
             "$P_SRV debug_level=3" \
             "$P_CLI debug_level=3 ecjpake_pw=bla \
@@ -5769,7 +5769,7 @@ run_test    "ECJPAKE: server not configured" \
             -C "found ecjpake_kkpp extension" \
             -s "None of the common ciphersuites is usable"
 
-requires_config_enabled MBEDTLS_KEY_EXCHANGE_ECJPAKE
+requires_config_enabled MBEDTLS_KEY_EXCHANGE_ECJPAKE_ENABLED
 run_test    "ECJPAKE: working, TLS" \
             "$P_SRV debug_level=3 ecjpake_pw=bla" \
             "$P_CLI debug_level=3 ecjpake_pw=bla \

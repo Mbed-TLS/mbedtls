0db765ac653c46f6732f9aefb3f4a61f3c26ec6e
Janos Follath <janos.follath@arm.com>
2020-08-26 15:28:10 +0100

    Bump version to Mbed TLS 2.7.17
    Executed "./scripts/bump_version.sh --version 2.7.17"
    
    Signed-off-by: Janos Follath <janos.follath@arm.com>

--- include/mbedtls/version.h
+++ include/mbedtls/version.h
@@ -65,16 +65,16 @@
  */
 #define MBEDTLS_VERSION_MAJOR  2
 #define MBEDTLS_VERSION_MINOR  7
-#define MBEDTLS_VERSION_PATCH  16
+#define MBEDTLS_VERSION_PATCH  17
 
 /**
  * The single version number has the following structure:
  *    MMNNPP00
  *    Major version | Minor version | Patch version
  */
-#define MBEDTLS_VERSION_NUMBER         0x02071000
-#define MBEDTLS_VERSION_STRING         "2.7.16"
-#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.7.16"
+#define MBEDTLS_VERSION_NUMBER         0x02071100
+#define MBEDTLS_VERSION_STRING         "2.7.17"
+#define MBEDTLS_VERSION_STRING_FULL    "mbed TLS 2.7.17"
 
 #if defined(MBEDTLS_VERSION_C)
 

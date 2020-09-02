a0c2d19d8451d95b2af7d48a429b1c469c656910
Victor Krasnoshchok <ct3da21164@protonmail.ch>
2020-09-03 00:07:05 +0300

    Code style fix #3175
    Signed-off-by: Victor Krasnoshchok <ct3da21164@protonmail.ch>

--- library/entropy.c
+++ library/entropy.c
@@ -466,7 +466,7 @@ int mbedtls_entropy_update_nv_seed( mbedtls_entropy_context *ctx )
 #if defined(MBEDTLS_FS_IO)
 int mbedtls_entropy_write_seed_file( mbedtls_entropy_context *ctx, const char *path )
 {
-    int ret = 0;
+    int ret = MBEDTLS_ERR_ERROR_CORRUPTION_DETECTED;
     FILE *f = NULL;
     unsigned char buf[MBEDTLS_ENTROPY_BLOCK_SIZE];
 
@@ -487,6 +487,8 @@ int mbedtls_entropy_write_seed_file( mbedtls_entropy_context *ctx, const char *p
         ret = MBEDTLS_ERR_ENTROPY_FILE_IO_ERROR;
         goto exit;
     }
+
+    ret = 0;
 
 exit:
     mbedtls_platform_zeroize( buf, sizeof( buf ) );

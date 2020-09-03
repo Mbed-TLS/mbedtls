4b37fba90010a0a383c9ecfc1aa99758cb230c0b
Carl Aage Amundsen <Carl.Amundsen@silabs.com>
2020-09-03 19:25:18 +0200

    Update copyright notice.
    Signed-off-by: Carl Aage Amundsen <Carl.Amundsen@silabs.com>

--- tests/include/opaque_test_driver.h
+++ tests/include/opaque_test_driver.h
@@ -1,12 +1,11 @@
 /**
  * \file opaque_test_driver.h
  *
- * \brief   This file contains prototypes for the opaque driver implementation
- *          sample.
+ * \brief   This file contains prototypes for the opaque test driver.
  */
 
 /*
- *  Copyright (C) 2020, ARM Limited, All Rights Reserved
+ *  Copyright The Mbed TLS Contributors
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
@@ -20,8 +19,6 @@
  *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
- *
- *  This file is part of mbed TLS (https://tls.mbed.org)
  */
 
 #ifndef OPAQUE_TEST_DRIVER_H

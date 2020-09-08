58c5cea73b7763f714f056263a40da56aeb84ca5
Hanno Becker <hanno.becker@arm.com>
2020-09-08 10:31:33 +0100

    Include common.h from ssl_tls13_keys.c
    `common.h` takes care of the logic of chosing the correct
    configuration file, so we don't need to replicate it in
    each source file.
    
    Signed-off-by: Hanno Becker <hanno.becker@arm.com>

--- library/ssl_tls13_keys.c
+++ library/ssl_tls13_keys.c
@@ -17,11 +17,7 @@
  *  limitations under the License.
  */
 
-#if !defined(MBEDTLS_CONFIG_FILE)
-#include "mbedtls/config.h"
-#else
-#include MBEDTLS_CONFIG_FILE
-#endif
+#include "common.h"
 
 #if defined(MBEDTLS_SSL_PROTO_TLS1_3_EXPERIMENTAL)
 

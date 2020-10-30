0c3752a46beda8be9ddb6134ae4d12cb2f4b6b15
Ronald Cron <ronald.cron@arm.com>
2020-10-30 11:54:03 +0100

    psa: slot mgmt: Add unaccessed slots counter in stats
    Add a counter of unaccessed slots and use it
    in tests to check that at the end of PSA tests
    all key slot are unaccessed.
    
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- library/psa_crypto_slot_management.c
+++ library/psa_crypto_slot_management.c
@@ -408,6 +408,10 @@ void mbedtls_psa_get_stats( mbedtls_psa_stats_t *stats )
     for( slot_idx = 0; slot_idx < PSA_KEY_SLOT_COUNT; slot_idx++ )
     {
         const psa_key_slot_t *slot = &global_data.key_slots[ slot_idx ];
+        if( ! psa_is_key_slot_accessed( slot ) )
+        {
+            ++stats->unaccessed_slots;
+        }
         if( ! psa_is_key_slot_occupied( slot ) )
         {
             ++stats->empty_slots;
--- tests/include/test/psa_crypto_helpers.h
+++ tests/include/test/psa_crypto_helpers.h
@@ -24,6 +24,7 @@
 #include "test/psa_helpers.h"
 
 #include <psa/crypto.h>
+#include <psa_crypto_slot_management.h>
 
 static int test_helper_is_psa_pristine( int line, const char *file )
 {
@@ -40,6 +41,10 @@ static int test_helper_is_psa_pristine( int line, const char *file )
         msg = "An external slot has not been closed properly.";
     else if( stats.half_filled_slots != 0 )
         msg = "A half-filled slot has not been cleared properly.";
+    else if( stats.unaccessed_slots != PSA_KEY_SLOT_COUNT )
+    {
+        msg = "Some slots are still marked as accessed.";
+    }
 
     /* If the test has already failed, don't overwrite the failure
      * information. Do keep the stats lookup above, because it can be

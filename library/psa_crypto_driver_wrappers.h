2470861f72a5d02aaa8c7ac8e229a6768b32b163
Ronald Cron <ronald.cron@arm.com>
2020-11-26 15:16:05 +0100

    psa: Call export software implementation as a driver
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- library/psa_crypto_driver_wrappers.h
+++ library/psa_crypto_driver_wrappers.h
@@ -53,6 +53,11 @@ psa_status_t psa_driver_wrapper_import_key(
     uint8_t *key_buffer, size_t key_buffer_size,
     size_t *key_buffer_length, size_t *bits );
 
+psa_status_t psa_driver_wrapper_export_key(
+    const psa_key_attributes_t *attributes,
+    const uint8_t *key_buffer, size_t key_buffer_size,
+    uint8_t *data, size_t data_size, size_t *data_length );
+
 psa_status_t psa_driver_wrapper_export_public_key(
     const psa_key_attributes_t *attributes,
     const uint8_t *key_buffer, size_t key_buffer_size,

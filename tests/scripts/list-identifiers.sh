1b52ee9b1646f1094e4545fb3948b5aa54a73475
Mateusz Starzyk <mateusz.starzyk@mobica.com>
2021-02-16 14:07:47 +0100

    Remove 1.3 to 2.0 transition helpers files.
    Signed-off-by: Mateusz Starzyk <mateusz.starzyk@mobica.com>

--- tests/scripts/list-identifiers.sh
+++ tests/scripts/list-identifiers.sh
@@ -47,9 +47,9 @@ done
 
 if [ $INTERNAL ]
 then
-    HEADERS=$( ls include/mbedtls/*_internal.h library/*.h | egrep -v 'compat-1\.3\.h|bn_mul' )
+    HEADERS=$( ls include/mbedtls/*_internal.h library/*.h | egrep -v 'bn_mul' )
 else
-    HEADERS=$( ls include/mbedtls/*.h include/psa/*.h library/*.h | egrep -v 'compat-1\.3\.h|bn_mul' )
+    HEADERS=$( ls include/mbedtls/*.h include/psa/*.h library/*.h | egrep -v 'bn_mul' )
     HEADERS="$HEADERS 3rdparty/everest/include/everest/everest.h 3rdparty/everest/include/everest/x25519.h"
 fi
 

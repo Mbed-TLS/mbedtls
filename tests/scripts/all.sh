753f973f8721bd4b4fc5fb496c88616ff6a0e514
Steven Cooreman <steven.cooreman@silabs.com>
2021-03-15 11:38:44 +0100

    Use full config during driver testing
    Due to the way the test drivers are setup, we require the full setup.
    
    Signed-off-by: Steven Cooreman <steven.cooreman@silabs.com>

--- tests/scripts/all.sh
+++ tests/scripts/all.sh
@@ -2235,6 +2235,7 @@ component_test_se_default () {
 
 component_test_psa_crypto_drivers () {
     msg "build: MBEDTLS_PSA_CRYPTO_DRIVERS w/ driver hooks"
+    scripts/config.py full
     scripts/config.py set MBEDTLS_PSA_CRYPTO_DRIVERS
     # Need to define the correct symbol and include the test driver header path in order to build with the test driver
     loc_cflags="$ASAN_CFLAGS -DPSA_CRYPTO_DRIVER_TEST"
@@ -2258,7 +2259,7 @@ component_test_psa_crypto_drivers () {
     make CC=gcc CFLAGS="${loc_cflags}" LDFLAGS="$ASAN_CFLAGS"
     unset loc_cflags
 
-    msg "test: MBEDTLS_PSA_CRYPTO_DRIVERS, signature"
+    msg "test: full + MBEDTLS_PSA_CRYPTO_DRIVERS"
     make test
 }
 

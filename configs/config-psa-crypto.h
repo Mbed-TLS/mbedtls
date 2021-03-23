f0ae0b9a03fef7f3a0d740c4854ab1a4ac4b5cfb
Mateusz Starzyk <mateusz.starzyk@mobica.com>
2021-03-23 14:32:42 +0100

    Remove PKCS#11 library wrapper.
    Signed-off-by: Mateusz Starzyk <mateusz.starzyk@mobica.com>

--- configs/config-psa-crypto.h
+++ configs/config-psa-crypto.h
@@ -2478,21 +2478,6 @@
 #define MBEDTLS_PKCS5_C
 
 /**
- * \def MBEDTLS_PKCS11_C
- *
- * Enable wrapper for PKCS#11 smartcard support.
- *
- * Module:  library/pkcs11.c
- * Caller:  library/pk.c
- *
- * Requires: MBEDTLS_PK_C
- *
- * This module enables SSL/TLS PKCS #11 smartcard support.
- * Requires the presence of the PKCS#11 helper library (libpkcs11-helper)
- */
-//#define MBEDTLS_PKCS11_C
-
-/**
  * \def MBEDTLS_PKCS12_C
  *
  * Enable PKCS#12 PBE functions.

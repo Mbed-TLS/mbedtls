8ed22d344d8424effc331ad60c31beeb2819b406
Nick Child <nick.child@ibm.com>
2021-04-20 15:14:33 -0400

    pkcs7.c: Close file if alloc fails
    The static analysis tool `cppcheck` will show that the function `mbedtls_pkcs7_load_file` does not properly close the opened file if memory allocation fails for the data `buf`. This commit ensures that, if a memory allocation error does occur, the  opened file will be closed before returning.
    
    Signed-off-by: Nick Child <nick.child@ibm.com>

--- library/pkcs7.c
+++ library/pkcs7.c
@@ -74,7 +74,11 @@ int mbedtls_pkcs7_load_file( const char *path, unsigned char **buf, size_t *n )
 
     *buf = mbedtls_calloc( 1, *n + 1 );
     if( *buf == NULL )
+    {
+        fclose( file );
+
         return( MBEDTLS_ERR_PKCS7_ALLOC_FAILED );
+    }
 
     if( fread( *buf, 1, *n, file ) != *n )
     {

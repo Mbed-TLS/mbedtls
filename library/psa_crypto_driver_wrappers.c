ce1d8d2c4ed4e5f11384374b5bd49327a590583e
Ronald Cron <ronald.cron@arm.com>
2021-04-30 17:00:34 +0200

    tests: Revert test_driver.h name change
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- library/psa_crypto_driver_wrappers.c
+++ library/psa_crypto_driver_wrappers.c
@@ -37,7 +37,7 @@
 #ifndef PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT
 #define PSA_CRYPTO_ACCELERATOR_DRIVER_PRESENT
 #endif
-#include "test/drivers/mbedtls_test_driver.h"
+#include "test/drivers/test_driver.h"
 #endif /* PSA_CRYPTO_DRIVER_TEST */
 
 /* Repeat above block for each JSON-declared driver during autogeneration */

e95bd6e2648a418c3568fc3d64152e91ff63eb2a
toth92g <toth92g@gmail.com>
2021-05-04 11:31:03 +0200

    Replacing tabs with spaces
    Signed-off-by: toth92g <toth92g@gmail.com>

--- library/x509_crt.c
+++ library/x509_crt.c
@@ -660,6 +660,11 @@ static int x509_get_subject_key_id(unsigned char **p,
                MBEDTLS_ERR_ASN1_LENGTH_MISMATCH;
     }
 
+    if (*p != end) {
+        return MBEDTLS_ERR_X509_INVALID_EXTENSIONS +
+               MBEDTLS_ERR_ASN1_LENGTH_MISMATCH;
+    }
+
     return 0;
 }
 

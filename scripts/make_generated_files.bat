d2968bd12226f03c728bde3de06a396eeaeff1f8
Bence Szépkúti <bence.szepkuti@arm.com>
2021-05-06 09:55:44 +0200

    Explicitly specify exit code for "exit /b"
    "exit /b" without an explicit exit value doesn't copy the value of the
    last command executed, causing issues on Jenkins.
    
    Signed-off-by: Bence Szépkúti <bence.szepkuti@arm.com>

--- scripts/make_generated_files.bat
+++ scripts/make_generated_files.bat
@@ -1,9 +1,9 @@
 @rem Generate automatically-generated configuration-independent source files
 @rem and build scripts.
 @rem Perl and Python 3 must be on the PATH.
-perl scripts\generate_errors.pl || exit /b
-perl scripts\generate_query_config.pl || exit /b
-perl scripts\generate_features.pl || exit /b
-perl scripts\generate_visualc_files.pl || exit /b
-python scripts\generate_psa_constants.py || exit /b
-python tests\scripts\generate_psa_tests.py || exit /b
+perl scripts\generate_errors.pl || exit /b 1
+perl scripts\generate_query_config.pl || exit /b 1
+perl scripts\generate_features.pl || exit /b 1
+perl scripts\generate_visualc_files.pl || exit /b 1
+python scripts\generate_psa_constants.py || exit /b 1
+python tests\scripts\generate_psa_tests.py || exit /b 1

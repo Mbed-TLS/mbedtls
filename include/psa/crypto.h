79e1c47a54c0504b42827343df0b61a2e10ef721
Janos Follath <janos.follath@arm.com>
2021-05-25 15:53:13 +0100

    PSA PAKE: explain implicit key confirmation
    Signed-off-by: Janos Follath <janos.follath@arm.com>

--- include/psa/crypto.h
+++ include/psa/crypto.h
@@ -4638,6 +4638,14 @@ psa_status_t psa_pake_input(psa_pake_operation_t *operation,
 
 /** Get implicitly confirmed shared secret from a PAKE.
  *
+ * At this point there is a cryptographic guarantee that only the authenticated
+ * party who used the same password is able to compute the key. But there is no
+ * guarantee that the peer is the party he claims to be and was able to do so.
+ *
+ * That is, the authentication is only implicit (the peer is not authenticated
+ * at this point, and no action should be taken that assume that they are - like
+ * for example accessing restricted files).
+ *
  * This function can be called after the key exchange phase of the operation
  * has completed. It imports the shared secret output of the PAKE into the
  * provided derivation operation. The input step

22984de191c952b1a3c353ccfb909be31cc883b8
gabor-mezei-arm <gabor.mezei@arm.com>
2021-06-24 17:20:22 +0200

    Remove comments
    These comment cannot bring more information than the code does.
    
    Signed-off-by: gabor-mezei-arm <gabor.mezei@arm.com>

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -3269,7 +3269,6 @@ static psa_status_t psa_cipher_setup( psa_cipher_operation_t *operation,
     if( operation->id != 0 )
         return( PSA_ERROR_BAD_STATE );
 
-    /* The requested algorithm must be one that can be processed by cipher. */
     if( ! PSA_ALG_IS_CIPHER( alg ) )
         return( PSA_ERROR_INVALID_ARGUMENT );
 
@@ -3495,7 +3494,6 @@ psa_status_t psa_cipher_encrypt( mbedtls_svc_key_id_t key,
     psa_key_type_t key_type;
     size_t iv_length;
 
-    /* The requested algorithm must be one that can be processed by cipher. */
     if( ! PSA_ALG_IS_CIPHER( alg ) )
         return( PSA_ERROR_INVALID_ARGUMENT );
 

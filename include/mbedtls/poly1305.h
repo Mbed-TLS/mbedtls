594b00b5768493e6200659d22fd22605cb08a870
Lukasz Gniadzik <lukasz.gniadzik@mobica.com>
2021-08-17 17:15:00 +0200

    Reorder fields in mbedtls_poly1305_context structure.
    Goal here is to maximize usage of immediate offset access.
    
    Results:
    
    library/poly1305.o: 2267 -> 2255 (diff: 12)
    
    Based on "tests/scripts/all.sh build_arm_none_eabi_gcc_m0plus".
    
    Signed-off-by: Lukasz Gniadzik <lukasz.gniadzik@mobica.com>

--- include/mbedtls/poly1305.h
+++ include/mbedtls/poly1305.h
@@ -62,10 +62,10 @@ extern "C" {
 
 typedef struct mbedtls_poly1305_context
 {
+    uint8_t queue[16];  /** The current partial block of data. */
     uint32_t r[4];      /** The value for 'r' (low 128 bits of the key). */
     uint32_t s[4];      /** The value for 's' (high 128 bits of the key). */
     uint32_t acc[5];    /** The accumulator number. */
-    uint8_t queue[16];  /** The current partial block of data. */
     size_t queue_len;   /** The number of bytes stored in 'queue'. */
 }
 mbedtls_poly1305_context;

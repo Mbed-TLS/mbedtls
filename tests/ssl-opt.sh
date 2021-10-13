e3b8d9b92a68b071666f494638d6999385052628
Gilles Peskine <Gilles.Peskine@arm.com>
2021-10-13 12:28:02 +0200

    Temporarily disable test cases that fail randomly on the CI
    https://github.com/ARMmbed/mbedtls/issues/5012
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -2694,6 +2694,7 @@ run_test    "Session resume using tickets, DTLS: openssl server" \
             -c "parse new session ticket" \
             -c "a session has been resumed"
 
+requires_config_enabled TEMPORARILY_DISABLED # https://github.com/ARMmbed/mbedtls/issues/5012
 run_test    "Session resume using tickets, DTLS: openssl client" \
             "$P_SRV dtls=1 debug_level=3 tickets=1" \
             "( $O_CLI -dtls -sess_out $SESSION; \
@@ -2894,6 +2895,7 @@ run_test    "Session resume using cache, DTLS: session copy" \
             -s "a session has been resumed" \
             -c "a session has been resumed"
 
+requires_config_enabled TEMPORARILY_DISABLED # https://github.com/ARMmbed/mbedtls/issues/5012
 run_test    "Session resume using cache, DTLS: openssl client" \
             "$P_SRV dtls=1 debug_level=3 tickets=0" \
             "( $O_CLI -dtls -sess_out $SESSION; \

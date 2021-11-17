1e4423b535e4a5f5c9c3426f48a611a4153d0a45
Bence Szépkúti <bence.szepkuti@arm.com>
2021-11-17 14:39:02 +0100

    Remove expected failure from test_psa_compliance
    Issue #5143 was fixed in PR #5180.
    
    Signed-off-by: Bence Szépkúti <bence.szepkuti@arm.com>

--- tests/scripts/test_psa_compliance.py
+++ tests/scripts/test_psa_compliance.py
@@ -33,11 +33,6 @@ import sys
 # Test number 2xx corresponds to the files in the folder
 # psa-arch-tests/api-tests/dev_apis/crypto/test_c0xx
 EXPECTED_FAILURES = {
-    # psa_key_derivation_output_key() returns PSA_ERROR_NOT_PERMITTED instead of
-    # PSA_ERROR_BAD_STATE when called after the operation was aborted.
-    # - Tracked in issue #5143
-    221,
-
     # psa_aead_[encrypt/decrypt]() returns PSA_ERROR_NOT_SUPPORTED instead of
     # PSA_ERROR_INVALID_ARGUMENT when called with an invalid nonce.
     # - Tracked in issue #5144

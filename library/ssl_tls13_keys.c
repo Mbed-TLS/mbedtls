0cb50d65b52140ded42635401e4501351a64295d
Jerry Yu <jerry.h.yu@arm.com>
2021-11-22 18:16:45 +0800

    Fix overflow error
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- library/ssl_tls13_keys.c
+++ library/ssl_tls13_keys.c
@@ -296,7 +296,7 @@ int mbedtls_ssl_tls1_3_evolve_secret(
     int ret = MBEDTLS_ERR_SSL_INTERNAL_ERROR;
     size_t hlen, ilen;
     unsigned char tmp_secret[ MBEDTLS_MD_MAX_SIZE ] = { 0 };
-    unsigned char tmp_input [ MBEDTLS_MD_MAX_SIZE ] = { 0 };
+    unsigned char tmp_input [ MBEDTLS_ECP_MAX_BYTES ] = { 0 };
 
     const mbedtls_md_info_t *md;
     md = mbedtls_md_info_from_type( hash_alg );

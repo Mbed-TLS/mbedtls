5d40b67c6a5b104c259aeafb0e5593a2791a0dde
Jerry Yu <jerry.h.yu@arm.com>
2021-11-22 16:21:46 +0800

    Add TLS1_3 ciphersuites
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -264,7 +264,7 @@ requires_config_value_equals() {
 # Space-separated list of ciphersuites supported by this build of
 # Mbed TLS.
 P_CIPHERSUITES=" $($P_CLI --help 2>/dev/null |
-                   grep TLS- |
+                   grep 'TLS-\|TLS1-3' |
                    tr -s ' \n' ' ')"
 requires_ciphersuite_enabled() {
     case $P_CIPHERSUITES in

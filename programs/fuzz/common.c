ca53459bed53a84ab901002e6e437a8e192c00d8
David Horstmann <david.horstmann@arm.com>
2021-11-29 18:57:10 +0000

    programs/fuzz: Remove superfluous MBEDTLS_HAVE_TIME
    MBEDTLS_HAVE_TIME_ALT implies MBEDTLS_HAVE_TIME, so an extra
    check for MBEDTLS_HAVE_TIME is not needed.
    
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- programs/fuzz/common.c
+++ programs/fuzz/common.c
@@ -15,7 +15,7 @@ mbedtls_time_t dummy_constant_time( mbedtls_time_t* time )
 
 void dummy_init()
 {
-#if (defined(MBEDTLS_HAVE_TIME) && defined(MBEDTLS_PLATFORM_TIME_ALT))
+#if defined(MBEDTLS_PLATFORM_TIME_ALT)
     mbedtls_platform_set_time( dummy_constant_time );
 #else
     fprintf(stderr, "Warning: fuzzing without constant time\n");

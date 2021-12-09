b6a6650a646dc083b3c65e5216535fe5c2c687a0
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2021-12-09 11:11:54 +0100

    psa_asymmetric_encrypt: add test driver impl
    Signed-off-by: Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>

--- include/mbedtls/rsa.h
+++ include/mbedtls/rsa.h
@@ -38,6 +38,60 @@
 #include "mbedtls/threading.h"
 #endif
 
+#if defined(PSA_CRYPTO_DRIVER_TEST)
+#include <psa/crypto_driver_common.h>
+#include <psa/crypto.h>
+
+typedef struct {
+    /* If non-null, on success, copy this to the output. */
+    void *forced_output;
+    size_t forced_output_length;
+    /* If not PSA_SUCCESS, return this error code instead of processing the
+     * function call. */
+    psa_status_t forced_status;
+    /* Count the amount of times one of the rsa driver functions is called. */
+    unsigned long hits;
+} mbedtls_test_driver_rsa_hooks_t;
+
+#define MBEDTLS_TEST_DRIVER_RSA_INIT { NULL, 0, PSA_SUCCESS, 0 }
+
+static inline mbedtls_test_driver_rsa_hooks_t
+     mbedtls_test_driver_rsa_hooks_init( void )
+{
+    const mbedtls_test_driver_rsa_hooks_t v = MBEDTLS_TEST_DRIVER_RSA_INIT;
+    return( v );
+}
+
+extern mbedtls_test_driver_rsa_hooks_t mbedtls_test_driver_rsa_hooks;
+
+psa_status_t mbedtls_test_transparent_asymmetric_encrypt(
+    const psa_key_attributes_t *attributes,
+    const uint8_t *key_buffer,
+    size_t key_buffer_size,
+    psa_algorithm_t alg,
+    const uint8_t *input,
+    size_t input_length,
+    const uint8_t *salt,
+    size_t salt_length,
+    uint8_t *output,
+    size_t output_size,
+    size_t *output_length );
+
+psa_status_t mbedtls_test_opaque_asymmetric_encrypt(
+    const psa_key_attributes_t *attributes,
+    const uint8_t *key,
+    size_t key_length,
+    psa_algorithm_t alg,
+    const uint8_t *input,
+    size_t input_length,
+    const uint8_t *salt,
+    size_t salt_length,
+    uint8_t *output,
+    size_t output_size,
+    size_t *output_length);
+
+#endif /* PSA_CRYPTO_DRIVER_TEST */
+
 /*
  * RSA Error codes
  */

234f318bd7eb6f7597d273d19e2b62faeaebfea5
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2021-12-09 11:13:58 +0100

    psa_asymmetric_encrypt: move build-in impl to mbedtls_psa_asymmetric_encrypt
    Signed-off-by: Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>

--- library/psa_crypto_rsa.h
+++ library/psa_crypto_rsa.h
@@ -211,4 +211,17 @@ psa_status_t mbedtls_psa_rsa_verify_hash(
     const uint8_t *key_buffer, size_t key_buffer_size,
     psa_algorithm_t alg, const uint8_t *hash, size_t hash_length,
     const uint8_t *signature, size_t signature_length );
+
+psa_status_t mbedtls_psa_asymmetric_encrypt( const psa_key_attributes_t *attributes,
+                                             const uint8_t *key_buffer,
+                                             size_t key_buffer_size,
+                                             psa_algorithm_t alg,
+                                             const uint8_t *input,
+                                             size_t input_length,
+                                             const uint8_t *salt,
+                                             size_t salt_length,
+                                             uint8_t *output,
+                                             size_t output_size,
+                                             size_t *output_length );
+
 #endif /* PSA_CRYPTO_RSA_H */

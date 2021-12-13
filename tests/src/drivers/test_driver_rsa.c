71284eabdb0bff469edd48097cf637c8e533257b
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2021-12-13 09:00:52 +0100

    psa_asymmetric_decrypt: add test driver impl
    Signed-off-by: Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>

--- tests/src/drivers/test_driver_rsa.c
+++ tests/src/drivers/test_driver_rsa.c
@@ -80,6 +80,54 @@ psa_status_t mbedtls_test_transparent_asymmetric_encrypt(
     return( PSA_ERROR_NOT_SUPPORTED );
 }
 
+psa_status_t mbedtls_test_transparent_asymmetric_decrypt(
+    const psa_key_attributes_t *attributes,
+    const uint8_t *key_buffer,
+    size_t key_buffer_size,
+    psa_algorithm_t alg,
+    const uint8_t *input,
+    size_t input_length,
+    const uint8_t *salt,
+    size_t salt_length,
+    uint8_t *output,
+    size_t output_size,
+    size_t *output_length )
+{
+    mbedtls_test_driver_rsa_hooks.hits++;
+
+    if( mbedtls_test_driver_rsa_hooks.forced_output != NULL )
+    {
+        if( output_size < mbedtls_test_driver_rsa_hooks.forced_output_length )
+            return( PSA_ERROR_BUFFER_TOO_SMALL );
+
+        memcpy( output,
+                mbedtls_test_driver_rsa_hooks.forced_output,
+                mbedtls_test_driver_rsa_hooks.forced_output_length );
+        *output_length = mbedtls_test_driver_rsa_hooks.forced_output_length;
+
+        return( mbedtls_test_driver_rsa_hooks.forced_status );
+    }
+
+    if( mbedtls_test_driver_rsa_hooks.forced_status != PSA_SUCCESS )
+        return( mbedtls_test_driver_rsa_hooks.forced_status );
+
+#if defined(MBEDTLS_TEST_LIBTESTDRIVER1) && \
+    defined(LIBTESTDRIVER1_MBEDTLS_PSA_BUILTIN_CIPHER)
+    return( libtestdriver1_mbedtls_psa_asymmetric_decrypt(
+                (const libtestdriver1_psa_key_attributes_t *)attributes,
+                key_buffer, key_buffer_size,
+                alg, input, input_length, salt, salt_length,
+                output, output_size, output_length ) );
+#else
+    return( mbedtls_psa_asymmetric_decrypt(
+                attributes, key_buffer, key_buffer_size,
+                alg, input, input_length, salt, salt_length,
+                output, output_size, output_length ) );
+#endif
+
+    return( PSA_ERROR_NOT_SUPPORTED );
+}
+
 /*
  * opaque versions, to do
  */
@@ -110,5 +158,31 @@ psa_status_t mbedtls_test_opaque_asymmetric_encrypt(
     return( PSA_ERROR_NOT_SUPPORTED );
 }
 
+psa_status_t mbedtls_test_opaque_asymmetric_decrypt(
+    const psa_key_attributes_t *attributes,
+    const uint8_t *key,
+    size_t key_length,
+    psa_algorithm_t alg,
+    const uint8_t *input,
+    size_t input_length,
+    const uint8_t *salt,
+    size_t salt_length,
+    uint8_t *output,
+    size_t output_size,
+    size_t *output_length)
+{
+    (void) attributes;
+    (void) key;
+    (void) key_length;
+    (void) alg;
+    (void) input;
+    (void) input_length;
+    (void) salt;
+    (void) salt_length;
+    (void) output;
+    (void) output_size;
+    (void) output_length;
+    return( PSA_ERROR_NOT_SUPPORTED );
+}
 
 #endif /* MBEDTLS_PSA_CRYPTO_DRIVERS && PSA_CRYPTO_DRIVER_TEST */

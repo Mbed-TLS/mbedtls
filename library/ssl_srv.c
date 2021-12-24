ffef9c52d462458fd206854ab6af0073572f5505
Jerry Yu <jerry.h.yu@arm.com>
2021-12-24 22:31:08 +0800

    fix alignment issue
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- library/ssl_srv.c
+++ library/ssl_srv.c
@@ -318,8 +318,8 @@ static int ssl_parse_signature_algorithms_ext( mbedtls_ssl_context *ssl,
 #if defined(MBEDTLS_ECDH_C) || defined(MBEDTLS_ECDSA_C) || \
     defined(MBEDTLS_KEY_EXCHANGE_ECJPAKE_ENABLED)
 static int ssl_parse_supported_groups_ext( mbedtls_ssl_context *ssl,
-                                                const unsigned char *buf,
-                                                size_t len )
+                                           const unsigned char *buf,
+                                           size_t len )
 {
     size_t list_size, our_size;
     const unsigned char *p;

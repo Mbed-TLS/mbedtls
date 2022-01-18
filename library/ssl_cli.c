3caa0edb9ba026d41dd4f323767af175d44d3307
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2022-01-18 12:41:48 +0100

    Remove dead preprocessor code
    There's no way currently (see below regarding the future) that ECC-based
    key exchanges are enabled without ECP_C being defined. So, the #if was
    fully redundant with the checks surrounding the function, as it always
    evaluated to true.
    
    The situation arose as, in the old days (before Mbed TLS 2.0),
    mbedtls_ssl_conf_curves() (or ssl_set_curves() as it was called back
    then) was optional, controlled by its own compile-time option
    POLARSSL_SSL_SET_CURVES. So, in turn mbedtls_ssl_check_curve() depended
    on POLARSSL_SSL_SET_CURVES too, and all calls to it were guarded by
    that.
    
    When it was made non-optional, a blind
    s/POLARSSL_SSL_SET_CURVES/MBEDTLS_ECP_C/ was done, which resulted in
    stupid situations like this with redundant checks for ECP_C.
    
    Note regarding the future: at some point it will be possible to compile
    with ECC-based key exchanges but without ECP_C. This doesn't change
    anything to the reasoning above: mbedtls_ssl_check_curve() will be
    available in all builds where ECC is used; it will just need a new
    definition (with new guards), but that doesn't change anything for its
    callers.
    
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- library/ssl_cli.c
+++ library/ssl_cli.c
@@ -2334,12 +2334,7 @@ static int ssl_check_server_ecdh_params( const mbedtls_ssl_context *ssl )
 
     MBEDTLS_SSL_DEBUG_MSG( 2, ( "ECDH curve: %s", curve_info->name ) );
 
-#if defined(MBEDTLS_ECP_C)
     if( mbedtls_ssl_check_curve( ssl, grp_id ) != 0 )
-#else
-    if( ssl->handshake->ecdh_ctx.grp.nbits < 163 ||
-        ssl->handshake->ecdh_ctx.grp.nbits > 521 )
-#endif
         return( -1 );
 
     MBEDTLS_SSL_DEBUG_ECDH( 3, &ssl->handshake->ecdh_ctx,

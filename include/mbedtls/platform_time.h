fe5fb8e5d24ada0496af88eab4c249e029459787
Andrzej Kurek <andrzej.kurek@arm.com>
2022-01-24 10:33:13 -0500

    doxygen: remove empty platform_time configuration section
    Signed-off-by: Andrzej Kurek <andrzej.kurek@arm.com>

--- include/mbedtls/platform_time.h
+++ include/mbedtls/platform_time.h
@@ -32,14 +32,6 @@
 extern "C" {
 #endif
 
-/**
- * \name SECTION: Module settings
- *
- * The configuration options you can set for this module are in this section.
- * Either change them in config.h or define them on the compiler command line.
- * \{
- */
-
 /*
  * The time_t datatype
  */
@@ -77,5 +69,4 @@ int mbedtls_platform_set_time( mbedtls_time_t (*time_func)( mbedtls_time_t* time
 }
 #endif
 
-/** \} name SECTION: Module settings */
 #endif /* platform_time.h */

e4f28688fd656cf32e0949bc66ea80722a6e9382
Neil Armstrong <narmstrong@baylibre.com>
2022-02-24 15:41:39 +0100

    Fix comment typo in rsa_sign_wrap()
    Signed-off-by: Neil Armstrong <narmstrong@baylibre.com>

--- library/pk_wrap.c
+++ library/pk_wrap.c
@@ -222,7 +222,7 @@ static int rsa_sign_wrap( void *ctx, mbedtls_md_type_t md_alg,
     if( sig_size < *sig_len )
         return( MBEDTLS_ERR_PK_BUFFER_TOO_SMALL );
 
-    /* mbedtls_pk_write_pubkey() expects a full PK context;
+    /* mbedtls_pk_write_key_der() expects a full PK context;
      * re-construct one to make it happy */
     key.pk_info = &pk_info;
     key.pk_ctx = ctx;

2fa1c311cd4ea064d6c4b167986bcf6faf9553eb
Gabor Mezei <gabor.mezei@arm.com>
2022-03-10 15:23:38 +0100

    Remove test dependency
    The SSL ticket rotation test case is enabled when PSA is used.
    
    Signed-off-by: Gabor Mezei <gabor.mezei@arm.com>

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -2816,7 +2816,6 @@ run_test    "Session resume using tickets: basic" \
             -c "a session has been resumed"
 
 requires_config_enabled MBEDTLS_SSL_PROTO_TLS1_2
-requires_config_disabled MBEDTLS_USE_PSA_CRYPTO
 run_test    "Session resume using tickets: manual rotation" \
             "$P_SRV debug_level=3 tickets=1 ticket_rotate=1" \
             "$P_CLI debug_level=3 tickets=1 reconnect=1" \

711d0f5e2949d4b4e1dcc05ed2253ca611cc43fd
Przemek Stekiel <przemyslaw.stekiel@mobica.com>
2022-03-18 13:52:26 +0100

    Add implemetation of ECP keypair export function
    Signed-off-by: Przemek Stekiel <przemyslaw.stekiel@mobica.com>

--- library/ecp.c
+++ library/ecp.c
@@ -3356,6 +3356,30 @@ cleanup:
     return( ret );
 }
 
+/*
+ * Export generic key-pair parameters.
+ */
+int mbedtls_ecp_export(const mbedtls_ecp_keypair *key, mbedtls_ecp_group *grp,
+                       mbedtls_mpi *d, mbedtls_ecp_point *Q)
+{
+    int ret = MBEDTLS_ERR_ERROR_CORRUPTION_DETECTED;
+    ECP_VALIDATE_RET( key != NULL );
+    ECP_VALIDATE_RET( grp != NULL );
+    ECP_VALIDATE_RET( d != NULL );
+    ECP_VALIDATE_RET( Q != NULL );
+
+    if( ( ret = mbedtls_ecp_group_copy( grp, &key->grp ) ) != 0 )
+        return ret;
+
+    if( ( ret = mbedtls_mpi_copy( d, &key->d ) ) != 0 )
+        return ret;
+
+    if( ( ret = mbedtls_ecp_copy( Q, &key->Q ) ) != 0 )
+        return ret;
+
+    return 0;
+}
+
 #if defined(MBEDTLS_SELF_TEST)
 
 /*

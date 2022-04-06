296787f75c44dae2e1f9c7e8674f9696669380c5
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2022-04-06 13:28:27 +0200

    Rm DES from invocations of compat.sh
    It no longer makes sense, either in -e or -f: those ciphersuites have
    been removed anyway.
    
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- tests/scripts/basic-build-test.sh
+++ tests/scripts/basic-build-test.sh
@@ -124,10 +124,10 @@ echo '################ compat.sh ################'
     sh compat.sh -m 'tls1_2 dtls1_2'
     echo
 
-    echo '#### compat.sh: legacy (null, DES)'
+    echo '#### compat.sh: legacy (null)'
     OPENSSL_CMD="$OPENSSL_LEGACY" \
     GNUTLS_CLI="$GNUTLS_LEGACY_CLI" GNUTLS_SERV="$GNUTLS_LEGACY_SERV" \
-    sh compat.sh -e '^$' -f 'NULL\|DES'
+    sh compat.sh -e '^$' -f 'NULL'
     echo
 
     echo '#### compat.sh: next (ARIA, ChaCha)'

1341e214622f0dc4fccb338b1dbb115b58063d30
Gilles Peskine <Gilles.Peskine@arm.com>
2022-04-13 23:04:48 +0200

    Fix name mismatch in section end comment
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- include/mbedtls/config.h
+++ include/mbedtls/config.h
@@ -4102,7 +4102,7 @@
  */
 //#define MBEDTLS_ECDH_VARIANT_EVEREST_ENABLED
 
-/** \} name SECTION: Customisation configuration options */
+/** \} name SECTION: Module configuration options */
 
 /* Target and application specific configurations
  *

4c162464cabb6bd3d9c3c4ddff30482b6af19f76
Dave Rodgman <dave.rodgman@arm.com>
2022-05-17 17:59:14 +0100

    Revert "Fix potential memcpy() UB in mbedtls_asn1_store_named_data()"
    This reverts commit 067c4f1f6cedc23f34b658745d3f63495e6fb1c6.

--- library/asn1write.c
+++ library/asn1write.c
@@ -472,7 +472,7 @@ mbedtls_asn1_named_data *mbedtls_asn1_store_named_data(
         cur->val.len = val_len;
     }
 
-    if( val != NULL && val_len != 0 )
+    if( val != NULL )
         memcpy( cur->val.p, val, val_len );
 
     return( cur );

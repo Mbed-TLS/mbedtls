4b9f7a28c904d23c2cd847d4168ea38ba9cd47e1
Gilles Peskine <Gilles.Peskine@arm.com>
2022-06-20 18:51:18 +0200

    Remove redundant sentence
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- scripts/abi_check.py
+++ scripts/abi_check.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-This script compares the interfaces of two versions of Mbed TLS, looking
+"""This script compares the interfaces of two versions of Mbed TLS, looking
 for backward incompatibilities between two different Git revisions within
 an Mbed TLS repository. It must be run from the root of a Git working tree.
 
@@ -20,7 +19,6 @@ at a configurable location, or are given as a brief list of problems.
 Returns 0 on success, 1 on non-compliance, and 2 if there is an error
 while running the script.
 
-You must run this test from an Mbed TLS root.
 """
 
 # Copyright The Mbed TLS Contributors

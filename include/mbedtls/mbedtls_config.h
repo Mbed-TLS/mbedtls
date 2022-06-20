98473c4523a8f0bc33c07ba32f2503ec6a3ef279
Gilles Peskine <Gilles.Peskine@arm.com>
2022-06-20 18:46:22 +0200

    Officially deprecate MBEDTLS_PSA_CRYPTO_SE_C
    This was intended as experimental, and we've been saying for a long time
    that it's superseded by the "unified driver interface", but we hadn't
    documented that inside the Mbed TLS source code. So announce it as
    deprecated.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- include/mbedtls/mbedtls_config.h
+++ include/mbedtls/mbedtls_config.h
@@ -2686,11 +2686,11 @@
 /**
  * \def MBEDTLS_PSA_CRYPTO_SE_C
  *
- * Enable secure element support in the Platform Security Architecture
+ * Enable dynamic secure element support in the Platform Security Architecture
  * cryptography API.
  *
- * \warning This feature is not yet suitable for production. It is provided
- *          for API evaluation and testing purposes only.
+ * \deprecated This feature is deprecated. Please switch to the driver
+ *             interface enabled by #MBEDTLS_PSA_CRYPTO_DRIVERS.
  *
  * Module:  library/psa_crypto_se.c
  *

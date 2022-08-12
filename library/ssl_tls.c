5f25db20a972ddd73e4051f9b055e36202ab418a
Jerry Yu <jerry.h.yu@arm.com>
2022-08-12 13:56:53 +0800

    Add server only guards for psk callback
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -1788,6 +1788,7 @@ int mbedtls_ssl_set_hs_psk_opaque( mbedtls_ssl_context *ssl,
 }
 #endif /* MBEDTLS_USE_PSA_CRYPTO */
 
+#if defined(MBEDTLS_SSL_SRV_C)
 void mbedtls_ssl_conf_psk_cb( mbedtls_ssl_config *conf,
                      int (*f_psk)(void *, mbedtls_ssl_context *, const unsigned char *,
                      size_t),
@@ -1796,6 +1797,8 @@ void mbedtls_ssl_conf_psk_cb( mbedtls_ssl_config *conf,
     conf->f_psk = f_psk;
     conf->p_psk = p_psk;
 }
+#endif /* MBEDTLS_SSL_SRV_C */
+
 #endif /* MBEDTLS_KEY_EXCHANGE_SOME_PSK_ENABLED */
 
 #if defined(MBEDTLS_USE_PSA_CRYPTO)

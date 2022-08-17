7e16ce3a724b768286c7e334021c1b0ea0fdeabd
Andrzej Kurek <andrzej.kurek@arm.com>
2022-08-17 16:19:39 -0400

    Clarify TLS 1.2 dependencies with and without PSA crypto
    Signed-off-by: Andrzej Kurek <andrzej.kurek@arm.com>

--- include/mbedtls/check_config.h
+++ include/mbedtls/check_config.h
@@ -743,8 +743,8 @@
 #error "MBEDTLS_SHA256_USE_A64_CRYPTO_ONLY defined on non-Aarch64 system"
 #endif
 
-#if defined(MBEDTLS_SSL_PROTO_TLS1_2) && ( !defined(MBEDTLS_SHA1_C) &&     \
-    !defined(MBEDTLS_SHA256_C) && !defined(MBEDTLS_SHA512_C) )
+#if defined(MBEDTLS_SSL_PROTO_TLS1_2) && (!defined(MBEDTLS_USE_PSA_CRYPTO) &&\
+    !(defined(MBEDTLS_SHA1_C) || defined(MBEDTLS_SHA256_C) || defined(MBEDTLS_SHA512_C) ) )
 #error "MBEDTLS_SSL_PROTO_TLS1_2 defined, but not all prerequisites"
 #endif
 
@@ -793,7 +793,7 @@
 #endif
 
 #if defined(MBEDTLS_SSL_TLS_C) && ( !defined(MBEDTLS_CIPHER_C) ||     \
-    !defined(MBEDTLS_MD_C) )
+    ( !defined(MBEDTLS_MD_C) && !defined(MBEDTLS_USE_PSA_CRYPTO) ) )
 #error "MBEDTLS_SSL_TLS_C defined, but not all prerequisites"
 #endif
 

345a92b321d03c6e99d1524aa15eda8db988d70f
Andrzej Kurek <Andrzej.Kurek@arm.com>
2022-08-31 15:00:31 -0400

    Adjust PKCS5 dependencies in check_config
    It's possible to build PKCS5 with PSA instead of MD
    Signed-off-by: Andrzej Kurek <andrzej.kurek@arm.com>

--- include/mbedtls/check_config.h
+++ include/mbedtls/check_config.h
@@ -154,8 +154,9 @@
 #error "MBEDTLS_PKCS12_C defined, but not all prerequisites"
 #endif
 
-#if defined(MBEDTLS_PKCS5_C) && (!defined(MBEDTLS_MD_C) || \
-                                 !defined(MBEDTLS_CIPHER_C))
+#if defined(MBEDTLS_PKCS5_C) && \
+    ( !( defined(MBEDTLS_MD_C) || defined(MBEDTLS_PSA_CRYPTO_C) ) || \
+        !defined(MBEDTLS_CIPHER_C) )
 #error "MBEDTLS_PKCS5_C defined, but not all prerequisites"
 #endif
 

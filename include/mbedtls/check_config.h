e31ba83675e59926a073e36bf694ecf448906395
Przemek Stekiel <przemyslaw.stekiel@mobica.com>
2022-09-28 09:44:58 +0200

    Use basic symbols instead MBEDTLS_CIPHER_MODE_AEAD in check config
    Signed-off-by: Przemek Stekiel <przemyslaw.stekiel@mobica.com>

--- include/mbedtls/check_config.h
+++ include/mbedtls/check_config.h
@@ -962,11 +962,11 @@
 #error "MBEDTLS_SSL_VARIABLE_BUFFER_LENGTH defined, but not all prerequisites"
 #endif
 
-#if defined(MBEDTLS_SSL_SESSION_TICKETS) && !( defined(MBEDTLS_CIPHER_MODE_AEAD) || defined(MBEDTLS_NIST_KW_C) )
+#if defined(MBEDTLS_SSL_SESSION_TICKETS) && !( defined(MBEDTLS_GCM_C) || defined(MBEDTLS_CCM_C) || defined(MBEDTLS_CHACHAPOLY_C) || defined(MBEDTLS_NIST_KW_C) )
 #error "MBEDTLS_SSL_SESSION_TICKETS defined, but not all prerequisites"
 #endif
 
-#if defined(MBEDTLS_SSL_CONTEXT_SERIALIZATION) && !defined(MBEDTLS_CIPHER_MODE_AEAD)
+#if defined(MBEDTLS_SSL_CONTEXT_SERIALIZATION) && !( defined(MBEDTLS_GCM_C) || defined(MBEDTLS_CCM_C) || defined(MBEDTLS_CHACHAPOLY_C) )
 #error "MBEDTLS_SSL_CONTEXT_SERIALIZATION defined, but not all prerequisites"
 #endif
 

a44e0f74fb1c7066a5b461eefe208a1f48a895ad
Jerry Yu <jerry.h.yu@arm.com>
2022-10-08 16:54:04 +0800

    Add psk modes support for GnuTLS
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- tests/scripts/tls13_compat/psk_compat_test.py
+++ tests/scripts/tls13_compat/psk_compat_test.py
@@ -64,3 +64,73 @@ class PSKOpenSSLServ(OpenSSLServ):
         return KexMode(list(filter(lambda a: a != 0, [common_kex_mode & i for i in (
             KexMode.psk_ephemeral, KexMode.psk, KexMode.ephemeral)]))[0])
 
+
+class PSKGnuTLSCli(GnuTLSCli):
+    """
+    Generate PSK test command for GnuTLS Client
+    """
+
+    def cmd(self):
+        identity, psk = list(self._psk_identities)[0]
+
+        def update_priority_string_list(items, map_table):
+            for item in items:
+                for i in map_table[item]:
+                    yield '+' + i
+
+        priority_string_list = ['NORMAL', '-VERS-ALL',
+                                '+VERS-TLS1.3', '-KX-ALL', '+ECDHE-PSK', '+DHE-PSK']
+        if self._kex_mode and self._kex_mode & KexMode.psk:
+            priority_string_list.append('+PSK')
+        if self._named_groups:
+            priority_string_list.append('-GROUP-ALL')
+            priority_string_list.extend(update_priority_string_list(
+                self._named_groups, self.NAMED_GROUP))
+        ret = ret = super().pre_cmd() + ['--priority', ':'.join(priority_string_list),
+                                         '--pskusername', identity, '--pskkey', psk, 'localhost']
+        return ret
+
+
+class PSKGnuTLSServ(GnuTLSServ):
+    """
+    Generate PSK test command for GnuTLS Client
+    """
+
+    def cmd(self):
+        def update_priority_string_list(items, map_table):
+            for item in items:
+                for i in map_table[item]:
+                    yield '+' + i
+
+        # OpenSSL s_server need it.
+        if not self._cert_sig_algs:
+            self.add_cert_signature_algorithms("ecdsa_secp256r1_sha256")
+
+        priority_string_list = ['NORMAL', '-VERS-ALL',
+                                '+VERS-TLS1.3', '-KX-ALL', '+ECDHE-PSK', '+DHE-PSK']
+        if self._kex_mode and self._kex_mode & KexMode.psk:
+            priority_string_list.append('+PSK')
+        if self._named_groups:
+            priority_string_list.append('-GROUP-ALL')
+            priority_string_list.extend(update_priority_string_list(
+                self._named_groups, self.NAMED_GROUP))
+
+        # workaround GnuTLS psk settings
+        ret = super().pre_cmd() + ['--priority', ':'.join(priority_string_list),
+                                   '--pskpasswd data_files/passwd.psk']
+        return ret
+
+    def post_checks(self, *args, **kwargs):
+        return []
+
+    def select_expected_kex_mode(self, peer_kex_mode):
+        special_cases = {(KexMode.psk_all, KexMode.ephemeral_all): None}
+        if (peer_kex_mode, self._kex_mode) in special_cases:
+            return special_cases[(peer_kex_mode, self._kex_mode)]
+
+        common_kex_mode = KexMode(self._kex_mode & peer_kex_mode)
+        if common_kex_mode == KexMode.ephemeral_all:
+            return KexMode.psk_ephemeral
+        return KexMode(list(filter(lambda a: a != 0, [common_kex_mode & i for i in (
+            KexMode.psk_ephemeral, KexMode.psk, KexMode.ephemeral, )]))[0])
+

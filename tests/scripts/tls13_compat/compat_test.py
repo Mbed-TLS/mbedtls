405aaf6f201642b948d3ad0b6a6aa65c0ad18d8a
Jerry Yu <jerry.h.yu@arm.com>
2022-10-08 16:51:33 +0800

    Add ephemeral test cases
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- tests/scripts/tls13_compat/compat_test.py
+++ tests/scripts/tls13_compat/compat_test.py
@@ -75,3 +75,66 @@ def generate_tls13_compat_tests():
                                    cipher=cipher, named_group=named_group,
                                    sig_alg=sig_alg)
 
+
+def generate_hrr_compat_test(client=None, server=None,
+                             client_named_group=None, server_named_group=None,
+                             cert_sig_alg=None):
+    """
+    Generate Hello Retry Request test case with `ssl-opt.sh` format.
+    """
+    # Skip tests when MBedTLS is not server or client.
+    if not (issubclass(server, MbedTLSBase) or issubclass(client, MbedTLSBase)):
+        return None
+    # HRR will not be triggered when name_groups are same.
+    if client_named_group == server_named_group:
+        return None
+    name_fmt_string = 'TLS 1.3 {client[0]}->{server[0]}:' + \
+                      ' ephemeral HRR {c_named_group} -> {s_named_group}'
+    name = name_fmt_string.format(client=client.PROG_NAME, server=server.PROG_NAME,
+                                  c_named_group=client_named_group,
+                                  s_named_group=server_named_group)
+    server_object = server(named_group=server_named_group, cert_sig_alg=cert_sig_alg)
+
+    client_object = client(named_group=client_named_group, cert_sig_alg=cert_sig_alg)
+    # Add server name_group that will be selected by second client hello.
+    client_object.add_named_groups(server_named_group)
+
+    cmd = ['run_test "{}"'.format(name),
+           '"{}"'.format(' '.join(server_object.cmd())),
+           '"{}"'.format(' '.join(client_object.cmd())),
+           '0']
+    cmd += server_object.post_checks()
+    if isinstance(server_object, MbedTLSServ):
+        cmd += ['-s "HRR selected_group: {:s}"'.format(server_named_group)]
+
+    cmd += client_object.post_checks()
+    if isinstance(client_object, MbedTLSCli):
+        cmd += ['-c "received HelloRetryRequest message"', '-c "selected_group ( {:d} )"'.format(
+            NAMED_GROUP_IANA_VALUE[server_named_group])]
+
+    prefix = ' \\\n' + (' '*9)
+    cmd = prefix.join(cmd)
+    return '\n'.join(server_object.pre_checks() +
+                     client_object.pre_checks() +
+                     [cmd])
+
+
+def generate_all_hrr_compat_tests():
+    """
+    Generate all HRR compatible tests
+    """
+    # Generate Hello Retry Request  compat test cases
+    for client, server, client_named_group, server_named_group in \
+        itertools.product(CLIENT_CLASSES,
+                          SERVER_CLASSES,
+                          NAMED_GROUP_IANA_VALUE.keys(),
+                          NAMED_GROUP_IANA_VALUE.keys()):
+
+        yield generate_hrr_compat_test(client=client, server=server,
+                                       client_named_group=client_named_group,
+                                       server_named_group=server_named_group,
+                                       cert_sig_alg="ecdsa_secp256r1_sha256")
+
+def generate_tests_of_hrr_and_compt():
+    yield from generate_tls13_compat_tests()
+    yield from generate_all_hrr_compat_tests()

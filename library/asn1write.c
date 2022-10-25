864cc8dba2b520df7e3620d7fc76139c3896c780
David Horstmann <david.horstmann@arm.com>
2022-10-25 10:16:45 +0100

    Minor changes to asn1write.c
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- library/asn1write.c
+++ library/asn1write.c
@@ -72,11 +72,11 @@ int mbedtls_asn1_write_len( unsigned char **p, unsigned char *start, size_t len 
         return( 4 );
     }
 
-    int len_valid = 1;
+    int len_is_valid = 1;
 #if SIZE_MAX > 0xFFFFFFFF
-    len_valid = ( len <= 0xFFFFFFFF );
+    len_is_valid = ( len <= 0xFFFFFFFF );
 #endif
-    if( len_valid )
+    if( len_is_valid )
     {
         if( *p - start < 5 )
             return( MBEDTLS_ERR_ASN1_BUF_TOO_SMALL );
@@ -88,10 +88,8 @@ int mbedtls_asn1_write_len( unsigned char **p, unsigned char *start, size_t len 
         *--(*p) = 0x84;
         return( 5 );
     }
-    else
-    {
-        return( MBEDTLS_ERR_ASN1_INVALID_LENGTH );
-    }
+
+    return( MBEDTLS_ERR_ASN1_INVALID_LENGTH );
 }
 
 int mbedtls_asn1_write_tag( unsigned char **p, unsigned char *start, unsigned char tag )

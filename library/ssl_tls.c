72dbfef6e4b48d02327f663dcc695cc2abadfa75
Xiaokang Qian <xiaokang.qian@arm.com>
2022-10-26 06:33:57 +0000

    Improve coding styles
    Signed-off-by: Xiaokang Qian <xiaokang.qian@arm.com>

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -1428,7 +1428,7 @@ void mbedtls_ssl_conf_tls13_key_exchange_modes( mbedtls_ssl_config *conf,
 
 #if defined(MBEDTLS_SSL_EARLY_DATA)
 void mbedtls_ssl_tls13_conf_early_data( mbedtls_ssl_config *conf,
-                                  int early_data_enabled )
+                                        int early_data_enabled )
 {
     conf->early_data_enabled = early_data_enabled;
 }

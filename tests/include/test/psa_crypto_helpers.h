57b10ac9de93af2b92284b4956354969343bb924
Yanray Wang <yanray.wang@arm.com>
2022-10-28 18:12:01 +0800

    Fix build error in CI
    During test of component build_arm_linux_gnueabi_gcc_arm5vte and
    build_arm_none_eabi_gcc_m0plus. It fails with
     - error: implicit declaration of function
       ‘test_fail_if_psa_leaking’
    
    It happens because test_fail_if_psa_leaking is defined in
    helpers.function. This block of code is not converted into C code
    while compiling ssl_helpers.c. The function has been moved to
    psa_crypto_helpers.c in order to fix this build error.
    
    Signed-off-by: Yanray Wang <yanray.wang@arm.com>

--- tests/include/test/psa_crypto_helpers.h
+++ tests/include/test/psa_crypto_helpers.h
@@ -193,6 +193,14 @@ psa_status_t mbedtls_test_record_status( psa_status_t status,
  */
 psa_key_usage_t mbedtls_test_update_key_usage_flags( psa_key_usage_t usage_flags );
 
+/** Check that no PSA Crypto key slots are in use.
+ *
+ * If any slots are in use, mark the current test as failed.
+ *
+ * \return 0 if the key store is empty, 1 otherwise.
+ */
+int test_fail_if_psa_leaking( int line_no, const char *filename );
+
 /** Skip a test case if the given key is a 192 bits AES key and the AES
  *  implementation is at least partially provided by an accelerator or
  *  alternative implementation.

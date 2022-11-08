14a87291bde671808d4d1cf1e9bad4e60f88b49e
Pengyu Lv <pengyu.lv@arm.com>
2022-11-08 19:41:28 +0800

    fix false negative reported by check_names.py
    The PSA_CRYPTO_C is excluded from typo check for the following
    false negative report:
    
    ```
      > include/mbedtls/check_config.h:329: 'PSA_CRYPTO_C' looks like a typo. It
        was not found in any macros or any enums. If this is not a typo, put //no-
        check-names after it.
        |
    329 |  * Note: ECJPAKE_C depends on MD_C || PSA_CRYPTO_C. */
        |                                       ^^^^^^^^^^^^
    
    ```
    
    Signed-off-by: Pengyu Lv <pengyu.lv@arm.com>

--- tests/scripts/check_names.py
+++ tests/scripts/check_names.py
@@ -847,7 +847,8 @@ class NameChecker():
             }
         typo_exclusion = re.compile(r"XXX|__|_$|^MBEDTLS_.*CONFIG_FILE$|"
                                     r"MBEDTLS_TEST_LIBTESTDRIVER*|"
-                                    r"PSA_CRYPTO_DRIVER_TEST")
+                                    r"PSA_CRYPTO_DRIVER_TEST|"
+                                    r"PSA_CRYPTO_C")
 
         for name_match in self.parse_result["mbed_psa_words"]:
             found = name_match.name in all_caps_names

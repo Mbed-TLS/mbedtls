d2ed20c5ec5f4642bba9990ec3697d50ff202ea2
Jerry Yu <jerry.h.yu@arm.com>
2022-12-02 13:04:25 +0800

    [DO-NOT-MERGE]Add EarlyData basic check
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -1755,7 +1755,28 @@ SKIP_NEXT="NO"
 
 trap cleanup INT TERM HUP
 
-
+for i in $(seq 1 400); do
+    requires_gnutls_tls1_3
+    requires_config_enabled MBEDTLS_DEBUG_C
+    requires_config_enabled MBEDTLS_SSL_CLI_C
+    requires_all_configs_enabled MBEDTLS_SSL_TLS1_3_COMPATIBILITY_MODE \
+                                MBEDTLS_SSL_TLS1_3_KEY_EXCHANGE_MODE_EPHEMERAL_ENABLED \
+                                MBEDTLS_SSL_EARLY_DATA
+    requires_any_configs_enabled MBEDTLS_SSL_TLS1_3_KEY_EXCHANGE_MODE_PSK_EPHEMERAL_ENABLED \
+                                MBEDTLS_SSL_TLS1_3_KEY_EXCHANGE_MODE_PSK_ENABLED
+    run_test    "TLS 1.3 m->G: EarlyData: basic check, good $i" \
+                "$G_NEXT_SRV -d 10 --priority=NORMAL:-VERS-ALL:+VERS-TLS1.3:+CIPHER-ALL:+ECDHE-PSK:+PSK --earlydata --disable-client-cert" \
+                "$P_CLI debug_level=4 early_data=1 reco_mode=1 reconnect=1" \
+                1 \
+                -c "Reconnecting with saved session" \
+                -c "NewSessionTicket: early_data(42) extension received." \
+                -c "ClientHello: early_data(42) extension exists." \
+                -c "EncryptedExtensions: early_data(42) extension received." \
+                -c "EncryptedExtensions: early_data(42) extension exists." \
+                -s "Parsing extension 'Early Data/42' (0 bytes)" \
+                -s "Sending extension Early Data/42 (0 bytes)" \
+                -s "early data accepted"
+done
 # for i in opt-testcases/*.sh
 # do
 #     TEST_SUITE_NAME=${i##*/}

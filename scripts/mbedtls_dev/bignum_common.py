5623ecc2d62bcdd2c4bd2dd501591502713a7725
Gilles Peskine <Gilles.Peskine@arm.com>
2022-12-20 19:16:54 +0100

    Mod operations: fill arguments to the width of the modulus
    With the default input style (which is "variable"), fill all bignum test
    case arguments to the same width as the modulus.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- scripts/mbedtls_dev/bignum_common.py
+++ scripts/mbedtls_dev/bignum_common.py
@@ -267,6 +267,12 @@ class ModOperationCommon(OperationCommon):
     def arg_n(self) -> str:
         return self.format_arg(self.val_n)
 
+    def format_arg(self, val: str) -> str:
+        if self.input_style == "variable":
+            return val.zfill(len(hex(self.int_n)) - 2)
+        else:
+            return super().format_arg(val)
+
     def arguments(self) -> List[str]:
         return [quote_str(self.arg_n)] + super().arguments()
 

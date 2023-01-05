f27a181455faf502f7152e339061c7bf43e0469e
Pengyu Lv <pengyu.lv@arm.com>
2023-01-05 14:56:40 +0800

    TLS13 SRV: Check ALPN when update early_data state
    Signed-off-by: Pengyu Lv <pengyu.lv@arm.com>
    Signed-off-by: Yanray Wang <yanray.wang@arm.com>

--- library/ssl_tls13_server.c
+++ library/ssl_tls13_server.c
@@ -1856,6 +1856,16 @@ static void ssl_tls13_update_early_data_status(mbedtls_ssl_context *ssl)
 
     }
 
+#if defined(MBEDTLS_SSL_ALPN)
+    if( session->ticket_alpn.alpn != ssl->alpn_chosen )
+    {
+        MBEDTLS_SSL_DEBUG_MSG(
+            1,
+            ( "EarlyData: rejected. chosen alpn differs from the ticket." ) );
+        return;
+    }
+#endif
+
     if (!mbedtls_ssl_session_ticket_allow_early_data(ssl->session_negotiate)) {
         MBEDTLS_SSL_DEBUG_MSG(
             1,

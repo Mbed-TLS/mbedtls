98682b50a4aafded139db3684360b7c8069e207b
Andrzej Kurek <andrzej.kurek@arm.com>
2023-01-23 06:16:23 -0500

    Remove obsolete comment from depends.py
    Signed-off-by: Andrzej Kurek <andrzej.kurek@arm.com>

--- tests/scripts/depends.py
+++ tests/scripts/depends.py
@@ -419,8 +419,7 @@ class DomainData:
                                  exclude=r'MBEDTLS_(MD|RIPEMD|SHA1_)' \
                                           '|MBEDTLS_SHA224_' \
                                           '|MBEDTLS_SHA384_'),
-            # Key exchange types. Only build the library and the sample
-            # programs.
+            # Key exchange types.
             'kex': ExclusiveDomain(key_exchange_symbols, build_and_test),
             'pkalgs': ComplementaryDomain(['MBEDTLS_ECDSA_C',
                                            'MBEDTLS_ECP_C',

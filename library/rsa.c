a61186e5e75990b19b2293ff5761dd5fcd957d85
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2023-01-26 12:09:11 +0100

    RSA self-test: support SHA1 routed via PSA
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- library/rsa.c
+++ library/rsa.c
@@ -2505,7 +2505,8 @@ int mbedtls_rsa_self_test(int verbose)
         mbedtls_printf("  PKCS#1 data sign  : ");
     }
 
-    if (mbedtls_sha1(rsa_plaintext, PT_LEN, sha1sum) != 0) {
+    if (mbedtls_md(mbedtls_md_info_from_type(MBEDTLS_MD_SHA1),
+                   rsa_plaintext, PT_LEN, sha1sum) != 0) {
         if (verbose != 0) {
             mbedtls_printf("failed\n");
         }

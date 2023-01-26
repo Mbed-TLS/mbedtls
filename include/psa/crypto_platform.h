f6236f032a9cb8890dcc5d63427e6c7251be80b7
Ronald Cron <ronald.cron@arm.com>
2023-01-26 16:22:25 +0100

    psa: Introduce psa/build_info.h
    The PSA cryptography repository is based to
    start with on the PSA cryptography implementation
    in Mbed TLS but with a different directority
    structure, build system and build configuration.
    
    The build-time configuration information in the
    PSA cryptography repository is psa/build_info.h.
    This commit introduces this file in Mbed TLS to
    be used in place of mbedtls/build_info.h (but
    basically just an alias to it) in PSA headers.
    This is to ease the update of the PSA cryptography
    repository.
    
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- include/psa/crypto_platform.h
+++ include/psa/crypto_platform.h
@@ -34,9 +34,14 @@
 #define PSA_CRYPTO_PLATFORM_H
 #include "mbedtls/private_access.h"
 
-/* Include the Mbed TLS configuration file, the way Mbed TLS does it
- * in each of its header files. */
-#include "mbedtls/build_info.h"
+/*
+ * Include the build-time configuration information file. Here, we do not
+ * include `"mbedtls/build_info.h"` directly but `"psa/build_info.h"`, which
+ * is basically just an alias to it. This is to ease the maintenance of the
+ * PSA cryptography repository which has a different build system and
+ * configuration.
+ */
+#include "psa/build_info.h"
 
 /* PSA requires several types which C99 provides in stdint.h. */
 #include <stdint.h>

63aae68b8f1d7d923487f6e1e3297bc03c87d269
Gabor Mezei <gabor.mezei@arm.com>
2023-02-06 16:24:08 +0100

    Fix documentation
    Signed-off-by: Gabor Mezei <gabor.mezei@arm.com>

--- library/ecp_invasive.h
+++ library/ecp_invasive.h
@@ -81,8 +81,8 @@ int mbedtls_ecp_gen_privkey_mx(size_t n_bits,
 /** Fast quasi-reduction modulo p192 (FIPS 186-3 D.2.1)
  *
  * \param[in,out]   Np  The address of the MPI to be converted.
- *                      Must have the double the limbs than \p Nn.
- * \param[in]       Nn  The address of the modulus.
+ *                      Must have twice as many limbs as \p Nn.
+ * \param[in]       Nn  The length of \p Np in limbs.
  */
 MBEDTLS_STATIC_TESTABLE
 int mbedtls_ecp_mod_p192_raw(mbedtls_mpi_uint *Np, size_t Nn);

0cab0700cf5f45aa16cbd27cc4f30307e084c8ff
Paul Elliott <paul.elliott@arm.com>
2023-02-15 18:23:53 +0000

    Add safety for keys larger than we currently support.
    Prevent buffer overflow with keys whos grp.nbits is greater than
    PSA_VENDOR_ECC_MAX_CURVE_BITS.
    
    Signed-off-by: Paul Elliott <paul.elliott@arm.com>

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -3586,6 +3586,11 @@ psa_status_t mbedtls_psa_sign_hash_start(
     required_hash_length = (hash_length < operation->coordinate_bytes ?
                             hash_length : operation->coordinate_bytes);
 
+    if (required_hash_length > sizeof(operation->hash)) {
+        /* Shouldn't happen, but better safe than sorry. */
+        return PSA_ERROR_CORRUPTION_DETECTED;
+    }
+
     memcpy(operation->hash, hash, required_hash_length);
     operation->hash_length = required_hash_length;
 
@@ -3817,6 +3822,11 @@ psa_status_t mbedtls_psa_verify_hash_start(
      * here, it would be truncated by the internal implementation anyway. */
     required_hash_length = (hash_length < coordinate_bytes ? hash_length :
                             coordinate_bytes);
+
+    if (required_hash_length > sizeof(operation->hash)) {
+        /* Shouldn't happen, but better safe than sorry. */
+        return PSA_ERROR_CORRUPTION_DETECTED;
+    }
 
     memcpy(operation->hash, hash, required_hash_length);
     operation->hash_length = required_hash_length;

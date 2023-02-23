1aa6e8d6e9f5af25c24a2484d774f9a879aa873a
Ronald Cron <ronald.cron@arm.com>
2023-02-23 09:46:54 +0100

    Restore same PSK length enforcement
    Restore same PSK length enforcement in
    conf_psk and set_hs_psk, whether the
    negotiated protocol is TLS 1.2 or TLS 1.3.
    
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- include/mbedtls/mbedtls_config.h
+++ include/mbedtls/mbedtls_config.h
@@ -3761,7 +3761,7 @@
  */
 //#define MBEDTLS_SSL_DTLS_MAX_BUFFERING             32768
 
-//#define MBEDTLS_PSK_MAX_LEN               32 /**< Max size of TLS pre-shared keys, in bytes (default 256 bits) */
+//#define MBEDTLS_PSK_MAX_LEN               32 /**< Max size of TLS pre-shared keys, in bytes (default 256 or 384 bits) */
 //#define MBEDTLS_SSL_COOKIE_TIMEOUT        60 /**< Default expiration delay of DTLS cookies, in seconds if HAVE_TIME, or in number of cookies issued */
 
 /**

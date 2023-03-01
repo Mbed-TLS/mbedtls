aeadc2d731613f9cbc8856a2ad4dc13032f3380d
Gabor Mezei <gabor.mezei@arm.com>
2023-03-01 16:53:03 +0100

    Apply naming convention
    Signed-off-by: Gabor Mezei <gabor.mezei@arm.com>

--- library/ecp_curves.c
+++ library/ecp_curves.c
@@ -4576,7 +4576,7 @@ int mbedtls_ecp_mod_p192_raw(mbedtls_mpi_uint *Np, size_t Nn);
 #if defined(MBEDTLS_ECP_DP_SECP224R1_ENABLED)
 static int ecp_mod_p224(mbedtls_mpi *);
 MBEDTLS_STATIC_TESTABLE
-int mbedtls_ecp_mod_p224_raw(mbedtls_mpi_uint *Np, size_t Nn);
+int mbedtls_ecp_mod_p224_raw(mbedtls_mpi_uint *X, size_t X_limbs);
 #endif
 #if defined(MBEDTLS_ECP_DP_SECP256R1_ENABLED)
 static int ecp_mod_p256(mbedtls_mpi *);

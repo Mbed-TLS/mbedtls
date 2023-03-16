de34578353f4f0944a068dd44a66694064c4a25b
Gilles Peskine <Gilles.Peskine@arm.com>
2023-03-16 13:06:14 +0100

    Fix code style
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- library/gcm.c
+++ library/gcm.c
@@ -754,8 +754,7 @@ int mbedtls_gcm_self_test(int verbose)
     int i, j, ret;
     mbedtls_cipher_id_t cipher = MBEDTLS_CIPHER_ID_AES;
 
-    if (verbose != 0)
-    {
+    if (verbose != 0) {
 #if defined(MBEDTLS_GCM_ALT)
         mbedtls_printf("  GCM note: alternative implementation.\n");
 #else /* MBEDTLS_GCM_ALT */

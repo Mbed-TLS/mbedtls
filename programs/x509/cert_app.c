0c9d048d26680722d58097380699e251110139ac
Przemek Stekiel <przemyslaw.stekiel@mobica.com>
2023-04-14 12:28:16 +0200

    cert_app: init entropy unconditionally
    When mbedtls_entropy_free() is called without mbedtls_entropy_init() entropy is uninitialized and contains garbage which may lead to segmentation fault.
    
    Signed-off-by: Przemek Stekiel <przemyslaw.stekiel@mobica.com>

--- programs/x509/cert_app.c
+++ programs/x509/cert_app.c
@@ -166,6 +166,7 @@ int main(int argc, char *argv[])
     mbedtls_ssl_init(&ssl);
     mbedtls_ssl_config_init(&conf);
     mbedtls_x509_crt_init(&cacert);
+    mbedtls_entropy_init(&entropy);
 #if defined(MBEDTLS_X509_CRL_PARSE_C)
     mbedtls_x509_crl_init(&cacrl);
 #else
@@ -351,7 +352,6 @@ usage:
         mbedtls_printf("\n  . Seeding the random number generator...");
         fflush(stdout);
 
-        mbedtls_entropy_init(&entropy);
         if ((ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy,
                                          (const unsigned char *) pers,
                                          strlen(pers))) != 0) {

2e863ecde9017d4a4f9fd407354e30a2a05e2d32
Dave Rodgman <dave.rodgman@arm.com>
2023-04-25 17:34:59 +0100

    Remove unnecessary if to save 16 bytes
    Signed-off-by: Dave Rodgman <dave.rodgman@arm.com>

--- library/bignum_core.c
+++ library/bignum_core.c
@@ -71,12 +71,10 @@ size_t mbedtls_mpi_core_bitlen(const mbedtls_mpi_uint *A, size_t A_limbs)
     int i;
     size_t j;
 
-    if (A_limbs != 0) {
-        for (i = (int) A_limbs - 1; i >= 0; i--) {
-            if (A[i] != 0) {
-                j = biL - mbedtls_mpi_core_clz(A[i]);
-                return (i * biL) + j;
-            }
+    for (i = ((int) A_limbs) - 1; i >= 0; i--) {
+        if (A[i] != 0) {
+            j = biL - mbedtls_mpi_core_clz(A[i]);
+            return (i * biL) + j;
         }
     }
 

73521b071531049265904552109dd7d52d25b4c3
Gilles Peskine <Gilles.Peskine@arm.com>
2023-04-28 21:01:49 +0200

    Tests: provide necessary functions for MBEDTLS_PSA_INJECT_ENTROPY
    The build option MBEDTLS_PSA_INJECT_ENTROPY requires some extra platform
    functions, for historical reasons. To enable us to test this option, provide
    a version of these functions for testing.
    
    (These versions would actually work in production, but providing them in the
    library in a way that doesn't break existing users might be slightly tricky,
    so it's out of scope of this commit.)
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/configs/user-config-for-test.h
+++ tests/configs/user-config-for-test.h
@@ -57,3 +57,23 @@
 #define MBEDTLS_PSA_ACCEL_ALG_HMAC
 
 #endif  /* PSA_CRYPTO_DRIVER_TEST_ALL */
+
+
+
+#if defined(MBEDTLS_PSA_INJECT_ENTROPY)
+/* The #MBEDTLS_PSA_INJECT_ENTROPY feature requires two extra platform
+ * functions, which must be configured as #MBEDTLS_PLATFORM_NV_SEED_READ_MACRO
+ * and #MBEDTLS_PLATFORM_NV_SEED_WRITE_MACRO. The job of these functions
+ * is to read and write from the entropy seed file, which is located
+ * in the PSA ITS file whose uid is #PSA_CRYPTO_ITS_RANDOM_SEED_UID.
+ * (These could have been provided as library functions, but for historical
+ * reasons, they weren't, and so each integrator has to provide a copy
+ * of these functions.)
+ *
+ * Provide implementations of these functions for testing. */
+#include <stddef.h>
+int mbedtls_test_inject_entropy_seed_read(unsigned char *buf, size_t len);
+int mbedtls_test_inject_entropy_seed_write(unsigned char *buf, size_t len);
+#define MBEDTLS_PLATFORM_NV_SEED_READ_MACRO mbedtls_test_inject_entropy_seed_read
+#define MBEDTLS_PLATFORM_NV_SEED_WRITE_MACRO mbedtls_test_inject_entropy_seed_write
+#endif /* MBEDTLS_PSA_INJECT_ENTROPY */

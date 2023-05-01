4fa8334bae7a29e4d4b541cf5f0ebf2ac1b6b7d7
Paul Elliott <paul.elliott@arm.com>
2023-05-01 22:30:54 +0100

    Convert curve 448 to use ecp core functions
    Signed-off-by: Paul Elliott <paul.elliott@arm.com>

--- library/ecp_invasive.h
+++ library/ecp_invasive.h
@@ -196,7 +196,7 @@ int mbedtls_ecp_mod_p256k1(mbedtls_mpi *N);
 #if defined(MBEDTLS_ECP_DP_CURVE448_ENABLED)
 
 MBEDTLS_STATIC_TESTABLE
-int mbedtls_ecp_mod_p448(mbedtls_mpi *N);
+int mbedtls_ecp_mod_p448(mbedtls_mpi_uint *N, size_t N_limbs);
 
 #endif /* MBEDTLS_ECP_DP_CURVE448_ENABLED */
 

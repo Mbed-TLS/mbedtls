6e14d9dc779961e126b8a73bc2e4ded143edf975
Gilles Peskine <Gilles.Peskine@arm.com>
2023-05-22 12:18:08 +0200

    Fix copypasta
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- include/psa/crypto_config.h
+++ include/psa/crypto_config.h
@@ -91,7 +91,8 @@
 #define PSA_WANT_ALG_TLS12_PSK_TO_MS            1
 #define PSA_WANT_ALG_TLS12_ECJPAKE_TO_PMS       1
 
-/* Note: when adding support, also adjust include/mbedtls/config_psa.h */
+/* XTS is not yet supported via the PSA API in Mbed TLS.
+ * Note: when adding support, also adjust include/mbedtls/config_psa.h */
 //#define PSA_WANT_ALG_XTS                        1
 
 #define PSA_WANT_ECC_BRAINPOOL_P_R1_256         1

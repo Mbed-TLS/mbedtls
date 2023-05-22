247db0e44e46eda80341a5db13794533c2bf629c
Jerry Yu <jerry.h.yu@arm.com>
2023-05-22 16:47:54 +0800

    fix check_files fail
    Signed-off-by: Jerry Yu <jerry.h.yu@arm.com>

--- test.sh
+++ test.sh
@@ -1,9 +1,19 @@
 set -ex
-scripts/config.py full
-make clean
-make all -j20
-cd tests
 
-./test_suite_x509parse 2>&1 | tee test1.log 
-faketime -f '+4y' ./test_suite_x509parse 2>&1 | tee test.log
+test_x905_parser(){
+    make clean
+    make all -j20
+    cd tests
+
+    ./test_suite_x509parse 2>&1 > test1.log
+    faketime -f '+4y' ./test_suite_x509parse 2>&1 >test.log
+    cd ..
+}
+
+git checkout HEAD -- include/mbedtls/mbedtls_config.h
+test_x905_parser
+
+scripts/config.py full
+test_x905_parser
+
 

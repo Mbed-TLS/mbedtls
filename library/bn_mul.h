f89e3c5fbd0c83539f68450423abb186f1ea8787
Dave Rodgman <dave.rodgman@gmail.com>
2023-06-04 20:41:52 -0400

    Improve docs & check for non-gcc compilers
    Signed-off-by: Dave Rodgman <dave.rodgman@gmail.com>

--- library/bn_mul.h
+++ library/bn_mul.h
@@ -661,9 +661,14 @@
 #if defined(__arm__)
 
 #if defined(__thumb__) && !defined(__thumb2__)
-#if !defined(__ARMCC_VERSION) && !defined(__clang__)
+#if !defined(__ARMCC_VERSION) && !defined(__clang__) \
+    && !defined(__llvm__) && !defined(__INTEL_COMPILER)
 /*
- * Thumb 1 ISA. This code path does not build on clang or armclang.
+ * Thumb 1 ISA. This code path has only been tested successfully on gcc;
+ * it does not compile on clang or armclang.
+ *
+ * Other compilers which define __GNUC__ may not work. The above macro
+ * attempts to exclude these untested compilers.
  */
 
 #if !defined(__OPTIMIZE__) && defined(__GNUC__)

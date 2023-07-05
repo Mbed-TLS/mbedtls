852b6c30b787f28a1fc085b7ccf3b776b0fa09f5
Dave Rodgman <dave.rodgman@arm.com>
2023-07-05 19:47:08 +0100

    Support MBEDTLS_MD_SHA3_xxx_VIA_PSA
    Signed-off-by: Dave Rodgman <dave.rodgman@arm.com>

--- library/md.c
+++ library/md.c
@@ -259,6 +259,22 @@ static psa_algorithm_t psa_alg_of_md(const mbedtls_md_info_t *info)
         case MBEDTLS_MD_SHA512:
             return PSA_ALG_SHA_512;
 #endif
+#if defined(MBEDTLS_MD_SHA3_224_VIA_PSA)
+        case MBEDTLS_MD_SHA3_224:
+            return PSA_ALG_SHA3_224;
+#endif
+#if defined(MBEDTLS_MD_SHA3_256_VIA_PSA)
+        case MBEDTLS_MD_SHA3_256:
+            return PSA_ALG_SHA3_256;
+#endif
+#if defined(MBEDTLS_MD_SHA3_384_VIA_PSA)
+        case MBEDTLS_MD_SHA3_384:
+            return PSA_ALG_SHA3_384;
+#endif
+#if defined(MBEDTLS_MD_SHA3_512_VIA_PSA)
+        case MBEDTLS_MD_SHA3_512:
+            return PSA_ALG_SHA3_512;
+#endif
         default:
             return PSA_ALG_NONE;
     }

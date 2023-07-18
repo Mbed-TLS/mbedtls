42f42f4394ed239fb14e289cb347db21638fdec0
David Horstmann <david.horstmann@arm.com>
2023-07-18 17:53:01 +0100

    Support psa-crypto repo in psa_storage.py
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- scripts/mbedtls_dev/psa_storage.py
+++ scripts/mbedtls_dev/psa_storage.py
@@ -27,6 +27,7 @@ from typing import Dict, List, Optional, Set, Union
 import unittest
 
 from . import c_build_helper
+from . import build_tree
 
 
 class Expr:
@@ -51,13 +52,16 @@ class Expr:
     def update_cache(self) -> None:
         """Update `value_cache` for expressions registered in `unknown_values`."""
         expressions = sorted(self.unknown_values)
+        includes = ['include']
+        if build_tree.looks_like_psa_crypto_root('.'):
+            includes.append('drivers/builtin/include')
         values = c_build_helper.get_c_expression_values(
             'unsigned long', '%lu',
             expressions,
             header="""
             #include <psa/crypto.h>
             """,
-            include_path=['include']) #type: List[str]
+            include_path=includes) #type: List[str]
         for e, v in zip(expressions, values):
             self.value_cache[e] = int(v, 0)
         self.unknown_values.clear()

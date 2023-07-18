36991343fdd633c1ef9ecdad12b5dd43684585d9
Thomas Daubney <thomas.daubney@arm.com>
2023-07-18 17:35:10 +0100

    Bring code-style up-to-date
    This PR was originally created before the code
    style was changed. This commit updates the style.
    
    Signed-off-by: Thomas Daubney <thomas.daubney@arm.com>

--- programs/psa/psa_random.c
+++ programs/psa/psa_random.c
@@ -48,8 +48,7 @@
 typedef mbedtls_psa_external_random_context_t mbedtls_psa_random_context_t;
 
 
-typedef struct
-{
+typedef struct {
     mbedtls_psa_random_context_t rng;
     unsigned initialized : 1;
     unsigned rng_state : 2;
@@ -59,72 +58,71 @@ static psa_global_data_t global_data;
 
 psa_status_t mbedtls_psa_external_gen_random(
     mbedtls_psa_external_random_context_t *context,
-    uint8_t *output, size_t output_size, size_t *output_length )
+    uint8_t *output, size_t output_size, size_t *output_length)
 {
     (void) context;
     size_t i;
 
     /* This implementation is for test purposes only! */
-    for( i = 0; i < output_size; ++i )
+    for (i = 0; i < output_size; ++i) {
         output[i] = '\0';
+    }
 
     *output_length = output_size;
-    return( PSA_SUCCESS );
+    return PSA_SUCCESS;
 }
 #endif /* MBEDTLS_PSA_CRYPTO_EXTERNAL_RNG */
 
 #if !defined(MBEDTLS_PSA_CRYPTO_C)
-int main( void )
+int main(void)
 {
-    printf( "MBEDTLS_PSA_CRYPTO_C not defined.\r\n" );
-    return( 0 );
+    printf("MBEDTLS_PSA_CRYPTO_C not defined.\r\n");
+    return 0;
 }
 #else
-int main( void )
+int main(void)
 {
     psa_status_t status;
-    uint8_t output[BUFFER_SIZE] = {0};
+    uint8_t output[BUFFER_SIZE] = { 0 };
 
-    status = psa_crypto_init( );
-    if( status != PSA_SUCCESS )
-    {
-        printf( "psa_crypto_init failed\n" );
-        return( EXIT_FAILURE );
+    status = psa_crypto_init();
+    if (status != PSA_SUCCESS) {
+        printf("psa_crypto_init failed\n");
+        return EXIT_FAILURE;
     }
 #if defined(MBEDTLS_PSA_CRYPTO_EXTERNAL_RNG)
     size_t output_length = 0;
     size_t output_size = 0;
 
-    status = mbedtls_psa_external_gen_random( &global_data.rng,
-                                              output, output_size,
-                                              &output_length );
+    status = mbedtls_psa_external_gen_random(&global_data.rng,
+                                             output, output_size,
+                                             &output_length);
 
-    if( status != PSA_SUCCESS )
-    {
-        printf( "mbedtls_psa_external_gen_random failed\n" );
-        return( EXIT_FAILURE );
+    if (status != PSA_SUCCESS) {
+        printf("mbedtls_psa_external_gen_random failed\n");
+        return EXIT_FAILURE;
     }
 
 #else
-    status = psa_generate_random( output, BUFFER_SIZE );
-    if( status != PSA_SUCCESS )
-    {
-        printf( "psa_generate_random failed\n" );
-        return( EXIT_FAILURE );
+    status = psa_generate_random(output, BUFFER_SIZE);
+    if (status != PSA_SUCCESS) {
+        printf("psa_generate_random failed\n");
+        return EXIT_FAILURE;
     }
 #endif /* MBEDTLS_PSA_CRYPTO_EXTERNAL_RNG */
 
-    printf( "Random bytes generated:\n" );
+    printf("Random bytes generated:\n");
 
-    for( size_t j = 0; j < BUFFER_SIZE; j++ )
-    {
-        if( j % 8 == 0 ) printf( "\n    " );
-        printf( "%02x ", output[j] );
+    for (size_t j = 0; j < BUFFER_SIZE; j++) {
+        if (j % 8 == 0) {
+            printf("\n    ");
+        }
+        printf("%02x ", output[j]);
     }
 
-    printf( "\n" );
+    printf("\n");
 
-    mbedtls_psa_crypto_free( );
-    return( 0 );
+    mbedtls_psa_crypto_free();
+    return 0;
 }
 #endif /* MBEDTLS_PSA_CRYPTO_C */

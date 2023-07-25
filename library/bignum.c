bc345e8685f2e5c5ce9e9a9d2f0c61f7cd7f9e06
Tom Cosgrove <tom.cosgrove@arm.com>
2023-07-25 15:17:39 +0100

    Protect macro parameter expansion with parentheses
    Signed-off-by: Tom Cosgrove <tom.cosgrove@arm.com>

--- library/bignum.c
+++ library/bignum.c
@@ -55,7 +55,7 @@
     MBEDTLS_INTERNAL_VALIDATE(cond)
 
 /* Implementation that should never be optimized out by the compiler */
-#define mbedtls_mpi_zeroize_and_free(v, n) mbedtls_zeroize_and_free(v, ciL * n)
+#define mbedtls_mpi_zeroize_and_free(v, n) mbedtls_zeroize_and_free(v, ciL * (n))
 
 /*
  * Initialize one MPI

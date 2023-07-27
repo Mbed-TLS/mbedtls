05b60f40acc709b83599dc0ad31d1b8c2c4f9699
Dave Rodgman <dave.rodgman@arm.com>
2023-07-27 14:22:34 +0100

    Make code_style.py -s more precise
    Signed-off-by: Dave Rodgman <dave.rodgman@arm.com>

--- scripts/code_style.py
+++ scripts/code_style.py
@@ -81,11 +81,15 @@ def get_src_files(since: Optional[str]) -> List[str]:
                                      universal_newlines=True)
     src_files = output.split()
     if since:
-        output = subprocess.check_output(["git", "diff", "--name-only",
-                                          since, "--"] +
-                                         src_files,
-                                         universal_newlines=True)
-        src_files = output.split()
+        # get all files changed in commits since the starting point
+        cmd = ["git", "log", since + "..HEAD", "--name-only", "--pretty=", "--" ] + src_files
+        output = subprocess.check_output(cmd, universal_newlines=True)
+        committed_changed_files = output.split()
+        # and also get all files with uncommitted changes
+        cmd = ["git", "diff", "--name-only", "--" ] + src_files
+        output = subprocess.check_output(cmd, universal_newlines=True)
+        uncommitted_changed_files = output.split()
+        src_files = set(committed_changed_files + uncommitted_changed_files)
 
     generated_files = list_generated_files()
     # Don't correct style for third-party files (and, for simplicity,

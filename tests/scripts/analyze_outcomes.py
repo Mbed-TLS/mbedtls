358c6c644a49d5b86a3f24d3e69edbfb10e4c11d
Tomás González <tomasagustin.gonzalezorlando@arm.com>
2023-08-14 15:43:46 +0100

    Add EdDSA and XTS to the allow list
    As specified in
    https://github.com/Mbed-TLS/mbedtls/issues/5390#issuecomment-1669585707
    EdDSA and XTS tests are legitimately never executed, so add them to
    the allow list.
    
    Signed-off-by: Tomás González <tomasagustin.gonzalezorlando@arm.com>

--- tests/scripts/analyze_outcomes.py
+++ tests/scripts/analyze_outcomes.py
@@ -180,7 +180,11 @@ TASKS = {
     'analyze_coverage':                 {
         'test_function': do_analyze_coverage,
         'args': {
-            'allow_list': [],
+            'allow_list': [
+                'test_suite_psa_crypto_metadata;Asymmetric signature: '
+                'pure EdDSA',
+                'test_suite_psa_crypto_metadata;Cipher: XTS'
+                ],
             'full_coverage': False,
         }
         },

546fc9ce9e0dc1cd04b48fa31fd821a128377d2e
Tomás González <tomasagustin.gonzalezorlando@arm.com>
2023-08-17 16:56:42 +0100

    Revert "Add opt-testcases into check list"
    ssl-opt.sh now takes care of looking at the files in the
    opt-testcases subdirectory, so use ssl-opt.sh directly
    in walk_ssl_opt_sh().
    
     * Revert commit f17a60f147ec3ba6a9f76da38d3802a7f9f05227.
    
    Signed-off-by: Tomás González <tomasagustin.gonzalezorlando@arm.com>

--- tests/scripts/check_test_cases.py
+++ tests/scripts/check_test_cases.py
@@ -143,9 +143,6 @@ state may override this method.
             ssl_opt_sh = os.path.join(directory, 'ssl-opt.sh')
             if os.path.exists(ssl_opt_sh):
                 self.walk_ssl_opt_sh(ssl_opt_sh)
-            for ssl_opt_file_name in glob.glob(os.path.join(directory, 'opt-testcases',
-                                                            '*.sh')):
-                self.walk_ssl_opt_sh(ssl_opt_file_name)
             compat_sh = os.path.join(directory, 'compat.sh')
             if os.path.exists(compat_sh):
                 self.walk_compat_sh(compat_sh)

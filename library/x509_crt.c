4a46d23a36528816af28bf64abd3349d9841ada8
David Horstmann <david.horstmann@arm.com>
2023-08-18 18:36:02 +0100

    Fix: Set type_id in x509_get_other_name()
    When parsing a subject alternative name of type otherName, retain the
    type-id field of the otherName. Previously this was not copied to the
    mbedtls_x509_san_other_name struct when it should have been.
    
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- library/x509_crt.c
+++ library/x509_crt.c
@@ -1741,6 +1741,7 @@ static int x509_get_other_name(const mbedtls_x509_buf *subject_alt_name,
     if (MBEDTLS_OID_CMP(MBEDTLS_OID_ON_HW_MODULE_NAME, &cur_oid) != 0) {
         return MBEDTLS_ERR_X509_FEATURE_UNAVAILABLE;
     }
+    other_name->type_id = cur_oid;
 
     p += len;
     if ((ret = mbedtls_asn1_get_tag(&p, end, &len,

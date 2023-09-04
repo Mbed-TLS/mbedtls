fc830590b28a5d5b548b677d32a9eaa0306fcef7
Xiaokang Qian <xiaokang.qian@arm.com>
2023-09-04 10:57:55 +0000

    Move functions out of the static file
    Move get_key_buf_size/get_builtin_key out of
        the psa wrapper auto generated file
    Slot_management.c include the head file instead of the source file
    
    Signed-off-by: Xiaokang Qian <xiaokang.qian@arm.com>

--- library/psa_crypto_driver_wrappers_no_static.c
+++ library/psa_crypto_driver_wrappers_no_static.c
@@ -156,5 +156,86 @@ psa_status_t psa_driver_wrapper_export_public_key(
 
 }
 
+/** Get the key buffer size required to store the key material of a key
+ *  associated with an opaque driver.
+ *
+ * \param[in] attributes  The key attributes.
+ * \param[out] key_buffer_size  Minimum buffer size to contain the key material
+ *
+ * \retval #PSA_SUCCESS
+ *         The minimum size for a buffer to contain the key material has been
+ *         returned successfully.
+ * \retval #PSA_ERROR_NOT_SUPPORTED
+ *         The type and/or the size in bits of the key or the combination of
+ *         the two is not supported.
+ * \retval #PSA_ERROR_INVALID_ARGUMENT
+ *         The key is declared with a lifetime not known to us.
+ */
+
+psa_status_t psa_driver_wrapper_get_key_buffer_size(
+    const psa_key_attributes_t *attributes,
+    size_t *key_buffer_size)
+{
+    psa_key_location_t location = PSA_KEY_LIFETIME_GET_LOCATION(attributes->core.lifetime);
+    psa_key_type_t key_type = attributes->core.type;
+    size_t key_bits = attributes->core.bits;
+
+    *key_buffer_size = 0;
+    switch (location) {
+#if defined(PSA_CRYPTO_DRIVER_TEST)
+        case PSA_CRYPTO_TEST_DRIVER_LOCATION:
+#if defined(MBEDTLS_PSA_CRYPTO_BUILTIN_KEYS)
+            /* Emulate property 'builtin_key_size' */
+            if (psa_key_id_is_builtin(
+                    MBEDTLS_SVC_KEY_ID_GET_KEY_ID(
+                        psa_get_key_id(attributes)))) {
+                *key_buffer_size = sizeof(psa_drv_slot_number_t);
+                return PSA_SUCCESS;
+            }
+#endif /* MBEDTLS_PSA_CRYPTO_BUILTIN_KEYS */
+            *key_buffer_size = mbedtls_test_opaque_size_function(key_type,
+                                                                 key_bits);
+            return (*key_buffer_size != 0) ?
+                   PSA_SUCCESS : PSA_ERROR_NOT_SUPPORTED;
+#endif /* PSA_CRYPTO_DRIVER_TEST */
+
+        default:
+            (void) key_type;
+            (void) key_bits;
+            return PSA_ERROR_INVALID_ARGUMENT;
+    }
+}
+
+psa_status_t psa_driver_wrapper_get_builtin_key(
+    psa_drv_slot_number_t slot_number,
+    psa_key_attributes_t *attributes,
+    uint8_t *key_buffer, size_t key_buffer_size, size_t *key_buffer_length)
+{
+
+    psa_key_location_t location = PSA_KEY_LIFETIME_GET_LOCATION(attributes->core.lifetime);
+    switch (location) {
+#if defined(PSA_CRYPTO_DRIVER_TEST)
+
+#if (defined(PSA_CRYPTO_DRIVER_TEST))
+        case 0x7fffff:
+            return mbedtls_test_opaque_get_builtin_key(
+                slot_number,
+                attributes,
+                key_buffer,
+                key_buffer_size,
+                key_buffer_length);
+#endif
+
+
+#endif /* PSA_CRYPTO_DRIVER_TEST */
+        default:
+            (void) slot_number;
+            (void) key_buffer;
+            (void) key_buffer_size;
+            (void) key_buffer_length;
+            return PSA_ERROR_DOES_NOT_EXIST;
+    }
+
+}
 
 #endif /* MBEDTLS_PSA_CRYPTO_C */

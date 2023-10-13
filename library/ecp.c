7d32b7b67cda6a90c6afa49f2a1fcc058274caaa
Tom Cosgrove <tom.cosgrove@arm.com>
2023-10-13 16:50:11 +0100

    mbedtls_ecp_read_key(): don't check priv key when no curves enabled
    Signed-off-by: Tom Cosgrove <tom.cosgrove@arm.com>

--- library/ecp.c
+++ library/ecp.c
@@ -3288,7 +3288,10 @@ int mbedtls_ecp_read_key(mbedtls_ecp_group_id grp_id, mbedtls_ecp_keypair *key,
         MBEDTLS_MPI_CHK(mbedtls_mpi_read_binary(&key->d, buf, buflen));
     }
 #endif
+#if defined(MBEDTLS_ECP_MONTGOMERY_ENABLED) || \
+    defined(MBEDTLS_ECP_SHORT_WEIERSTRASS_ENABLED)
     MBEDTLS_MPI_CHK(mbedtls_ecp_check_privkey(&key->grp, &key->d));
+#endif
 
 cleanup:
 

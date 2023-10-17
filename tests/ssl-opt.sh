d26069657c87981b9219022a721c226a71b257f1
Xiaokang Qian <xiaokang.qian@arm.com>
2023-10-17 10:07:42 +0000

    Process the case with server6 certificate
    Signed-off-by: Xiaokang Qian <xiaokang.qian@arm.com>

--- tests/ssl-opt.sh
+++ tests/ssl-opt.sh
@@ -403,6 +403,7 @@ detect_required_features() {
 
     case "$CMD_LINE" in
         *server5*|\
+        *server6*|\
         *dir-maxpath*)
             if [ "$TLS_VERSION" = "TLS13" ]; then
                 # In case of TLS13 the support for ECDSA is enough

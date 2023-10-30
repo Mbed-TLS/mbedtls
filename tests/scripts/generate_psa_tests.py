2bff1bfd472c103ca4809daa5283e05d6748a7c2
Tomás González <tomasagustin.gonzalezorlando@arm.com>
2023-10-30 15:29:23 +0000

    New test suite for the low-level hash interface
    Some basic test coverage for now:
    
    * Nominal operation.
    * Larger output buffer.
    * Clone an operation and use it after the original operation stops.
    
    Generate test data automatically. For the time being, only do that for
    hashes that Python supports natively. Supporting all algorithms is future
    work.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>
    Signed-off-by: Tomás González <tomasagustin.gonzalezorlando@arm.com>

--- tests/scripts/generate_psa_tests.py
+++ tests/scripts/generate_psa_tests.py
@@ -26,6 +26,7 @@ import sys
 from typing import Callable, Dict, FrozenSet, Iterable, Iterator, List, Optional
 
 import scripts_path # pylint: disable=unused-import
+from mbedtls_dev import crypto_data_tests
 from mbedtls_dev import crypto_knowledge
 from mbedtls_dev import macro_collector #pylint: disable=unused-import
 from mbedtls_dev import psa_information
@@ -809,6 +810,8 @@ class PSATestGenerator(test_data_generation.TestGenerator):
         lambda info: KeyGenerate(info).test_cases_for_key_generation(),
         'test_suite_psa_crypto_not_supported.generated':
         lambda info: KeyTypeNotSupported(info).test_cases_for_not_supported(),
+        'test_suite_psa_crypto_low_hash.generated':
+        lambda info: crypto_data_tests.HashPSALowLevel(info).all_test_cases(),
         'test_suite_psa_crypto_op_fail.generated':
         lambda info: OpFail(info).all_test_cases(),
         'test_suite_psa_crypto_storage_format.current':

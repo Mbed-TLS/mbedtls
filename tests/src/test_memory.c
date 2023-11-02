87270e533783e291baf734ab856b6bde978f24cd
Gilles Peskine <Gilles.Peskine@arm.com>
2023-11-02 17:14:01 +0100

    New files for memory-related test functions
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ tests/src/test_memory.c
@@ -0,0 +1,15 @@
+/**
+ * \file memory.c
+ *
+ * \brief   Helper functions related to testing memory management.
+ */
+
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
+#include <test/helpers.h>
+#include <test/macros.h>
+#include <test/memory.h>
+

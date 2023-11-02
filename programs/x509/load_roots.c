16799db69a2947b1c30e4fb17116901a93a7918b
Dave Rodgman <dave.rodgman@arm.com>
2023-11-02 19:47:20 +0000

    update headers
    Signed-off-by: Dave Rodgman <dave.rodgman@arm.com>

--- programs/x509/load_roots.c
+++ programs/x509/load_roots.c
@@ -2,25 +2,13 @@
  *  Root CA reading application
  *
  *  Copyright The Mbed TLS Contributors
- *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later OR GPL-2.0-or-later
  *
  *  This file is provided under the Apache License 2.0, or the
  *  GNU General Public License v2.0 or later.
  *
  *  **********
  *  Apache License 2.0:
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may
- *  not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
  *
  *  **********
  *

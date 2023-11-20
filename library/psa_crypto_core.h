34c434a3cadbe1525fea72e362d70962e341cdbf
David Horstmann <david.horstmann@arm.com>
2023-11-20 12:16:08 +0000

    Add initializers for input / output copies
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- library/psa_crypto_core.h
+++ library/psa_crypto_core.h
@@ -520,6 +520,8 @@ typedef struct psa_crypto_input_copy_s {
     size_t len;
 } psa_crypto_input_copy_t;
 
+#define PSA_CRYPTO_INPUT_COPY_INIT { NULL, 0 }
+
 /** Allocate a local copy of an input buffer.
  *
  * \param[in] input             Pointer to input buffer.
@@ -547,6 +549,8 @@ typedef struct psa_crypto_output_copy_s {
     uint8_t *buffer;
     size_t len;
 } psa_crypto_output_copy_t;
+
+#define PSA_CRYPTO_OUTPUT_COPY_INIT { NULL, NULL, 0 }
 
 /** Allocate a local copy of an output buffer.
  *

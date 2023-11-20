167ecbaa9091514ca671d359dec498455fe7069c
Ryan Everett <ryan.everett@arm.com>
2023-11-20 12:16:18 +0000

    Fix indentation causing code style failure
    Signed-off-by: Ryan Everett <ryan.everett@arm.com>

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -1711,7 +1711,7 @@ static psa_status_t psa_start_key_creation(
         status = psa_copy_key_material_into_slot(
             slot, (uint8_t *) (&slot_number), sizeof(slot_number));
         if (status != PSA_SUCCESS) {
-                return status;
+            return status;
         }
     }
 

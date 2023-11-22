b42c50bd6044b29d00012adf02bd8f607c5a2dc3
Thomas Daubney <thomas.daubney@arm.com>
2023-11-22 15:53:38 +0000

    Make use of new crypto_core_directory function
    Signed-off-by: Thomas Daubney <thomas.daubney@arm.com>

--- scripts/generate_driver_wrappers.py
+++ scripts/generate_driver_wrappers.py
@@ -178,10 +178,7 @@ def main() -> int:
 
     repo_root = os.path.abspath(args.repo_root)
 
-    if build_tree.looks_like_mbedtls_root(repo_root):
-        library_dir = 'library'
-    elif build_tree.looks_like_tf_psa_crypto_root(repo_root):
-        library_dir = 'core'
+    library_dir = build_tree.crypto_core_directory(repo_root)
 
     output_directory = args.output_directory if args.output_directory is not None else \
         os.path.join(repo_root, library_dir)

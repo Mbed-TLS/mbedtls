b807cc6ebaea1d353bd877a4f7e110619477eab7
Yanray Wang <yanray.wang@arm.com>
2023-11-30 17:16:20 +0800

    Add checks for PK_[PARSE/WRITE]_C when PK_HAVE_ECC_KEYS is set
    When PK_HAVE_ECC_KEYS is set with PK_[PARSE/WRITE]_C, it needs OID_C
    to be enabled. This commit adds proper checks in check_config.h
    
    Signed-off-by: Yanray Wang <yanray.wang@arm.com>

--- include/mbedtls/check_config.h
+++ include/mbedtls/check_config.h
@@ -444,6 +444,16 @@
 #error "MBEDTLS_PK_WRITE_C defined, but not all prerequisites"
 #endif
 
+#if defined(MBEDTLS_PK_PARSE_C) && defined(MBEDTLS_PK_HAVE_ECC_KEYS) && \
+    !defined(MBEDTLS_OID_C)
+#error "MBEDTLS_PK_PARSE_C and MBEDTLS_PK_HAVE_ECC_KEYS require MBEDTLS_OID_C"
+#endif
+
+#if defined(MBEDTLS_PK_WRITE_C) && defined(MBEDTLS_PK_HAVE_ECC_KEYS) && \
+    !defined(MBEDTLS_OID_C)
+#error "MBEDTLS_PK_WRITE_C and MBEDTLS_PK_HAVE_ECC_KEYS require MBEDTLS_OID_C"
+#endif
+
 #if defined(MBEDTLS_PLATFORM_EXIT_ALT) && !defined(MBEDTLS_PLATFORM_C)
 #error "MBEDTLS_PLATFORM_EXIT_ALT defined, but not all prerequisites"
 #endif

304fa091cf47fcdc4e289c1c2e5aa23abf856384
Oldes Huhuman <oldes.huhuman@gmail.com>
2023-12-01 12:23:26 +0100

    Shortening a comment line Related to: https://github.com/Mbed-TLS/mbedtls/issues/8562
    Signed-off-by: Oldes Huhuman <oldes.huhuman@gmail.com>

--- library/platform_util.c
+++ library/platform_util.c
@@ -245,7 +245,7 @@ extern inline void mbedtls_put_unaligned_uint64(void *p, uint64_t x);
     (defined(unix) || defined(__unix) || defined(__unix__) || \
     (defined(__APPLE__) && defined(__MACH__)) || defined(__HAIKU__))
 #include <unistd.h>
-#endif /* !_WIN32 && (unix || __unix || __unix__ || (__APPLE__ && __MACH__) || defined(__HAIKU__)) */
+#endif /* !_WIN32 && (unix || __unix || __unix__ || (__APPLE__ && __MACH__) || __HAIKU__) */
 #if (defined(_POSIX_VERSION) && _POSIX_VERSION >= 199309L) || defined(__HAIKU__)
 mbedtls_ms_time_t mbedtls_ms_time(void)
 {

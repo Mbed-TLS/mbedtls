f05b768457dd5088089391d3d350a74a53389ed9
Thomas Daubney <thomas.daubney@arm.com>
2023-12-08 09:47:48 +0000

    Use existing variable containing full path
    Signed-off-by: Thomas Daubney <thomas.daubney@arm.com>

--- scripts/generate_driver_wrappers.py
+++ scripts/generate_driver_wrappers.py
@@ -181,7 +181,7 @@ def main() -> int:
     crypto_core_directory = build_tree.crypto_core_directory(project_root)
 
     output_directory = args.output_directory if args.output_directory is not None else \
-        os.path.join(project_root, crypto_core_directory)
+        crypto_core_directory
 
     template_directory = args.template_dir if args.template_dir is not None else \
         os.path.join(project_root,

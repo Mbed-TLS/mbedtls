7863627bd6fe7320542095fde87eae093b30a61d
Valerio Setti <valerio.setti@nordicsemi.no>
2024-01-04 13:17:04 +0100

    psa_util: remove support for secp224k1 in EC conversion functions
    Signed-off-by: Valerio Setti <valerio.setti@nordicsemi.no>

--- library/psa_util.c
+++ library/psa_util.c
@@ -232,11 +232,7 @@ psa_ecc_family_t mbedtls_ecc_group_to_psa(mbedtls_ecp_group_id grpid,
             return PSA_ECC_FAMILY_SECP_K1;
 #endif
 #if defined(MBEDTLS_ECP_HAVE_SECP224K1)
-        /* secp224k1 has 224-bit coordinates but 225-bit private keys.
-         * The nominal key size in PSA is the private key size, hence 225. */
-        case MBEDTLS_ECP_DP_SECP224K1:
-            *bits = 225;
-            return PSA_ECC_FAMILY_SECP_K1;
+    /* secp224k1 is not and will not be supported in PSA (#3541). */
 #endif
 #if defined(MBEDTLS_ECP_HAVE_SECP256K1)
         case MBEDTLS_ECP_DP_SECP256K1:
@@ -320,10 +316,7 @@ mbedtls_ecp_group_id mbedtls_ecc_group_from_psa(psa_ecc_family_t curve,
                     return MBEDTLS_ECP_DP_SECP192K1;
 #endif
 #if defined(PSA_WANT_ECC_SECP_K1_224)
-                /* secp224k1 has 224-bit coordinates but 225-bit private keys.
-                 * The nominal key size in PSA is the private key size, hence 225. */
-                case 225:
-                    return MBEDTLS_ECP_DP_SECP224K1;
+            /* secp224k1 is not and will not be supported in PSA (#3541). */
 #endif
 #if defined(PSA_WANT_ECC_SECP_K1_256)
                 case 256:

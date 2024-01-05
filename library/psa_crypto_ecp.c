0d438fa390355958c0745f0393859110029f0bf6
Valerio Setti <valerio.setti@nordicsemi.no>
2024-01-05 10:33:51 +0100

    psa_crypto_ecp: fix comment for secp224k1 in check_ecc_parameters
    Signed-off-by: Valerio Setti <valerio.setti@nordicsemi.no>

--- library/psa_crypto_ecp.c
+++ library/psa_crypto_ecp.c
@@ -80,8 +80,8 @@ static int check_ecc_parameters(psa_ecc_family_t family, size_t *bits)
                 case 256:
                     return PSA_SUCCESS;
                 /* secp224k1 is not and will not be supported in PSA (#3541).
-                 * Note: secp224k1 has 225-bit private keys which are rounded
-                 * up to 232 for their representation. */
+                 * Note: secp224k1 has 224-bit coordinates but 225-bit private
+                 * keys which are rounded up to 232 for their representation. */
                 case 224:
                 case 232:
                     return PSA_ERROR_NOT_SUPPORTED;

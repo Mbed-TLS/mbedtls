4e8215057c4dfb2ea38dce1b3214f40ea6566665
David Horstmann <david.horstmann@arm.com>
2024-02-06 15:44:08 +0000

    Generate test wrappers for psa_generate_random()
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- tests/scripts/generate_psa_wrappers.py
+++ tests/scripts/generate_psa_wrappers.py
@@ -171,6 +171,8 @@ class PSAWrapperGenerator(c_wrapper_generator.Base):
                              'psa_hash_verify',
                              'psa_hash_compute',
                              'psa_hash_compare'):
+
+        if function_name == 'psa_generate_random':
             return True
         return False
 

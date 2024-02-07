447bbce8b4e7fd95388dc365b71c09202cb3d238
Valerio Setti <valerio.setti@nordicsemi.no>
2024-02-07 08:02:03 +0100

    rsa: remove unnecessary check in priv/pub key parsing
    Signed-off-by: Valerio Setti <valerio.setti@nordicsemi.no>

--- library/rsa.c
+++ library/rsa.c
@@ -107,11 +107,8 @@ int mbedtls_rsa_parse_key(mbedtls_rsa_context *rsa, const unsigned char *key, si
         return ret;
     }
 
+    /* mbedtls_asn1_get_tag() already ensures that len is valid (i.e. p+len <= end)*/
     end = p + len;
-
-    if (end > (key + keylen)) {
-        return MBEDTLS_ERR_ASN1_LENGTH_MISMATCH;
-    }
 
     if ((ret = mbedtls_asn1_get_int(&p, end, &version)) != 0) {
         return ret;
@@ -243,11 +240,8 @@ int mbedtls_rsa_parse_pubkey(mbedtls_rsa_context *rsa, const unsigned char *key,
         return ret;
     }
 
+    /* mbedtls_asn1_get_tag() already ensures that len is valid (i.e. p+len <= end)*/
     end = p + len;
-
-    if (end > (key + keylen)) {
-        return MBEDTLS_ERR_ASN1_LENGTH_MISMATCH;
-    }
 
     /* Import N */
     if ((ret = mbedtls_asn1_get_tag(&p, end, &len, MBEDTLS_ASN1_INTEGER)) != 0) {

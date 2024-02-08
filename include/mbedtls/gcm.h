4a9610bffede9b709453d3200e1e15b35beb82b8
Matthias Schulz <mschulz@hilscher.com>
2024-02-08 18:39:17 +0100

    Remove comments referencing private defines.
    Signed-off-by: Matthias Schulz <mschulz@hilscher.com>

--- include/mbedtls/gcm.h
+++ include/mbedtls/gcm.h
@@ -70,11 +70,7 @@ typedef struct mbedtls_gcm_context {
     unsigned char MBEDTLS_PRIVATE(mode);                     /*!< The operation to perform:
                                                               #MBEDTLS_GCM_ENCRYPT or
                                                               #MBEDTLS_GCM_DECRYPT. */
-    unsigned char MBEDTLS_PRIVATE(acceleration);             /*!< The acceleration to use:
-                                                              #MBEDTLS_GCM_ACC_SMALLTABLE,
-                                                              #MBEDTLS_GCM_ACC_LARGETABLE,
-                                                              #MBEDTLS_GCM_ACC_AESNI,
-                                                              #MBEDTLS_GCM_ACC_AESCE */
+    unsigned char MBEDTLS_PRIVATE(acceleration);             /*!< The acceleration to use. */
 }
 mbedtls_gcm_context;
 

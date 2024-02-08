61758e606ea226c275d8c667e539c964ecc0dcfe
Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>
2024-02-08 12:03:28 +0100

    Fix wrong dependency of ECJPAKE_C
    It always uses MD now. (The "fall back" to PSA Crypto was only in the
    1st iteration of driver-only hash support, before we changed the
    architecture to make everything go through MD.)
    
    Signed-off-by: Manuel Pégourié-Gonnard <manuel.pegourie-gonnard@arm.com>

--- include/mbedtls/check_config.h
+++ include/mbedtls/check_config.h
@@ -234,9 +234,8 @@
 #endif
 #endif /* MBEDTLS_PK_C && MBEDTLS_USE_PSA_CRYPTO */
 
-#if defined(MBEDTLS_ECJPAKE_C) &&           \
-    ( !defined(MBEDTLS_ECP_C) ||            \
-      !( defined(MBEDTLS_MD_C) || defined(MBEDTLS_PSA_CRYPTO_C) ) )
+#if defined(MBEDTLS_ECJPAKE_C) && \
+    (!defined(MBEDTLS_ECP_C) || !defined(MBEDTLS_MD_C))
 #error "MBEDTLS_ECJPAKE_C defined, but not all prerequisites"
 #endif
 

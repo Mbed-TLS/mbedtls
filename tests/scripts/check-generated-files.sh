557abf03882c65c65e8dbc96fb17ebd39ee350ef
David Horstmann <david.horstmann@arm.com>
2024-04-03 17:23:46 +0100

    Update references to tests/src and tests/include
    These are references that are part of the PSA shared memory testing
    work and thus were not previously updated.
    
    Signed-off-by: David Horstmann <david.horstmann@arm.com>

--- tests/scripts/check-generated-files.sh
+++ tests/scripts/check-generated-files.sh
@@ -148,4 +148,4 @@ fi
 # Generated files that are present in the repository even in the development
 # branch. (This is intended to be temporary, until the generator scripts are
 # fully reviewed and the build scripts support a generated header file.)
-check tests/scripts/generate_psa_wrappers.py tests/include/test/psa_test_wrappers.h tests/src/psa_test_wrappers.c
+check tests/scripts/generate_psa_wrappers.py framework/include/test/psa_test_wrappers.h framework/src/psa_test_wrappers.c

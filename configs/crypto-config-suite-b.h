c3051572cac18d6222fdc37c1294e5824eafe066
Ryan Everett <ryan.everett@arm.com>
2024-04-30 17:20:42 +0100

    Address suite-b comments
    Signed-off-by: Ryan Everett <ryan.everett@arm.com>

--- configs/crypto-config-suite-b.h
+++ configs/crypto-config-suite-b.h
@@ -25,7 +25,6 @@
 #ifndef PSA_CRYPTO_CONFIG_H
 #define PSA_CRYPTO_CONFIG_H
 
-#define PSA_WANT_ALG_ECB_NO_PADDING              1
 #define PSA_WANT_ALG_ECDH                        1
 #define PSA_WANT_ALG_ECDSA                       1
 #define PSA_WANT_ALG_GCM                         1
@@ -35,12 +34,10 @@
 #define PSA_WANT_ALG_SHA_512                     1
 #define PSA_WANT_ECC_SECP_R1_256                 1
 #define PSA_WANT_ALG_TLS12_PRF                   1
-#define PSA_WANT_ALG_TLS12_PSK_TO_MS             1
-#define PSA_WANT_ALG_TLS12_ECJPAKE_TO_PMS        1
 
 #define PSA_WANT_KEY_TYPE_AES                    1
 #define PSA_WANT_KEY_TYPE_ECC_KEY_PAIR_BASIC     1
-#define PSA_WANT_KEY_TYPE_ECC_KEY_PAIR_DERIVE    1
+#define PSA_WANT_KEY_TYPE_ECC_KEY_PAIR_IMPORT    1
 #define PSA_WANT_KEY_TYPE_ECC_KEY_PAIR_GENERATE  1
 #define PSA_WANT_KEY_TYPE_HMAC                   1
 #endif /* PSA_CRYPTO_CONFIG_H */

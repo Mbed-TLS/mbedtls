d74d2ab9db5532c4b0727ac356699bb9c64ce596
Valerio Setti <valerio.setti@nordicsemi.no>
2024-05-03 15:51:21 +0200

    check-generated-files: add test_certs.h file to the list of checked items
    Signed-off-by: Valerio Setti <valerio.setti@nordicsemi.no>

--- tests/scripts/check-generated-files.sh
+++ tests/scripts/check-generated-files.sh
@@ -132,6 +132,7 @@ check tests/scripts/generate_bignum_tests.py $(tests/scripts/generate_bignum_tes
 check tests/scripts/generate_ecp_tests.py $(tests/scripts/generate_ecp_tests.py --list)
 check tests/scripts/generate_psa_tests.py $(tests/scripts/generate_psa_tests.py --list)
 check tests/scripts/generate_test_keys.py tests/src/test_keys.h
+check tests/scripts/generate_test_cert_macros.py tests/src/test_certs.h
 check scripts/generate_driver_wrappers.py $library_dir/psa_crypto_driver_wrappers.h $library_dir/psa_crypto_driver_wrappers_no_static.c
 
 # Additional checks for Mbed TLS only

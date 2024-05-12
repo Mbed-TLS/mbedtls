605bc16e67d753272e665996fc94db31ddc39562
Gilles Peskine <Gilles.Peskine@arm.com>
2024-05-12 21:38:07 +0200

    Make the file a bit more readable
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- scripts/make_generated_files.bat
+++ scripts/make_generated_files.bat
@@ -1,6 +1,10 @@
 @rem Generate automatically-generated configuration-independent source files
 @rem and build scripts.
-@rem Perl and Python 3 must be on the PATH.
+@rem Requirements:
+@rem * Perl must be on the PATH ("perl" command).
+@rem * Python 3.8 or above must be on the PATH ("python" command).
+
+@rem @@@@ library\** @@@@
 @rem psa_crypto_driver_wrappers.h needs to be generated prior to
 @rem generate_visualc_files.pl being invoked.
 python scripts\generate_driver_wrappers.py || exit /b 1
@@ -8,8 +12,14 @@ perl scripts\generate_errors.pl || exit /b 1
 perl scripts\generate_query_config.pl || exit /b 1
 perl scripts\generate_features.pl || exit /b 1
 python scripts\generate_ssl_debug_helpers.py || exit /b 1
+
+@rem @@@@ Build @@@@
 perl scripts\generate_visualc_files.pl || exit /b 1
+
+@rem @@@@ programs\** @@@@
 python scripts\generate_psa_constants.py || exit /b 1
+
+@rem @@@@ tests\** @@@@
 python framework\scripts\generate_bignum_tests.py || exit /b 1
 python framework\scripts\generate_config_tests.py || exit /b 1
 python framework\scripts\generate_ecp_tests.py || exit /b 1

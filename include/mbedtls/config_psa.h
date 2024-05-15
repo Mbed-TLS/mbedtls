3153ae47a987fd05e735c2d49c3c12217f757e2c
Valerio Setti <valerio.setti@nordicsemi.no>
2024-05-15 07:39:47 +0200

    config_psa: do not update legacy symbols in client-only PSA build
    Signed-off-by: Valerio Setti <valerio.setti@nordicsemi.no>

--- include/mbedtls/config_psa.h
+++ include/mbedtls/config_psa.h
@@ -34,7 +34,11 @@
  * before we deduce what built-ins are required. */
 #include "psa/crypto_adjust_config_key_pair_types.h"
 
+#if defined(MBEDTLS_PSA_CRYPTO_C)
+/* If we are implementing PSA crypto ourselves, then we want to enable the
+ * required built-ins. Otherwise, PSA features will be provided by the server. */
 #include "mbedtls/config_adjust_legacy_from_psa.h"
+#endif
 
 #else /* MBEDTLS_PSA_CRYPTO_CONFIG */
 

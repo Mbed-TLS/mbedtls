0d915a90eabfbdfc51c2974f497e283234c57a3f
Thomas Daubney <thomas.daubney@arm.com>
2024-06-20 16:45:51 +0100

    Remove final references to padlock
    Signed-off-by: Thomas Daubney <thomas.daubney@arm.com>

--- scripts/generate_errors.pl
+++ scripts/generate_errors.pl
@@ -36,7 +36,7 @@ my $error_format_file = $data_dir.'/error.fmt';
 my @low_level_modules = qw( AES ARIA ASN1 BASE64 BIGNUM
                             CAMELLIA CCM CHACHA20 CHACHAPOLY CMAC CTR_DRBG DES
                             ENTROPY ERROR GCM HKDF HMAC_DRBG LMS MD5
-                            NET OID PADLOCK PBKDF2 PLATFORM POLY1305 RIPEMD160
+                            NET OID PBKDF2 PLATFORM POLY1305 RIPEMD160
                             SHA1 SHA256 SHA512 SHA3 THREADING );
 my @high_level_modules = qw( CIPHER DHM ECP MD
                              PEM PK PKCS12 PKCS5

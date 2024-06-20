53377f13bad7570a643075bfec02cad358d5bb6f
Thomas Daubney <thomas.daubney@arm.com>
2024-06-20 16:17:46 +0100

    Update comment in further places
    Signed-off-by: Thomas Daubney <thomas.daubney@arm.com>

--- tf-psa-crypto/drivers/builtin/src/aes.c
+++ tf-psa-crypto/drivers/builtin/src/aes.c
@@ -1925,8 +1925,9 @@ int mbedtls_aes_self_test(int verbose)
 
             /*
              * AES-192 is an optional feature that may be unavailable when
-             * there is an alternative underlying implementation such as a
-             * PSA driver.
+             * there is an alternative underlying implementation i.e when
+             * MBEDTLS_AES_SETKEY_ENC_ALT or MBEDTLS_AES_SETKEY_DEC_ALT
+             * are defined.
              */
             if (ret == MBEDTLS_ERR_PLATFORM_FEATURE_UNSUPPORTED && keybits == 192) {
                 mbedtls_printf("skipped\n");
@@ -1992,8 +1993,9 @@ int mbedtls_aes_self_test(int verbose)
             ret = mbedtls_aes_setkey_enc(&ctx, key, keybits);
             /*
              * AES-192 is an optional feature that may be unavailable when
-             * there is an alternative underlying implementation such as a
-             * PSA driver.
+             * there is an alternative underlying implementation i.e when
+             * MBEDTLS_AES_SETKEY_ENC_ALT or MBEDTLS_AES_SETKEY_DEC_ALT
+             * are defined.
              */
             if (ret == MBEDTLS_ERR_PLATFORM_FEATURE_UNSUPPORTED && keybits == 192) {
                 mbedtls_printf("skipped\n");
@@ -2056,8 +2058,9 @@ int mbedtls_aes_self_test(int verbose)
             ret = mbedtls_aes_setkey_enc(&ctx, key, keybits);
             /*
              * AES-192 is an optional feature that may be unavailable when
-             * there is an alternative underlying implementation such as a
-             * PSA driver.
+             * there is an alternative underlying implementation i.e when
+             * MBEDTLS_AES_SETKEY_ENC_ALT or MBEDTLS_AES_SETKEY_DEC_ALT
+             * are defined.
              */
             if (ret == MBEDTLS_ERR_PLATFORM_FEATURE_UNSUPPORTED && keybits == 192) {
                 mbedtls_printf("skipped\n");

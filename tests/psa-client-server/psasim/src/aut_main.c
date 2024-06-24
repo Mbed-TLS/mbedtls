e9829e59c50878680344bf167aa0be616bf83457
Valerio Setti <valerio.setti@nordicsemi.no>
2024-06-24 16:37:27 +0200

    psasim: add AUT for cipher encryption/decryption
    Signed-off-by: Valerio Setti <valerio.setti@nordicsemi.no>

--- tests/psa-client-server/psasim/src/aut_main.c
+++ tests/psa-client-server/psasim/src/aut_main.c
@@ -23,6 +23,7 @@ int psa_hash_compute_main(void);
 int psa_hash_main(void);
 int psa_aead_encrypt_main(char *cipher_name);
 int psa_aead_encrypt_decrypt_main(void);
+int psa_cipher_encrypt_decrypt_main(void);
 int psa_random_main(void);
 int psa_mac_main(void);
 int psa_key_agreement_main(void);
@@ -53,13 +54,14 @@ int main()
     TEST_MODULE(psa_aead_encrypt_main("aes256-gcm"));
     TEST_MODULE(psa_aead_encrypt_main("aes128-gcm_8"));
     TEST_MODULE(psa_aead_encrypt_main("chachapoly"));
+    TEST_MODULE(psa_aead_encrypt_decrypt_main());
+    TEST_MODULE(psa_cipher_encrypt_decrypt_main());
 
     TEST_MODULE(psa_random_main());
 
     TEST_MODULE(psa_mac_main());
     TEST_MODULE(psa_key_agreement_main());
     TEST_MODULE(psa_sign_verify_main());
-    TEST_MODULE(psa_aead_encrypt_decrypt_main());
     TEST_MODULE(psa_hkdf_main());
 
 exit:

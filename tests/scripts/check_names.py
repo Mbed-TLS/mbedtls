6921d547b6687b74d147af2971dfe076edbb7918
Ronald Cron <ronald.cron@arm.com>
2024-07-02 08:33:05 +0200

    Adapt check_names.py
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- tests/scripts/check_names.py
+++ tests/scripts/check_names.py
@@ -245,10 +245,14 @@ class CodeParser():
         ])
         all_macros["internal"] = self.parse_macros([
             "library/*.h",
+            "tf-psa-crypto/core/*.h",
+            "tf-psa-crypto/drivers/builtin/src/*.h",
             "tests/include/test/drivers/*.h",
         ])
         all_macros["private"] = self.parse_macros([
             "library/*.c",
+            "tf-psa-crypto/core/*.c",
+            "tf-psa-crypto/drivers/builtin/src/*.c",
         ])
         enum_consts = self.parse_enum_consts([
             "include/mbedtls/*.h",
@@ -256,7 +260,11 @@ class CodeParser():
             "tf-psa-crypto/include/psa/*.h",
             "tf-psa-crypto/drivers/builtin/include/mbedtls/*.h",
             "library/*.h",
+            "tf-psa-crypto/core/*.h",
+            "tf-psa-crypto/drivers/builtin/src/*.h",
             "library/*.c",
+            "tf-psa-crypto/core/*.c",
+            "tf-psa-crypto/drivers/builtin/src/*.c",
             "3rdparty/everest/include/everest/everest.h",
             "3rdparty/everest/include/everest/x25519.h"
         ])
@@ -266,6 +274,8 @@ class CodeParser():
             "tf-psa-crypto/include/psa/*.h",
             "tf-psa-crypto/drivers/builtin/include/mbedtls/*.h",
             "library/*.h",
+            "tf-psa-crypto/core/*.h",
+            "tf-psa-crypto/drivers/builtin/src/*.h",
             "3rdparty/everest/include/everest/everest.h",
             "3rdparty/everest/include/everest/x25519.h"
         ], ["3rdparty/p256-m/p256-m/p256-m.h"])
@@ -275,12 +285,16 @@ class CodeParser():
             "tf-psa-crypto/include/psa/*.h",
             "tf-psa-crypto/drivers/builtin/include/mbedtls/*.h",
             "library/*.h",
+            "tf-psa-crypto/core/*.h",
+            "tf-psa-crypto/drivers/builtin/src/*.h",
             "3rdparty/everest/include/everest/everest.h",
             "3rdparty/everest/include/everest/x25519.h",
             "library/*.c",
+            "tf-psa-crypto/core/*.c",
+            "tf-psa-crypto/drivers/builtin/src/*.c",
             "3rdparty/everest/library/everest.c",
             "3rdparty/everest/library/x25519.c"
-        ], ["library/psa_crypto_driver_wrappers.h"])
+        ], ["tf-psa-crypto/core/psa_crypto_driver_wrappers.h"])
         symbols = self.parse_symbols()
 
         # Remove identifier macros like mbedtls_printf or mbedtls_calloc

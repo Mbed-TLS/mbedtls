d5d69bb4a07ee916e794f73dcac51fd257c774ff
Gilles Peskine <Gilles.Peskine@arm.com>
2024-07-29 16:30:44 +0200

    Match spacing in pointer types in documentation with the code style
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tf-psa-crypto/include/psa/crypto_struct.h
+++ tf-psa-crypto/include/psa/crypto_struct.h
@@ -245,9 +245,9 @@ struct psa_custom_key_parameters_s {
  */
 /* This is a deprecated variant of `struct psa_custom_key_parameters_s`.
  * It has exactly the same layout, plus an extra field which is a flexible
- * array member. Thus a `const struct psa_key_production_parameters_s*`
+ * array member. Thus a `const struct psa_key_production_parameters_s *`
  * can be passed to any function that reads a
- * `const struct psa_custom_key_parameters_s*`.
+ * `const struct psa_custom_key_parameters_s *`.
  */
 struct psa_key_production_parameters_s {
     uint32_t flags;

b298d1f8d228781a5918fc53dcaf0dbf15538451
Minos Galanakis <minos.galanakis@arm.com>
2024-08-01 16:58:17 +0100

    Extract compliance-components into a separate file.
    Signed-off-by: Minos Galanakis <minos.galanakis@arm.com>

--- tests/scripts/components-compliance.sh
+++ tests/scripts/components-compliance.sh
@@ -8,3 +8,24 @@
 ################################################################
 #### Compliance Testing
 ################################################################
+
+component_test_psa_compliance () {
+    # The arch tests build with gcc, so require use of gcc here to link properly
+    msg "build: make, default config (out-of-box), libmbedcrypto.a only"
+    CC=gcc make -C library libmbedcrypto.a
+
+    msg "unit test: test_psa_compliance.py"
+    CC=gcc ./tests/scripts/test_psa_compliance.py
+}
+
+support_test_psa_compliance () {
+    # psa-compliance-tests only supports CMake >= 3.10.0
+    ver="$(cmake --version)"
+    ver="${ver#cmake version }"
+    ver_major="${ver%%.*}"
+
+    ver="${ver#*.}"
+    ver_minor="${ver%%.*}"
+
+    [ "$ver_major" -eq 3 ] && [ "$ver_minor" -ge 10 ]
+}

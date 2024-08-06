83222b8c4158d1887414c59488c77de018ea08fe
Minos Galanakis <minos.galanakis@arm.com>
2024-08-06 12:06:51 +0100

    Add -Wmissing-prototypes to component_build_no_ssl_srv and component_build_no_ssl_cli in all.sh
    Signed-off-by: Michael Schuster <michael@schuster.ms>
    Signed-off-by: Minos Galanakis <minos.galanakis@arm.com>

--- tests/scripts/components-configuration-tls.sh
+++ tests/scripts/components-configuration-tls.sh
@@ -315,14 +315,14 @@ component_build_no_ssl_srv () {
     msg "build: full config except SSL server, make, gcc" # ~ 30s
     scripts/config.py full
     scripts/config.py unset MBEDTLS_SSL_SRV_C
-    make CC=gcc CFLAGS='-Werror -Wall -Wextra -O1'
+    make CC=gcc CFLAGS='-Werror -Wall -Wextra -O1 -Wmissing-prototypes'
 }
 
 component_build_no_ssl_cli () {
     msg "build: full config except SSL client, make, gcc" # ~ 30s
     scripts/config.py full
     scripts/config.py unset MBEDTLS_SSL_CLI_C
-    make CC=gcc CFLAGS='-Werror -Wall -Wextra -O1'
+    make CC=gcc CFLAGS='-Werror -Wall -Wextra -O1 -Wmissing-prototypes'
 }
 
 component_test_no_max_fragment_length () {

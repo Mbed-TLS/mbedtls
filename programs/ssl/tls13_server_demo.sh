ff399eaad1bc1c66bf0cf1c54e9269a85bc73a43
Gilles Peskine <Gilles.Peskine@arm.com>
2024-08-27 14:33:05 +0200

    Demo scripts for a basic TLS server
    Add demo scripts for the basic TLS server ssl_server, running against an
    OpenSSL client.
    
    Have separate scripts for TLS 1.2 and TLS 1.3. This makes it easier to
    specify configuration dependencies and track failures.
    
    This commit does not yet handle DLTS with dtls_server.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ programs/ssl/tls13_server_demo.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Copyright The Mbed TLS Contributors
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+
+program="${0%/*}"/ssl_server
+protocol='TLS 1.3'
+
+. "${0%/*}/tls_server_demo_common.sh"
+
+depends_on MBEDTLS_SSL_PROTO_TLS1_3 MBEDTLS_SSL_TLS1_3_KEY_EXCHANGE_MODE_EPHEMERAL_ENABLED
+
+run_one_connection -tls1_3
+
+cleanup
---
+++ programs/ssl/tls_server_demo_common.sh
@@ -0,0 +1,54 @@
+## Common parts for an interoperability demo between an Mbed TLS server
+## and an OpenSSL client.
+
+# Required named parameters:
+# * $protocol: human-readable protocol version.
+# * $program: server program to run.
+
+# Copyright The Mbed TLS Contributors
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+
+. "${0%/*}/../demo_common.sh"
+
+msg <<'EOF'
+This script demonstrates the interoperability between a very simple
+$protocol server using Mbed TLS and an OpenSSL client.
+
+EOF
+
+if ! openssl version >/dev/null; then
+    echo >&2 "This demo script requires the 'openssl' command line program."
+    exit 100
+fi
+
+depends_on MBEDTLS_SSL_TLS_C MBEDTLS_SSL_SRV_C
+
+client_log="openssl-s_client-$$.log"
+files_to_clean="$client_log"
+
+run_one_connection () {
+    echo
+    echo "#### Local connection: $protocol ####"
+
+    # Start the server in the background
+    printf '+ %s &\n' "$program"
+    "$program" &
+    server_pid=$!
+
+    # Give the server a reasonable amount of time to start
+    sleep 1
+    set openssl s_client -connect localhost:4433 "$@"
+    printf '\n+ %s\n' "$*"
+    echo "This is some content." | "$@" >"$client_log" 2>&1
+
+    ret=0
+    kill "$server_pid" || wait "$server_pid" || ret=$?
+    if [ "$ret" -ne 0 ]; then
+        echo "FAIL: $* returned $ret"
+        echo "BEGIN client output"
+        cat "$client_log"
+        echo "END client output"
+        rm "$client_log"
+    fi
+    return "$ret"
+}

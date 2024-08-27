f69845c69f37dcb5d9a70ac2a4376fbb68270cd3
Gilles Peskine <Gilles.Peskine@arm.com>
2024-08-27 12:17:39 +0200

    Demo scripts for a basic TLS client
    Add demo scripts for the basic TLS client ssl_client1, running against an
    OpenSSL server.
    
    Have separate scripts for TLS 1.2 and TLS 1.3. This makes it easier to
    specify configuration dependencies and track failures.
    
    This commit does not yet handle DLTS with dtls_client.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ programs/ssl/tls12_client_demo.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Copyright The Mbed TLS Contributors
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+
+program="${0%/*}"/ssl_client1
+protocol='TLS 1.2'
+
+. "${0%/*}/tls_client_demo_common.sh"
+
+depends_on MBEDTLS_SSL_PROTO_TLS1_2
+if ! { config_has MBEDTLS_KEY_EXCHANGE_ECDH_ECDSA_ENABLED ||
+       config_has MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED ||
+       config_has MBEDTLS_KEY_EXCHANGE_ECDH_RSA_ENABLED ||
+       config_has MBEDTLS_KEY_EXCHANGE_ECDHE_RSA_ENABLED ||
+       config_has MBEDTLS_KEY_EXCHANGE_DHE_RSA_ENABLED ||
+       config_has MBEDTLS_KEY_EXCHANGE_RSA_ENABLED; }; then
+    depends_on 'MBEDTLS_KEY_EXCHANGE_<any-non-PSK>_ENABLED'
+fi
+
+run_one_connection -www -tls1_2
+
+cleanup
---
+++ programs/ssl/tls_client_demo_common.sh
@@ -0,0 +1,56 @@
+## Common parts for an interoperability demo between an Mbed TLS client
+## and an OpenSSL server.
+
+# Required named parameters:
+# * $protocol: human-readable protocol version.
+# * $program: client program to run.
+
+# Copyright The Mbed TLS Contributors
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+
+. "${0%/*}/../demo_common.sh"
+
+msg <<'EOF'
+This script demonstrates the interoperability between a very simple
+$protocol client using Mbed TLS and an OpenSSL server.
+
+EOF
+
+if ! openssl version >/dev/null; then
+    echo >&2 "This demo script requires the 'openssl' command line program."
+    exit 100
+fi
+
+depends_on MBEDTLS_SSL_TLS_C MBEDTLS_SSL_CLI_C
+
+server_log="openssl-s_server-$$.log"
+files_to_clean="$server_log"
+
+run_one_connection () {
+    echo
+    echo "#### Local connection: $protocol ####"
+    # Pass a key and certificate. The ssl_client1 program doesn't actually
+    # check the certificates, so it doesn't matter what we pass here.
+    set -- \
+        -key "$root_dir/framework/data_files/server5.key" \
+        -cert "$root_dir/framework/data_files/server5.crt" \
+        "$@"
+    set openssl s_server -accept 4433 "$@"
+    printf '+ %s &\n' "$*"
+    "$@" >"$server_log" 2>&1 &
+    server_pid=$!
+    # Give the server a reasonable amount of time to start
+    sleep 1
+    ret=0
+    printf '+ %s\n' "$program"
+    "$program" || ret=$?
+    kill "$server_pid" 2>/dev/null || true # The server may exit first
+    if [ "$ret" -ne 0 ]; then
+        echo "FAIL: $program returned $ret"
+        echo "BEGIN server output"
+        cat "$server_log"
+        echo "END server output"
+        rm "$server_log"
+    fi
+    return "$ret"
+}

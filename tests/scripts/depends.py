6f90e79ae5c9d410e68168cc8d4ae15dae01fef8
Gabor Mezei <gabor.mezei@arm.com>
2024-09-03 16:58:25 +0200

    Update old class names
    Signed-off-by: Gabor Mezei <gabor.mezei@arm.com>

--- tests/scripts/depends.py
+++ tests/scripts/depends.py
@@ -537,7 +537,7 @@ def main():
                             default=True)
         options = parser.parse_args()
         os.chdir(options.directory)
-        conf = config.ConfigFile(options.config)
+        conf = config.MbedTLSConfig(options.config)
         domain_data = DomainData(options, conf)
 
         if options.tasks is True:

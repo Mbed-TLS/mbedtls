a602a41168eef1225b054c9dd15d3b74a5f0a47d
Gilles Peskine <Gilles.Peskine@arm.com>
2024-09-04 16:04:42 +0200

    Prepare to test SSL sample programs
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ tests/opt-testcases/sample.sh
@@ -0,0 +1,6 @@
+# Test that SSL sample programs can interoperate with OpenSSL and GnuTLS.
+
+# Copyright The Mbed TLS Contributors
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+
+: ${PROGRAMS_DIR:=../programs/ssl}

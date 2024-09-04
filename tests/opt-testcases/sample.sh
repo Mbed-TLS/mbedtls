f8b373e8693d6e728f2b055971b2c5d0082df373
Gilles Peskine <Gilles.Peskine@arm.com>
2024-09-04 16:07:56 +0200

    Test dtls_client
    Test against both OpenSSL and GnuTLS.
    
    Don't use a proxy. It's not particularly useful here, and would complicate
    figuring out port numbers.
    
    Clean up compile-time requirements in dtls_client.c: any certificate-based
    key exchange is ok, so don't insist on built-in RSA.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/opt-testcases/sample.sh
+++ tests/opt-testcases/sample.sh
@@ -46,3 +46,31 @@ run_test    "Sample: ssl_client1, gnutls server, TLS 1.3" \
             -c "<TD>Protocol version:</TD><TD>TLS1.3</TD>" \
             -S "Error" \
             -C "error"
+
+requires_protocol_version dtls12
+run_test    "Sample: dtls_client, openssl server, DTLS 1.2" \
+            -P 4433 \
+            "$O_SRV -dtls1_2" \
+            "$PROGRAMS_DIR/dtls_client" \
+            0 \
+            -s "Echo this" \
+            -s "DONE" \
+            -c "Echo this" \
+            -c "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -S "ERROR" \
+            -C "error"
+
+requires_protocol_version dtls12
+run_test    "Sample: dtls_client, gnutls server, DTLS 1.2" \
+            -P 4433 \
+            "$G_SRV -u --echo --priority=NORMAL:-VERS-TLS-ALL:+VERS-TLS1.2" \
+            "$PROGRAMS_DIR/dtls_client" \
+            0 \
+            -s "Server listening" \
+            -s "[1-9][0-9]* bytes command:" \
+            -c "Echo this" \
+            -c "[1-9][0-9]* bytes written" \
+            -c "[1-9][0-9]* bytes read" \
+            -S "Error" \
+            -C "error"

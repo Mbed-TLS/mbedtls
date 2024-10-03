efe084b2eeead0d3d2a1ecfd7e3f6e1212734986
Gilles Peskine <Gilles.Peskine@arm.com>
2024-10-03 17:38:32 +0200

    Create a module to split branch-independent code out of analyze_outcomes.py
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ tests/scripts/outcome_analysis.py
@@ -0,0 +1,9 @@
+"""Outcome file analysis code.
+
+This module is the bulk of the code of tests/scripts/analyze_outcomes.py
+in each consuming branch. The consuming script is expected to derive
+the classes with branch-specific customizations such as ignore lists.
+"""
+
+# Copyright The Mbed TLS Contributors
+# SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later

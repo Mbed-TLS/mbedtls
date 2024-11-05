61811e6595b42143a7181c1e42b022ad94b4f49c
Harry Ramsey <harry.ramsey@arm.com>
2024-11-05 06:31:42 +0000

    Update path in check-generated-files.sh
    This commit updates the path to generate_psa_constants.py in
    check-generated-files.sh
    
    Signed-off-by: Harry Ramsey <harry.ramsey@arm.com>

--- tests/scripts/check-generated-files.sh
+++ tests/scripts/check-generated-files.sh
@@ -146,6 +146,7 @@ check()
 # directory in Mbed TLS that is not just a TF-PSA-Crypto submodule.
 if [ -d tf-psa-crypto ]; then
     cd tf-psa-crypto
+    check ./scripts/generate_psa_constants.py ./programs/psa/psa_constant_names_generated.c
     check ../framework/scripts/generate_bignum_tests.py $(../framework/scripts/generate_bignum_tests.py --list)
     check ../framework/scripts/generate_config_tests.py tests/suites/test_suite_config.psa_boolean.data
     check ../framework/scripts/generate_ecp_tests.py $(../framework/scripts/generate_ecp_tests.py --list)

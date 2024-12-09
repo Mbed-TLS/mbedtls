4eaf54e646d1ad28b7ffa34b45a65d34247287be
Ronald Cron <ronald.cron@arm.com>
2024-12-09 18:46:11 +0100

    Move test_keys.h to include/test
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- scripts/make_generated_files.bat
+++ scripts/make_generated_files.bat
@@ -26,6 +26,6 @@ python framework\scripts\generate_bignum_tests.py || exit /b 1
 python framework\scripts\generate_config_tests.py || exit /b 1
 python framework\scripts\generate_ecp_tests.py || exit /b 1
 python framework\scripts\generate_psa_tests.py || exit /b 1
-python framework\scripts\generate_test_keys.py --output framework\tests\src\test_keys.h || exit /b 1
+python framework\scripts\generate_test_keys.py --output framework\tests\include\test\test_keys.h || exit /b 1
 python framework\scripts\generate_test_cert_macros.py --output tests\src\test_certs.h || exit /b 1
 python framework\scripts\generate_tls13_compat_tests.py || exit /b 1

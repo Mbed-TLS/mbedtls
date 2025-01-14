9a9c9a53c12e30c49493843766d01b83dd0e7ea5
Valerio Setti <valerio.setti@nordicsemi.no>
2025-01-14 17:04:09 +0100

    compat.sh: do not use DHE-PSK key exchange in gnutls tests
    DHE-PSK is being removed from Mbed TLS so we cannot use this key
    exchange with gnutls testing.
    
    Signed-off-by: Valerio Setti <valerio.setti@nordicsemi.no>

--- tests/compat.sh
+++ tests/compat.sh
@@ -620,7 +620,7 @@ setup_arguments()
     M_SERVER_ARGS="server_port=$PORT server_addr=0.0.0.0 force_version=$MODE"
     O_SERVER_ARGS="-accept $PORT -cipher ALL,COMPLEMENTOFALL -$O_MODE"
     G_SERVER_ARGS="-p $PORT --http $G_MODE"
-    G_SERVER_PRIO="NORMAL:${G_PRIO_CCM}+NULL:+MD5:+PSK:+DHE-PSK:+ECDHE-PSK:+SHA256:+SHA384:-VERS-TLS-ALL:$G_PRIO_MODE"
+    G_SERVER_PRIO="NORMAL:${G_PRIO_CCM}+NULL:+MD5:+PSK:+ECDHE-PSK:+SHA256:+SHA384:-VERS-TLS-ALL:$G_PRIO_MODE"
 
     # The default prime for `openssl s_server` depends on the version:
     # * OpenSSL <= 1.0.2a: 512-bit

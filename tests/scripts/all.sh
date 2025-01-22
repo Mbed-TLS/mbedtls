f6f3d1ceececc6a6892acf6a58bc1d66cbe9b2df
Gilles Peskine <Gilles.Peskine@arm.com>
2025-01-22 21:33:29 +0100

    MSan for constant flow: all.sh: support modern Clang
    Support Clang >=16 in component_test_memsan_constant_flow. Modern versions
    of Clang require an extra compiler option to avoid false positives.
    
    Fixes #9921.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/scripts/all.sh
+++ tests/scripts/all.sh
@@ -1724,15 +1724,14 @@ skip_suites_without_constant_flow () {
 component_test_memsan_constant_flow () {
     # This tests both (1) accesses to undefined memory, and (2) branches or
     # memory access depending on secret values. To distinguish between those:
-    # - unset MBEDTLS_TEST_CONSTANT_FLOW_MEMSAN - does the failure persist?
-    # - or alternatively, change the build type to MemSanDbg, which enables
+    # - change the build type to MemSan - does the failure persist?
+    # - or alternatively, change the build type to CFMemSanDbg, which enables
     # origin tracking and nicer stack traces (which are useful for debugging
     # anyway), and check if the origin was TEST_CF_SECRET() or something else.
     msg "build: cmake MSan (clang), full config with constant flow testing"
     scripts/config.py full
-    scripts/config.py set MBEDTLS_TEST_CONSTANT_FLOW_MEMSAN
     scripts/config.py unset MBEDTLS_AESNI_C # memsan doesn't grok asm
-    CC=clang cmake -D CMAKE_BUILD_TYPE:String=MemSan .
+    CC=clang cmake -D CMAKE_BUILD_TYPE:String=CFMemSan .
     make
 
     msg "test: main suites (Msan + constant flow)"

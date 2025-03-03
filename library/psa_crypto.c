e014887ea57dbe323279598dbeaf704dd3a382df
Waleed Elmelegy <waleed.elmelegy@arm.com>
2025-03-03 15:01:38 +0000

    Fix code style for key derivation input function
    Signed-off-by: Waleed Elmelegy <waleed.elmelegy@arm.com>

--- library/psa_crypto.c
+++ library/psa_crypto.c
@@ -4752,7 +4752,7 @@ static psa_status_t psa_key_derivation_input_internal(
     psa_algorithm_t kdf_alg = psa_key_derivation_get_kdf_alg(operation);
 
     if (kdf_alg == 0) {
-       /* This is a blank or aborted operation. */
+        /* This is a blank or aborted operation. */
         status = PSA_ERROR_BAD_STATE;
         goto exit;
     }

bc0255592fed525ac19459f6bf7f1f68540b680d
Gilles Peskine <Gilles.Peskine@arm.com>
2025-03-11 13:47:49 +0100

    Clarify DTLS
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- include/mbedtls/ssl.h
+++ include/mbedtls/ssl.h
@@ -4985,7 +4985,7 @@ int mbedtls_ssl_get_session(const mbedtls_ssl_context *ssl,
  *                 not selected).
  *
  * \note           Defragmentation of incoming handshake messages in TLS
- *                 (excluding DTLS) is supported with some limitations:
+ *                 is supported with some limitations:
  *                 - On an Mbed TLS server that only accepts TLS 1.2,
  *                   the initial ClientHello message must not be fragmented.
  *                   A TLS 1.2 ClientHello may be fragmented if the server
@@ -4993,6 +4993,7 @@ int mbedtls_ssl_get_session(const mbedtls_ssl_context *ssl,
  *                   that #MBEDTLS_SSL_PROTO_TLS1_3 enabled, and the
  *                   accepted versions have not been restricted with
  *                   mbedtls_ssl_conf_max_tls_version() or the like).
+ *                   This limitation does not apply to DTLS.
  *                 - The first fragment of a handshake message must be
  *                   at least 4 bytes long.
  *                 - Non-handshake records must not be interleaved between

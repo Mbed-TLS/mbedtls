15dbd63f5d1133445865d1de9650402dfa4cf8a8
Anton Matkin <anton.matkin@arm.com>
2025-03-19 14:56:57 +0100

    Combine psa_pake_set_password_key and psa_pake_setup into a single function
    Signed-off-by: Anton Matkin <anton.matkin@arm.com>

--- library/ssl_tls.c
+++ library/ssl_tls.c
@@ -1838,7 +1838,7 @@ static psa_status_t mbedtls_ssl_set_hs_ecjpake_password_common(
                                                  256));
     psa_pake_cs_set_hash(&cipher_suite, PSA_ALG_SHA_256);
 
-    status = psa_pake_setup(&ssl->handshake->psa_pake_ctx, &cipher_suite);
+    status = psa_pake_setup(&ssl->handshake->psa_pake_ctx, pwd, &cipher_suite);
     if (status != PSA_SUCCESS) {
         return status;
     }
@@ -1861,11 +1861,6 @@ static psa_status_t mbedtls_ssl_set_hs_ecjpake_password_common(
     }
 
     status = psa_pake_set_peer(&ssl->handshake->psa_pake_ctx, peer, peer_len);
-    if (status != PSA_SUCCESS) {
-        return status;
-    }
-
-    status = psa_pake_set_password_key(&ssl->handshake->psa_pake_ctx, pwd);
     if (status != PSA_SUCCESS) {
         return status;
     }

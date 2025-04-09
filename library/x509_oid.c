a647b9147b336b93c2c8ca7d5a6a036707fa15e8
Gilles Peskine <Gilles.Peskine@arm.com>
2025-04-09 20:03:03 +0200

    Create file for OID definitions used by X.509
    This will be parts of oid.c that are not used by crypto.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ library/x509_oid.c
@@ -0,0 +1,19 @@
+/**
+ * \file x509_oid.c
+ *
+ * \brief OID definitions used in X.509.
+ */
+
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
+#include "x509_internal.h"
+
+#if defined(MBEDTLS_X509_USE_C) || defined(MBEDTLS_X509_CREATE_C)
+
+#include "mbedtls/oid.h"
+#include "oid_definition_helpers.h"
+
+#endif /* defined(MBEDTLS_X509_USE_C) || defined(MBEDTLS_X509_CREATE_C) */

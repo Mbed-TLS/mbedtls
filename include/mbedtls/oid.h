30e1f16b152719e3392ecae8d29224720ca04737
Gilles Peskine <Gilles.Peskine@arm.com>
2025-05-07 20:10:35 +0200

    Create a public header file for OID values
    This will be a subset of the former `<mbedtls/oid.h>`, with only macro
    definitions, no function declarations.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ include/mbedtls/oid.h
@@ -0,0 +1,16 @@
+/**
+ * \file oid.h
+ *
+ * \brief Object Identifier (OID) values
+ */
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+#ifndef MBEDTLS_OID_H
+#define MBEDTLS_OID_H
+
+#include "mbedtls/build_info.h"
+
+
+#endif /* oid.h */

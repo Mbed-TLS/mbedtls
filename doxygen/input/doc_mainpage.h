0204470f388f432d83884f379b830cb121604d3b
Gilles Peskine <Gilles.Peskine@arm.com>
2025-06-25 15:40:01 +0200

    Slight improvement to the Doxygen entry point
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- doxygen/input/doc_mainpage.h
+++ doxygen/input/doc_mainpage.h
@@ -12,8 +12,25 @@
 /**
  * @mainpage Mbed TLS v4.0.0 API Documentation
  *
- * This documentation describes the internal structure of Mbed TLS.  It was
- * automatically generated from specially formatted comment blocks in
- * Mbed TLS's source code using Doxygen.  (See
- * https://www.doxygen.nl for more information on Doxygen)
+ * This documentation describes the application programming interface (API)
+ * of Mbed TLS.
+ * It was automatically generated from specially formatted comment blocks in
+ * Mbed TLS's source code using [Doxygen](https://www.doxygen.nl).
+ *
+ * ## Main entry points
+ *
+ * You can explore the full API from the “Files” or “Files list” section.
+ * Locate the header file for the module that you are interested in and
+ * explore its contents.
+ *
+ * Some parts of the API are best explored from the “Topics” or
+ * “Group list” section.
+ * This is notable the case for the PSA Cryptography API.
+ * Note that many parts of the API are not classified under a topic and
+ * can only be seen through the file structure.
+ *
+ * For information on configuring the library at compile time, see the
+ * configuration header files mbedtls/mbedtls_config.h and
+ * psa/crypto_config.h.
+ *
  */

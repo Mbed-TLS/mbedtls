d5da020a632a953eb33b5079c9e425a5eb04d8e6
Ronald Cron <ronald.cron@arm.com>
2025-07-02 09:12:50 +0200

    depends.py: Do not fail when disabling a non-existing option
    To ease the removal of legacy crypto options, do not
    fail in depends.py when disabling a non-existing option.
    This mimics the behavior of 'config.py unset'.
    
    Signed-off-by: Ronald Cron <ronald.cron@arm.com>

--- tests/scripts/depends.py
+++ tests/scripts/depends.py
@@ -109,6 +109,8 @@ def set_config_option_value(conf, option, colors, value: Union[bool, str]):
 value can be either True/False (set/unset config option), or a string,
 which will make a symbol defined with a certain value."""
     if not option_exists(conf, option):
+        if value is False:
+            return True
         log_line('Symbol {} was not found in {}'.format(option, conf.filename), color=colors.red)
         return False
 

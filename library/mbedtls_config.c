018e09872d728f291e32f03dd5fbe0a36ae25269
Gilles Peskine <Gilles.Peskine@arm.com>
2025-07-09 16:16:45 +0200

    New source file for configuration checks
    This will be populated in subsequent commits.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ library/mbedtls_config.c
@@ -0,0 +1,9 @@
+/*
+ *  Mbed TLS configuration checks
+ */
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
+#include <mbedtls/build_info.h>

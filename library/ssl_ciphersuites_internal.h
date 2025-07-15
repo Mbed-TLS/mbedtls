1030f80a0b9cab71941c83cbd322f3c4a9d52ddb
Ben Taylor <ben.taylor@linaro.org>
2025-07-15 14:55:41 +0100

    Add private include to additional files
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- library/ssl_ciphersuites_internal.h
+++ library/ssl_ciphersuites_internal.h
@@ -11,6 +11,9 @@
 #define MBEDTLS_SSL_CIPHERSUITES_INTERNAL_H
 
 #include "mbedtls/pk.h"
+#if defined(MBEDTLS_PK_HAVE_PRIVATE_HEADER)
+#include <mbedtls/private/pk_private.h>
+#endif /* MBEDTLS_PK_HAVE_PRIVATE_HEADER */
 
 #if defined(MBEDTLS_PK_C)
 mbedtls_pk_type_t mbedtls_ssl_get_ciphersuite_sig_pk_alg(const mbedtls_ssl_ciphersuite_t *info);

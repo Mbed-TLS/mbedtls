ac637ac9f81c4218b8c2dfffec244e85915f9338
Gilles Peskine <Gilles.Peskine@arm.com>
2025-07-22 21:54:31 +0200

    Make check_config.h private
    `check_config.h` only needs to run once on the configuration. It doesn't
    need to run every time an application is built. It used to be public up to
    Mbed TLS 2.x because it was included from `config.h`, and users could
    substitute that file completely and should still include `check_config.h`
    from their file. But since Mbed TLS 3.x, including `check_config.h` is a
    purely internal thing (done in `build_info.h`). So make the file itself
    purely internal.
    
    We don't need to include `check_config.h` when building every library file,
    just one: `mbedtls_config.c`, that's its job.
    
    Give the file a unique name, to avoid any clashes with TF-PSA-Crypto's
    `check_config.h`.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- library/mbedtls_config.c
+++ library/mbedtls_config.c
@@ -7,3 +7,7 @@
  */
 
 #include <mbedtls/build_info.h>
+
+/* Consistency checks in the configuration: check for incompatible options,
+ * missing options when at least one of a set needs to be enabled, etc. */
+#include "mbedtls_check_config.h"

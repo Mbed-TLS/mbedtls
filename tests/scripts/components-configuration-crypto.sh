fbd806ae95a656f1c474a3435ab17ceffc235491
Ben Taylor <ben.taylor@linaro.org>
2025-08-12 11:41:20 +0100

    Remove everest ECDH test as it is no longer required
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- tests/scripts/components-configuration-crypto.sh
+++ tests/scripts/components-configuration-crypto.sh
@@ -430,28 +430,6 @@ component_test_everest () {
     tests/compat.sh -f ECDH -V NO -e 'ARIA\|CAMELLIA\|CHACHA'
 }
 
-component_test_everest_curve25519_only () {
-    msg "build: Everest ECDH context, only Curve25519" # ~ 6 min
-    scripts/config.py set MBEDTLS_ECDH_VARIANT_EVEREST_ENABLED
-    scripts/config.py unset MBEDTLS_ECDSA_C
-    scripts/config.py unset PSA_WANT_ALG_DETERMINISTIC_ECDSA
-    scripts/config.py unset PSA_WANT_ALG_ECDSA
-    scripts/config.py set PSA_WANT_ALG_ECDH
-    scripts/config.py unset MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED
-    scripts/config.py unset MBEDTLS_ECJPAKE_C
-    scripts/config.py unset PSA_WANT_ALG_JPAKE
-
-    # Disable all curves
-    scripts/config.py unset-all "MBEDTLS_ECP_DP_[0-9A-Z_a-z]*_ENABLED"
-    scripts/config.py unset-all "PSA_WANT_ECC_[0-9A-Z_a-z]*$"
-    scripts/config.py set PSA_WANT_ECC_MONTGOMERY_255
-
-    make CC=$ASAN_CC CFLAGS="$ASAN_CFLAGS" LDFLAGS="$ASAN_CFLAGS"
-
-    msg "test: Everest ECDH context, only Curve25519" # ~ 50s
-    make test
-}
-
 component_test_psa_collect_statuses () {
   msg "build+test: psa_collect_statuses" # ~30s
   scripts/config.py full

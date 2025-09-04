a2aa7daacae757dac9cc02fa1250778b92f79ffe
Ben Taylor <ben.taylor@linaro.org>
2025-09-04 11:22:52 +0100

    Change unset of MBEDTLS config to more standard method
    Signed-off-by: Ben Taylor <ben.taylor@linaro.org>

--- tests/scripts/components-configuration-crypto.sh
+++ tests/scripts/components-configuration-crypto.sh
@@ -1356,7 +1356,7 @@ component_test_tfm_config_no_p256m () {
 
     # Disable P256M driver, which is on by default, so that analyze_outcomes
     # can compare this test with test_tfm_config_p256m_driver_accel_ec
-    sed -i '/MBEDTLS_PSA_P256M_DRIVER_ENABLED/d'  "$CRYPTO_CONFIG_H"
+    scripts/config.py -f "$CRYPTO_CONFIG_H" unset MBEDTLS_PSA_P256M_DRIVER_ENABLED
 
     msg "build: TF-M config without p256m"
     make CFLAGS='-Werror -Wall -Wextra -I../framework/tests/include/spe' tests

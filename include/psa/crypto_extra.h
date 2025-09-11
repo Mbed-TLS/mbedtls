6e1b66320a5cfd1c36c0caa86abed5dce896cc85
Gilles Peskine <Gilles.Peskine@arm.com>
2025-09-11 18:34:29 +0200

    Improve documentation
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- include/psa/crypto_extra.h
+++ include/psa/crypto_extra.h
@@ -600,9 +600,10 @@ psa_status_t mbedtls_psa_platform_get_builtin_key(
  * This means that PSA core was built with the corresponding PSA_WANT_ALG_xxx
  * set and that psa_crypto_init has already been called.
  *
- * \note When using Mbed TLS version of PSA core (i.e. MBEDTLS_PSA_CRYPTO_C is
- *       set) for now this function only checks the state of the driver
- *       subsystem, not the algorithm. This might be improved in the future.
+ * \note When using the built-in version of the PSA core (i.e.
+ *       #MBEDTLS_PSA_CRYPTO_C is set), for now, this function only checks
+ *       the state of the driver subsystem, not the algorithm.
+ *       This might be improved in the future.
  *
  * \param hash_alg  The hash algorithm.
  *
@@ -613,12 +614,15 @@ int psa_can_do_hash(psa_algorithm_t hash_alg);
 /**
  * Tell if PSA is ready for this cipher.
  *
- * \note            For now, only checks the state of the driver subsystem,
- *                  not the algorithm. Might do more in the future.
+ * \note When using the built-in version of the PSA core (i.e.
+ *       #MBEDTLS_PSA_CRYPTO_C is set), for now, this function only checks
+ *       the state of the driver subsystem, not the key type and algorithm.
+ *       This might be improved in the future.
  *
- * \param cipher_alg  The cipher algorithm (ignored for now).
+ * \param key_type    The key type.
+ * \param cipher_alg  The cipher algorithm.
  *
- * \return 1 if the driver subsytem is ready, 0 otherwise.
+ * \return 1 if the PSA can handle \p hash_alg, 0 otherwise.
  */
 int psa_can_do_cipher(psa_key_type_t key_type, psa_algorithm_t cipher_alg);
 

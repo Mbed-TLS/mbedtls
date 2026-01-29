29d00fa8613033fafe27cd4c3dafa0e8360cb003
Gilles Peskine <Gilles.Peskine@arm.com>
2026-01-29 16:20:19 +0100

    Create a mbedtls_common.h for the project
    We already have `x509_internal.h` which is common to all parts of the X.509
    library, and `ssl_misc.h` which is common to all parts of the TLS library.
    Also create `mbedtls_common.h` which is for the Mbed TLS project as a whole.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

---
+++ library/mbedtls_common.h
@@ -0,0 +1,17 @@
+/**
+ * \file mbedtls_common.h
+ *
+ * \brief Utility macros for internal use in the library
+ */
+/*
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
+#ifndef MBEDTLS_MBEDTLS_COMMON_H
+#define MBEDTLS_MBEDTLS_COMMON_H
+
+/* Mbed TLS requires TF-PSA-Crypto internals. */
+#include "tf_psa_crypto_common.h"
+
+#endif /* MBEDTLS_MBEDTLS_COMMON_H */

32caf3bad07ac9fd222625a2e78a2114caa7ec58
Minos Galanakis <minos.galanakis@arm.com>
2026-03-10 15:46:04 +0000

    ccm: Fail when calling finish without ccm_starts
    Signed-off-by: Minos Galanakis <minos.galanakis@arm.com>

--- library/ccm.c
+++ library/ccm.c
@@ -480,6 +480,10 @@ int mbedtls_ccm_finish(mbedtls_ccm_context *ctx,
         return MBEDTLS_ERR_ERROR_CORRUPTION_DETECTED;
     }
 
+    if (!(ctx->state & CCM_STATE__STARTED)) {
+        return MBEDTLS_ERR_CCM_BAD_INPUT;
+    }
+
     if (ctx->add_len > 0 && !(ctx->state & CCM_STATE__AUTH_DATA_FINISHED)) {
         return MBEDTLS_ERR_CCM_BAD_INPUT;
     }

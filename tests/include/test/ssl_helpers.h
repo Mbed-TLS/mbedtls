562326e4915f448406abd381c0bc23bd01e4159f
Janos Follath <janos.follath@arm.com>
2026-03-16 19:16:13 +0000

    send_invalid_sig_alg: use driver friendly guards
    The compile time guards were too restrictive, excluding builds with
    drivers, updating them will allow driver tests to run as well.
    
    Signed-off-by: Janos Follath <janos.follath@arm.com>

--- tests/include/test/ssl_helpers.h
+++ tests/include/test/ssl_helpers.h
@@ -73,25 +73,25 @@
 #endif
 
 #if defined(MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED) && \
-    defined(MBEDTLS_ECP_DP_SECP384R1_ENABLED) && \
-    defined(MBEDTLS_SHA384_C)
+    defined(MBEDTLS_ECP_HAVE_SECP384R1) && \
+    defined(MBEDTLS_MD_CAN_SHA384)
 #define MBEDTLS_CAN_HANDLE_ECDSA_TEST_KEY
 #endif
 
 #if defined(MBEDTLS_KEY_EXCHANGE_ECDHE_ECDSA_ENABLED) && \
-    defined(MBEDTLS_ECP_DP_SECP256R1_ENABLED) && \
-    defined(MBEDTLS_SHA256_C)
+    defined(MBEDTLS_ECP_HAVE_SECP256R1) && \
+    defined(MBEDTLS_MD_CAN_SHA256)
 #define MBEDTLS_CAN_HANDLE_ECDSA_CLIENT_TEST_KEY
 #endif
 
-#if defined(MBEDTLS_ECP_DP_CURVE25519_ENABLED)  || \
-    defined(MBEDTLS_ECP_DP_SECP256R1_ENABLED)   || \
-    defined(MBEDTLS_ECP_DP_SECP384R1_ENABLED)   || \
-    defined(MBEDTLS_ECP_DP_CURVE448_ENABLED)    || \
-    defined(MBEDTLS_ECP_DP_SECP521R1_ENABLED)   || \
-    defined(MBEDTLS_ECP_DP_BP256R1_ENABLED)     || \
-    defined(MBEDTLS_ECP_DP_BP384R1_ENABLED)     || \
-    defined(MBEDTLS_ECP_DP_BP512R1_ENABLED)
+#if defined(MBEDTLS_ECP_HAVE_CURVE25519)  || \
+    defined(MBEDTLS_ECP_HAVE_SECP256R1)   || \
+    defined(MBEDTLS_ECP_HAVE_SECP384R1)   || \
+    defined(MBEDTLS_ECP_HAVE_CURVE448)    || \
+    defined(MBEDTLS_ECP_HAVE_SECP521R1)   || \
+    defined(MBEDTLS_ECP_HAVE_BP256R1)     || \
+    defined(MBEDTLS_ECP_HAVE_BP384R1)     || \
+    defined(MBEDTLS_ECP_HAVE_BP512R1)
 #define MBEDTLS_TEST_HAS_DEFAULT_EC_GROUP
 #endif
 
@@ -99,8 +99,8 @@
  * To use the test keys we need PSA_WANT_ALG_SHA_256. Some test cases need an additional hash that
  * is configured by default (see mbedtls_ssl_config_defaults()), but it doesn't matter which one.
  */
-#if defined(MBEDTLS_SHA512_C)   || \
-    defined(MBEDTLS_SHA384_C)
+#if defined(MBEDTLS_MD_CAN_SHA512)   || \
+    defined(MBEDTLS_MD_CAN_SHA384)
 #define MBEDTLS_TEST_HAS_ADDITIONAL_HASH
 #endif
 

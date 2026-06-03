21df158961b1d51f1adbb3bcf81c5d57532e9cbc
Gilles Peskine <Gilles.Peskine@arm.com>
2026-06-03 15:43:18 +0200

    Add some hooks in mbedtls_test_ssl_perform_handshake()
    Let test code customize the kitchen sink function
    `mbedtls_test_ssl_perform_handshake()` at a few points.
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/src/test_helpers/ssl_helpers.c
+++ tests/src/test_helpers/ssl_helpers.c
@@ -2282,6 +2282,10 @@ int mbedtls_test_ssl_perform_connection(
         expected_handshake_result = MBEDTLS_ERR_SSL_BAD_PROTOCOL_VERSION;
     }
 
+    if (options->pre_handshake_fun != NULL) {
+        options->pre_handshake_fun(client, server, options->pre_handshake_param);
+    }
+
     TEST_EQUAL(mbedtls_test_move_handshake_to_state(&(client->ssl),
                                                     &(server->ssl),
                                                     MBEDTLS_SSL_HANDSHAKE_OVER),
@@ -2313,6 +2317,10 @@ int mbedtls_test_ssl_perform_connection(
                    options->expected_ciphersuite);
     }
 
+    if (options->post_handshake_fun != NULL) {
+        options->post_handshake_fun(client, server, options->post_handshake_param);
+    }
+
 #if defined(MBEDTLS_SSL_VARIABLE_BUFFER_LENGTH)
     if (options->resize_buffers != 0) {
         /* A server, when using DTLS, might delay a buffer resize to happen
@@ -2339,6 +2347,11 @@ int mbedtls_test_ssl_perform_connection(
                        options->expected_srv_fragments),
                    0);
     }
+
+    if (options->post_data_fun != NULL) {
+        options->post_data_fun(client, server, options->post_data_param);
+    }
+
 #if defined(MBEDTLS_SSL_CONTEXT_SERIALIZATION)
     if (options->serialize == 1) {
         TEST_ASSERT(test_serialization(options, client, server));
@@ -2395,6 +2408,10 @@ void mbedtls_test_ssl_perform_handshake(
     TEST_ASSERT(mbedtls_ssl_get_user_data_p(&client->ssl) == client);
     TEST_ASSERT(mbedtls_ssl_conf_get_user_data_p(&server->conf) == server);
     TEST_ASSERT(mbedtls_ssl_get_user_data_p(&server->ssl) == server);
+
+    if (options->pre_shutdown_fun != NULL) {
+        options->pre_shutdown_fun(client, server, options->pre_shutdown_param);
+    }
 
 exit:
     mbedtls_test_ssl_endpoint_free(client);

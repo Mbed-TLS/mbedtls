0b556f5a2deb146690a93907264ce81f381a4a6a
Valerio Setti <vsetti@baylibre.com>
2026-06-10 15:59:50 +0200

    pkcs7: add MBEDTLS_PKCS7_ALLOW_WEAK_SIGNATURES build symbol
    It allows weak hash algorithms to be used in PKCS7. It's only added in the
    LTS branch for backward compatibility, but it's disabled by default because
    that's the safest choice.
    
    This commit also updates test data in order to test the new build symbol.
    
    Signed-off-by: Valerio Setti <vsetti@baylibre.com>

--- library/pkcs7.c
+++ library/pkcs7.c
@@ -665,6 +665,7 @@ static int mbedtls_pkcs7_data_or_hash_verify(mbedtls_pkcs7 *pkcs7,
         return ret;
     }
 
+#if !defined(MBEDTLS_PKCS7_ALLOW_WEAK_SIGNATURES)
     /* Ensure the MD alg from the PKCS#7 context and signature algorithm from
      * the certificate belong to the list of secure algorithms
      * (i.e. mbedtls_x509_crt_profile_default). */
@@ -676,6 +677,7 @@ static int mbedtls_pkcs7_data_or_hash_verify(mbedtls_pkcs7 *pkcs7,
     if (ret != 0) {
         return MBEDTLS_ERR_PKCS7_INVALID_ALG;
     }
+#endif /* MBEDTLS_PKCS7_ALLOW_WEAK_SIGNATURES */
 
     md_info = mbedtls_md_info_from_type(md_alg);
     if (md_info == NULL) {

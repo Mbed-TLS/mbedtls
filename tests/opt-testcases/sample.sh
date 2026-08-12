5d1cb85fe839322058934c203dfe9ba4bc476e5e
Gilles Peskine <Gilles.Peskine@arm.com>
2026-08-12 13:37:06 +0200

    Add missing dependency on "same seed" test case
    The test case passes even if the two clients have different RNG seeds, but
    in that case, it doesn't do what it claims, but instead it duplicates the
    "2 successive clients, TLS 1.3" test case above. So run
    "2 successive clients with same seed, TLS 1.3" only when there is no entropy,
    like "2 successive clients with same seed, TLS 1.3".
    
    Signed-off-by: Gilles Peskine <Gilles.Peskine@arm.com>

--- tests/opt-testcases/sample.sh
+++ tests/opt-testcases/sample.sh
@@ -248,6 +248,9 @@ run_test    "Sample: ssl_fork_server, 2 successive clients with same seed, TLS 1
             -C "error" \
             -v 'distinct_server_random'
 
+requires_config_enabled MBEDTLS_ENTROPY_NO_SOURCES_OK
+requires_config_disabled MBEDTLS_PSA_BUILTIN_GET_ENTROPY
+requires_config_disabled MBEDTLS_PSA_DRIVER_GET_ENTROPY
 run_test    "Sample: ssl_fork_server, 2 successive clients with same seed, TLS 1.3: unique random" \
             -P 4433 \
             "server_with_own_seedfile $PROGRAMS_DIR/ssl_fork_server" \

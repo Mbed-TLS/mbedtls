52d8e85a7edaf63c1808c6c8fdda48b665620eee
Yi Wu <yi.wu2@arm.com>
2026-08-14 15:35:17 +0100

    CMake: make generated configs relocatable
    Signed-off-by: Yi Wu <yi.wu2@arm.com>

---
+++ programs/test/cmake_package_install/cmake_package_config.c
@@ -0,0 +1,13 @@
+/*
+ *  Simple program to test an installed Mbed TLS configuration.
+ *
+ *  Copyright The Mbed TLS Contributors
+ *  SPDX-License-Identifier: Apache-2.0 OR GPL-2.0-or-later
+ */
+
+#include "mbedtls/build_info.h"
+
+int main(void)
+{
+    return 0;
+}

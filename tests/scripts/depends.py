0a89faf18a66d489e82ae338a788443846d95cf6
Rohan Luchmun <Rohan.Luchmun@arm.com>
2026-09-25 15:08:40 +0100

    Added missing comma
    Signed-off-by: Rohan Luchmun <Rohan.Luchmun@arm.com>

--- tests/scripts/depends.py
+++ tests/scripts/depends.py
@@ -189,7 +189,7 @@ If what is False, announce that the job has failed.'''
             header += f'#error "{option}"\n'
             header += '#endif\n'
         include_path = ['include', 'tf-psa-crypto/include',
-                        'tf-psa-crypto/drivers/builtin/include'
+                        'tf-psa-crypto/drivers/builtin/include',
                         'tf-psa-crypto/drivers/pqcp/include']
 
         try:
@@ -206,6 +206,7 @@ If what is False, announce that the job has failed.'''
             log_line('Inconsistent config option(s):')
             for option in sorted(failed):
                 log_line('  ' + option)
+            print(e)
             return False
 
         finally:
